3d888bffa2145dda54f1c7d6f431cf5005a6229b
Daniel Frett <daniel.frett@ccci.org>
2013-07-23 13:40:21 -0400

    import several thread related utils

---
+++ src/main/java/org/ccci/gto/android/common/util/ThreadUtils.java
@@ -0,0 +1,35 @@
+package org.ccci.gto.android.common.util;
+
+import android.os.Looper;
+
+import java.util.Map;
+
+public final class ThreadUtils {
+    private ThreadUtils() {
+    }
+
+    public static void assertNotOnUiThread() {
+        if (isUiThread()) {
+            throw new RuntimeException("unsupported method on UI thread");
+        }
+    }
+
+    public static void assertOnUiThread() {
+        if (!isUiThread()) {
+            throw new RuntimeException("method requires UI thread");
+        }
+    }
+
+    public static <K> Object getLock(final Map<K, Object> locks, final K key) {
+        synchronized (locks) {
+            if (!locks.containsKey(key)) {
+                locks.put(key, new Object());
+            }
+            return locks.get(key);
+        }
+    }
+
+    public static boolean isUiThread() {
+        return Looper.getMainLooper().getThread() == Thread.currentThread();
+    }
+}
