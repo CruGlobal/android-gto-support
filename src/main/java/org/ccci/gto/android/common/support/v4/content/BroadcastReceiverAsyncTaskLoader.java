46b3eebc454e3eef51668a32d307f9bc1dadb6e1
Daniel Frett <daniel.frett@ccci.org>
2013-08-20 16:42:58 -0400

    implemented a generic AsyncTask BroadcastReceiverLoader

---
+++ src/main/java/org/ccci/gto/android/common/support/v4/content/BroadcastReceiverAsyncTaskLoader.java
@@ -0,0 +1,36 @@
+package org.ccci.gto.android.common.support.v4.content;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.support.v4.content.AsyncTaskLoader;
+
+public abstract class BroadcastReceiverAsyncTaskLoader<T> extends AsyncTaskLoader<T>
+        implements BroadcastReceiverLoaderHelper.Interface {
+    private final BroadcastReceiverLoaderHelper helper;
+
+    public BroadcastReceiverAsyncTaskLoader(final Context context, final IntentFilter... filters) {
+        super(context);
+        this.helper = new BroadcastReceiverLoaderHelper(this, filters);
+    }
+
+    /* BEGIN lifecycle */
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        this.helper.onStartLoading();
+    }
+
+    @Override
+    protected void onAbandon() {
+        super.onAbandon();
+        this.helper.onAbandon();
+    }
+
+    /* END lifecycle */
+
+    @Override
+    public final void addIntentFilter(final IntentFilter filter) {
+        this.helper.addIntentFilter(filter);
+    }
+}
