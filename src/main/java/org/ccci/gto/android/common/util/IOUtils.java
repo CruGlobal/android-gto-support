7f6f0f69659014e0912c7cb4f6a05afec073d91b
Daniel Frett <daniel.frett@ccci.org>
2013-08-29 14:32:10 -0400

    added some common util libraries from the ekko project

---
+++ src/main/java/org/ccci/gto/android/common/util/IOUtils.java
@@ -0,0 +1,52 @@
+package org.ccci.gto.android.common.util;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+
+public final class IOUtils {
+    private static final int DEFAULT_BUFFER_SIZE = 4096;
+    private static final int EOF = -1;
+
+    public static final void closeQuietly(final Closeable handle) {
+        if (handle != null) {
+            try {
+                handle.close();
+            } catch (final IOException e) {
+                // suppress the error
+            }
+        }
+    }
+
+    public static final void closeQuietly(final HttpURLConnection conn) {
+        if (conn != null) {
+            conn.disconnect();
+        }
+    }
+
+    public static final long copy(final InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+        long count = 0;
+        int n = 0;
+        while (EOF != (n = in.read(buffer))) {
+            out.write(buffer, 0, n);
+            count += n;
+        }
+        return count;
+    }
+
+    public static final String readString(final InputStream in) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"), DEFAULT_BUFFER_SIZE);
+        final StringBuilder out = new StringBuilder();
+        final char[] buffer = new char[DEFAULT_BUFFER_SIZE];
+        int n;
+        while (EOF != (n = reader.read(buffer, 0, buffer.length))) {
+            out.append(buffer, 0, n);
+        }
+        return out.toString();
+    }
+}
---
+++ src/main/java/org/ccci/gto/android/common/util/UriUtils.java
@@ -0,0 +1,49 @@
+package org.ccci.gto.android.common.util;
+
+import android.net.Uri;
+import android.net.Uri.Builder;
+
+import java.util.regex.Pattern;
+
+public final class UriUtils {
+    public static final Builder removeQueryParams(final Builder uri, final String... keys) {
+        if (keys.length > 0) {
+            final Uri uriTemp = uri.build();
+            String query = uriTemp.getEncodedQuery();
+            if (query != null) {
+                for (final String key : keys) {
+                    // remove all values for key from query
+                    final String encodedKey = Uri.encode(key);
+                    query = query.replaceAll("&?" + Pattern.quote(encodedKey) + "=[^&]*&?", "&");
+                }
+
+                // strip leading/trailing &
+                while (query.startsWith("&")) {
+                    query = query.substring(1);
+                }
+                while (query.endsWith("&")) {
+                    query = query.substring(0, query.length() - 1);
+                }
+
+                // replace query
+                uri.encodedQuery(query);
+            }
+        }
+
+        // return the Builder to allow chaining
+        return uri;
+    }
+
+    public static final Builder replaceQueryParam(final Builder uri, final String key, final String... values) {
+        // remove all values for key from query
+        removeQueryParams(uri, key);
+
+        // append all specified values
+        for (final String value : values) {
+            uri.appendQueryParameter(key, value);
+        }
+
+        // return the Builder to allow chaining
+        return uri;
+    }
+}
