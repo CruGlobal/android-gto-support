7e955dacf362e4b68a411a2d7a43ce910c1a786f
Daniel Frett <daniel.frett@ccci.org>
2013-10-17 13:19:39 -0400

    added a safe nextText XmlUtils static method

---
+++ src/main/java/org/ccci/gto/android/common/util/XmlUtils.java
@@ -0,0 +1,25 @@
+package org.ccci.gto.android.common.util;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+public final class XmlUtils {
+    /**
+     * safely call nextText on all versions of Android.
+     *
+     * @param parser the current parser
+     * @return the next text node from the XmlPullParser
+     * @throws IOException
+     * @throws XmlPullParserException
+     * @see "http://android-developers.blogspot.com/2011/12/watch-out-for-xmlpullparsernexttext.html"
+     */
+    public static final String safeNextText(final XmlPullParser parser) throws IOException, XmlPullParserException {
+        final String result = parser.nextText();
+        if (parser.getEventType() != XmlPullParser.END_TAG) {
+            parser.nextTag();
+        }
+        return result;
+    }
+}
