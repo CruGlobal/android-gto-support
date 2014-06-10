c60e68f7705b9fbdc0065ec3726610f884429f79
Daniel Frett <daniel.frett@ccci.org>
2014-06-10 16:42:55 -0400

    added a new BroadcastUtils class that contains utilities for Broadcasted Intents

---
+++ src/main/java/org/ccci/gto/android/common/util/BroadcastUtils.java
@@ -0,0 +1,26 @@
+package org.ccci.gto.android.common.util;
+
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.PatternMatcher;
+
+public final class BroadcastUtils {
+    public static void addDataUri(final IntentFilter filter, final Uri uri) {
+        addDataUri(filter, uri, PatternMatcher.PATTERN_LITERAL);
+    }
+
+    public static void addDataUri(final IntentFilter filter, final Uri uri, final int type) {
+        final String scheme = uri.getScheme();
+        if (scheme != null) {
+            filter.addDataScheme(scheme);
+        }
+        final String host = uri.getHost();
+        if (host != null) {
+            filter.addDataAuthority(host, null);
+        }
+        final String path = uri.getPath();
+        if (path != null) {
+            filter.addDataPath(path, type);
+        }
+    }
+}
