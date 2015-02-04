b38d01ec5873debec36dc29b16edd81a645c8aee
Daniel Frett <daniel.frett@ccci.org>
2015-02-03 23:41:16 -0500

    deprecate StringUtils.join in favor of built-in TextUtils.join

--- src/main/java/org/ccci/gto/android/common/util/StringUtils.java
+++ src/main/java/org/ccci/gto/android/common/util/StringUtils.java
@@ -1,23 +1,13 @@
 package org.ccci.gto.android.common.util;
 
+import android.text.TextUtils;
+
 public final class StringUtils {
     private StringUtils() {
     }
 
+    @Deprecated
     public static String join(final String sep, final String... parts) {
-        assert sep != null;
-
-        final StringBuilder sb = new StringBuilder(sep.length() * parts.length);
-        boolean first = true;
-        for (final String part : parts) {
-            assert part != null;
-            if (!first) {
-                sb.append(sep);
-            }
-            sb.append(part);
-            first = false;
-        }
-
-        return sb.toString();
+        return TextUtils.join(sep, parts);
     }
 }
