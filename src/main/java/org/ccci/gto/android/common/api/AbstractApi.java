d5ca438ff250844fafbefdc1caedfdbb525e0861
Daniel Frett <daniel.frett@ccci.org>
2015-02-07 18:43:31 -0500

    add initial session support for the AbstractApi

--- src/main/java/org/ccci/gto/android/common/api/AbstractApi.java
+++ src/main/java/org/ccci/gto/android/common/api/AbstractApi.java
@@ -1,10 +1,15 @@
 package org.ccci.gto.android.common.api;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import org.ccci.gto.android.common.api.AbstractApi.Request;
+import org.ccci.gto.android.common.api.AbstractApi.Session;
 import org.ccci.gto.android.common.util.IOUtils;
 import org.ccci.gto.android.common.util.UriUtils;
 
@@ -16,18 +21,41 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public abstract class AbstractApi<R extends AbstractApi.Request> {
+public abstract class AbstractApi<R extends Request<S>, S extends Session> {
     private static final int DEFAULT_ATTEMPTS = 3;
+    protected static final String PREF_SESSION_BASE_NAME = "session";
+
+    protected final Object LOCK_SESSION = new Object();
+
+    @NonNull
+    private final Context mContext;
 
     @NonNull
     private final Uri mBaseUri;
+    @NonNull
+    private final String mPrefFile;
 
-    protected AbstractApi(@NonNull final String baseUri) {
-        this(Uri.parse(baseUri.endsWith("/") ? baseUri : baseUri + "/"));
+    protected AbstractApi(@NonNull final Context context, @NonNull final String baseUri) {
+        this(context, baseUri, null);
     }
 
-    protected AbstractApi(@NonNull final Uri baseUri) {
-        mBaseUri = baseUri;
+    protected AbstractApi(@NonNull final Context context, @NonNull final String baseUri,
+                          @Nullable final String prefFile) {
+        this(context, Uri.parse(baseUri.endsWith("/") ? baseUri : baseUri + "/"), prefFile);
+    }
+
+    protected AbstractApi(@NonNull final Context context, @NonNull final Uri baseUri) {
+        this(context, baseUri, null);
+    }
+
+    protected AbstractApi(@NonNull final Context context, @NonNull final Uri baseUri, @Nullable final String prefFile) {
+        this.mContext = context;
+        this.mBaseUri = baseUri;
+        this.mPrefFile = prefFile != null ? prefFile : getClass().getSimpleName();
+    }
+
+    private SharedPreferences getPrefs() {
+        return mContext.getSharedPreferences(mPrefFile, Context.MODE_PRIVATE);
     }
 
     @NonNull
@@ -41,6 +69,30 @@ public abstract class AbstractApi<R extends AbstractApi.Request> {
         HttpURLConnection conn = null;
         boolean successful = false;
         try {
+            // load/establish the session if we are using sessions
+            if (request.useSession) {
+                // prepare for the session
+                this.onPrepareSession(request);
+
+                // get the session, establish a session if one doesn't exist or if we have a stale session
+                synchronized (LOCK_SESSION) {
+                    request.session = loadSession(request);
+                    if (request.session == null) {
+                        request.session = this.establishSession(request);
+
+                        // save the newly established session
+                        if (request.session != null) {
+                            this.saveSession(request.session);
+                        }
+                    }
+                }
+
+                // throw an exception if we don't have a valid session
+                if (request.session == null) {
+                    throw new InvalidSessionApiException();
+                }
+            }
+
             // build the request uri
             final Uri.Builder uri = mBaseUri.buildUpon();
             onPrepareUri(uri, request);
@@ -57,6 +109,21 @@ public abstract class AbstractApi<R extends AbstractApi.Request> {
 
             // no need to explicitly execute, accessing the response triggers the execute
 
+            // check for an invalid session
+            if (request.useSession && this.isSessionInvalid(conn, request)) {
+                // reset the session
+                synchronized (LOCK_SESSION) {
+                    // only reset if this is still the same session
+                    final Session current = this.loadSession(request);
+                    if (current != null && current.equals(request.session)) {
+                        this.deleteSession(request.session);
+                    }
+                }
+
+                // throw an invalid session exception
+                throw new InvalidSessionApiException();
+            }
+
             // process the response
             onProcessResponse(conn, request);
 
@@ -70,6 +137,12 @@ public abstract class AbstractApi<R extends AbstractApi.Request> {
             if(!successful) {
                 IOUtils.closeQuietly(conn);
             }
+
+            // clear out the session that was loaded & used
+            request.session = null;
+
+            // cleanup any request specific data
+            onCleanupRequest(request);
         }
     }
 
@@ -78,7 +151,65 @@ public abstract class AbstractApi<R extends AbstractApi.Request> {
         return new Request.Parameter(name, value);
     }
 
+    @Nullable
+    protected S loadSession(@NonNull final R request) {
+        // load a pre-existing session
+        final SharedPreferences prefs = this.getPrefs();
+        final S session;
+        synchronized (LOCK_SESSION) {
+            session = this.loadSession(prefs, request);
+        }
+
+        // only return valid sessions
+        return session != null && session.id != null ? session : null;
+    }
+
+    @Nullable
+    protected abstract S loadSession(@NonNull SharedPreferences prefs, @NonNull R request);
+
+    @Nullable
+    protected S establishSession(@NonNull final R request) throws ApiException {
+        return null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private void saveSession(@NonNull final S session) {
+        final SharedPreferences.Editor prefs = this.getPrefs().edit();
+        session.save(prefs);
+
+        synchronized (LOCK_SESSION) {
+            // store updates
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+                prefs.apply();
+            } else {
+                prefs.commit();
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private void deleteSession(@NonNull final S session) {
+        final SharedPreferences.Editor prefs = this.getPrefs().edit();
+        session.delete(prefs);
+
+        synchronized (LOCK_SESSION) {
+            // store updates
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+                prefs.apply();
+            } else {
+                prefs.commit();
+            }
+        }
+    }
+
+    protected boolean isSessionInvalid(@NonNull final HttpURLConnection conn, final R request) {
+        return false;
+    }
+
     /* BEGIN request lifecycle events */
+
+    protected void onPrepareSession(@NonNull final R request) throws ApiException {
+    }
 
     protected void onPrepareUri(@NonNull final Uri.Builder uri, @NonNull final R request)
             throws ApiException {
@@ -112,9 +243,63 @@ public abstract class AbstractApi<R extends AbstractApi.Request> {
             throws ApiException, IOException {
     }
 
+    protected void onCleanupRequest(@NonNull final R request) {
+    }
+
     /* END request lifecycle events */
 
-    protected static class Request {
+    protected static class Session {
+        @NonNull
+        private final String baseAttrName;
+
+        @Nullable
+        public final String id;
+
+        protected Session(@Nullable final String id) {
+            this(id, PREF_SESSION_BASE_NAME);
+        }
+
+        protected Session(@Nullable final String id, @Nullable final String baseAttrName) {
+            this.baseAttrName = baseAttrName != null ? baseAttrName : PREF_SESSION_BASE_NAME;
+            this.id = id;
+        }
+
+        protected Session(@NonNull final SharedPreferences prefs) {
+            this(prefs, null);
+        }
+
+        protected Session(@NonNull final SharedPreferences prefs, @Nullable final String baseAttrName) {
+            this.baseAttrName = baseAttrName != null ? baseAttrName : PREF_SESSION_BASE_NAME;
+            this.id = prefs.getString(getPrefAttrName("id"), null);
+        }
+
+        protected final String getPrefAttrName(@NonNull final String type) {
+            return baseAttrName + "." + type;
+        }
+
+        protected void save(@NonNull final SharedPreferences.Editor prefs) {
+            prefs.putString(getPrefAttrName("id"), this.id);
+        }
+
+        protected void delete(@NonNull final SharedPreferences.Editor prefs) {
+            prefs.remove(getPrefAttrName("id"));
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            final Session that = (Session) o;
+            return baseAttrName.equals(that.baseAttrName) && !(id != null ? !id.equals(that.id) : that.id != null);
+        }
+    }
+
+    protected static class Request<S extends Session> {
         public enum Method {GET, POST, PUT, DELETE}
 
         public enum MediaType {
@@ -146,6 +331,11 @@ public abstract class AbstractApi<R extends AbstractApi.Request> {
         final String path;
         public final Collection<Parameter> params = new ArrayList<>();
         public boolean replaceParams = false;
+
+        // session attributes
+        public boolean useSession = false;
+        @Nullable
+        public S session = null;
 
         // miscellaneous attributes
         @Nullable
