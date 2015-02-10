817e1244802ac25a0ed0867cb396b0fea05535fd
Daniel Frett <daniel.frett@ccci.org>
2015-02-10 18:57:55 -0500

    add an Activity findView util

--- src/main/java/org/ccci/gto/android/common/util/ViewUtils.java
+++ src/main/java/org/ccci/gto/android/common/util/ViewUtils.java
@@ -1,17 +1,28 @@
 package org.ccci.gto.android.common.util;
 
+import android.app.Activity;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 
 public final class ViewUtils {
     @Nullable
-    public static <T> T findView(@Nullable final View root, @NonNull final Class<T> clazz, final int id) {
+    public static <T> T findView(@Nullable final View root, @NonNull final Class<T> clazz, @IdRes final int id) {
         if (root != null) {
             final View view = root.findViewById(id);
             if (clazz.isInstance(view)) {
                 return clazz.cast(view);
             }
+        }
+        return null;
+    }
+
+    @Nullable
+    public static <T> T findView(@NonNull final Activity activity, @NonNull final Class<T> clazz, @IdRes final int id) {
+        final View view = activity.findViewById(id);
+        if (clazz.isInstance(view)) {
+            return clazz.cast(view);
         }
         return null;
     }
