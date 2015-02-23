181cf91518103d9d44f796f96d8fa9de73182979
Daniel Frett <daniel.frett@ccci.org>
2015-02-23 10:22:33 -0500

    work around an outstanding issue with DialogFragments in the support library

--- src/main/java/org/ccci/gto/android/common/support/v4/fragment/AbstractDialogFragment.java
+++ src/main/java/org/ccci/gto/android/common/support/v4/fragment/AbstractDialogFragment.java
@@ -1,10 +1,28 @@
 package org.ccci.gto.android.common.support.v4.fragment;
 
+import android.app.Dialog;
 import android.support.v4.app.DialogFragment;
 
 import org.ccci.gto.android.common.support.v4.util.FragmentUtils;
 
 public class AbstractDialogFragment extends DialogFragment {
+
+    /* BEGIN lifecycle */
+
+    @Override
+    public void onDestroyView() {
+        // Work around bug:
+        // http://code.google.com/p/android/issues/detail?id=17423
+        final Dialog dialog = this.getDialog();
+        if ((dialog != null) && this.getRetainInstance()) {
+            dialog.setDismissMessage(null);
+        }
+
+        super.onDestroyView();
+    }
+
+    /* END lifecycle */
+
     protected final <T> T findView(final Class<T> clazz, final int id) {
         return FragmentUtils.findView(this, clazz, id);
     }
