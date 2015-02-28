767dab79bd2a51f0274d53f7081a757d847a911c
Daniel Frett <daniel.frett@ccci.org>
2015-02-28 05:20:31 -0500

    add null annotations to LoaderBroadcastReceiver

--- src/main/java/org/ccci/gto/android/common/support/v4/content/LoaderBroadcastReceiver.java
+++ src/main/java/org/ccci/gto/android/common/support/v4/content/LoaderBroadcastReceiver.java
@@ -3,12 +3,14 @@ package org.ccci.gto.android.common.support.v4.content;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.content.Loader;
 
 public class LoaderBroadcastReceiver extends BroadcastReceiver {
+    @NonNull
     private final Loader mLoader;
 
-    public LoaderBroadcastReceiver(final Loader loader) {
+    public LoaderBroadcastReceiver(@NonNull final Loader loader) {
         mLoader = loader;
     }
 
