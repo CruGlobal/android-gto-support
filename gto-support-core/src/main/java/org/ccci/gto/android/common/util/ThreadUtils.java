ff4a9be96d4f613d3e268fe5aa43895c7db5ff0f
Daniel Frett <daniel.frett@ccci.org>
2015-04-07 15:22:33 -0400

    refactor runOnBackgroundThread to AsyncTaskCompat.execute

--- gto-support-core/src/main/java/org/ccci/gto/android/common/util/ThreadUtils.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/util/ThreadUtils.java
@@ -1,16 +1,8 @@
 package org.ccci.gto.android.common.util;
 
-import android.annotation.TargetApi;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Looper;
-import android.support.annotation.NonNull;
 
 import java.util.Map;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 public final class ThreadUtils {
     private ThreadUtils() {
@@ -40,37 +32,5 @@ public final class ThreadUtils {
 
     public static boolean isUiThread() {
         return Looper.getMainLooper().getThread() == Thread.currentThread();
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static void runOnBackgroundThread(@NonNull final Runnable task) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            AsyncTask.execute(task);
-        } else {
-            getExecutor().execute(task);
-        }
-    }
-
-    private static final Object LOCK_EXECUTOR = new Object();
-    private static Executor EXECUTOR;
-
-    @NonNull
-    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-    private static Executor getExecutor() {
-        synchronized (LOCK_EXECUTOR) {
-            if (EXECUTOR == null) {
-                EXECUTOR = Executors.newFixedThreadPool(1);
-                if (EXECUTOR instanceof ThreadPoolExecutor) {
-                    ((ThreadPoolExecutor) EXECUTOR).setKeepAliveTime(30, TimeUnit.SECONDS);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-                        ((ThreadPoolExecutor) EXECUTOR).allowCoreThreadTimeOut(true);
-                    } else {
-                        ((ThreadPoolExecutor) EXECUTOR).setCorePoolSize(0);
-                    }
-                }
-            }
-
-            return EXECUTOR;
-        }
     }
 }
