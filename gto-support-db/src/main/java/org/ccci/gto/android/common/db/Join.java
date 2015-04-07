0db73d76d39d8d3401bea0d29efcc8d56b68cf17
Daniel Frett <daniel.frett@ccci.org>
2015-04-07 15:19:10 -0400

    made Joins Parcelable

--- gto-support-db/src/main/java/org/ccci/gto/android/common/db/Join.java
+++ gto-support-db/src/main/java/org/ccci/gto/android/common/db/Join.java
@@ -1,9 +1,12 @@
 package org.ccci.gto.android.common.db;
 
+import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-public final class Join<S, T> {
+public final class Join<S, T> implements Parcelable {
     public static final Join[] NO_JOINS = new Join[0];
 
     @Nullable
@@ -28,6 +31,18 @@ public final class Join<S, T> {
         mTarget = target;
         mType = type != null ? type : "";
         mOn = on != null ? on : "";
+    }
+
+    @SuppressWarnings("unchecked")
+    Join(@NonNull final Parcel in, @Nullable final ClassLoader loader) {
+        mBase = in.readParcelable(loader);
+        try {
+            mTarget = (Class<T>) Class.forName(in.readString(), true, loader);
+        } catch (final ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        mType = in.readString();
+        mOn = in.readString();
     }
 
     @NonNull
@@ -61,5 +76,52 @@ public final class Join<S, T> {
             sb.append(" ON ").append(mOn);
         }
         return sb.toString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull final Parcel out, int flags) {
+        out.writeParcelable(mBase, flags);
+        out.writeString(mTarget.getName());
+        out.writeString(mType);
+        out.writeString(mOn);
+    }
+
+    public static final Creator<Join> CREATOR;
+    static {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR2) {
+            CREATOR = new Creator<Join>() {
+                @Override
+                public Join createFromParcel(@NonNull final Parcel in) {
+                    return new Join(in, null);
+                }
+
+                @Override
+                public Join[] newArray(final int size) {
+                    return new Join[size];
+                }
+            };
+        } else {
+            CREATOR = new ClassLoaderCreator<Join>() {
+                @Override
+                public Join createFromParcel(@NonNull final Parcel in, @Nullable final ClassLoader loader) {
+                    return new Join(in, loader);
+                }
+
+                @Override
+                public Join createFromParcel(@NonNull final Parcel in) {
+                    return createFromParcel(in, null);
+                }
+
+                @Override
+                public Join[] newArray(final int size) {
+                    return new Join[size];
+                }
+            };
+        }
     }
 }
