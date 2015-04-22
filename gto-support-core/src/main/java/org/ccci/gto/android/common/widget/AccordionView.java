e8b8ed88eaa09f80b15a18210d77a8ff65322fdd
Daniel Frett <daniel.frett@ccci.org>
2015-04-22 15:30:22 -0400

    add animations to AccordionView

--- gto-support-core/src/main/java/org/ccci/gto/android/common/widget/AccordionView.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/widget/AccordionView.java
@@ -3,17 +3,25 @@ package org.ccci.gto.android.common.widget;
 import static org.ccci.gto.android.common.widget.AccordionView.Adapter.POSITION_NONE;
 import static org.ccci.gto.android.common.widget.AccordionView.Adapter.POSITION_UNCHANGED;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.database.DataSetObservable;
 import android.database.DataSetObserver;
+import android.os.Build;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+
+import org.ccci.gto.android.common.animation.SimpleAnimatorListener;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -21,12 +29,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccordionView extends LinearLayout {
-    static final int STATE_CLOSED = 0;
+    public static final int STATE_CLOSED = 0;
     static final int STATE_OPENING = 1;
     static final int STATE_CLOSING = 2;
-    static final int STATE_OPEN = 3;
+    public static final int STATE_OPEN = 3;
 
     static final int HEIGHT_UNKNOWN = -1;
+
+    static final long DEFAULT_DURATION = 300;
+
+    static final int DEFAULT_INNER_HEIGHT = LayoutParams.MATCH_PARENT;
+    static final int DEFAULT_INNER_WIDTH = LayoutParams.MATCH_PARENT;
+    static final int DEFAULT_OUTER_HEIGHT = 0;
+    static final float DEFAULT_OUTER_WEIGHT = 1;
 
     @IntDef({STATE_CLOSED, STATE_OPENING, STATE_CLOSING, STATE_OPEN})
     @Retention(RetentionPolicy.SOURCE)
@@ -43,10 +58,16 @@ public class AccordionView extends LinearLayout {
     private ViewHolder[] mSections = new ViewHolder[0];
     @Nullable
     private ViewHolder mActiveSection;
+    private boolean mAnimate = true;
+    @Nullable
+    private AnimationManager mAnimationManager = null;
 
     public AccordionView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
         setOrientation(VERTICAL);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setAnimationManager(new AnimatorAnimationManager());
+        }
     }
 
     @Override
@@ -108,6 +129,15 @@ public class AccordionView extends LinearLayout {
 
         // update the AccordionView because the dataset changed
         dataSetChanged(old);
+    }
+
+    public void setAnimationManager(@Nullable final AnimationManager manager) {
+        mAnimationManager = manager;
+    }
+
+    @Nullable
+    public AnimationManager getAnimationManager() {
+        return mAnimationManager;
     }
 
     /**
@@ -186,8 +216,8 @@ public class AccordionView extends LinearLayout {
         contentOuter.setLayoutParams(generateDefaultLayoutParams());
         final FrameLayout contentInner = new FrameLayout(getContext());
         contentOuter.addView(contentInner);
-        contentInner.getLayoutParams().height = LayoutParams.MATCH_PARENT;
-        contentInner.getLayoutParams().width = LayoutParams.MATCH_PARENT;
+        contentInner.getLayoutParams().height = DEFAULT_INNER_HEIGHT;
+        contentInner.getLayoutParams().width = DEFAULT_INNER_WIDTH;
 
         // create new ViewHolder
         final ViewHolder holder = mAdapter.onCreateViewHolder(header, contentInner, position);
@@ -201,16 +231,15 @@ public class AccordionView extends LinearLayout {
         holder.mHeader.setLayoutParams(headerLp);
         final LayoutParams contentLp = upgradeLayoutParams(holder.mContentOuter.getLayoutParams());
         contentLp.mHolder = holder;
-        contentLp.height = 0;
-        contentLp.weight = 1;
+        contentLp.height = DEFAULT_OUTER_HEIGHT;
+        contentLp.weight = DEFAULT_OUTER_WEIGHT;
         holder.mContentOuter.setLayoutParams(contentLp);
 
         // set header OnClick handler
         holder.mHeader.setOnClickListener(mHeaderOnClick);
 
         // close section by default
-        holder.mState = STATE_CLOSED;
-        holder.mContentOuter.setVisibility(GONE);
+        holder.changeState(STATE_CLOSED);
 
         // return new ViewHolder
         return holder;
@@ -224,20 +253,19 @@ public class AccordionView extends LinearLayout {
 
         // start closing the previous active section
         if (mActiveSection != null) {
-            // update inner height if the section was OPEN
+            // update last known height if the section was OPEN
             if (mActiveSection.mState == STATE_OPEN) {
-                mActiveSection.mContentInner.getLayoutParams().height = mActiveSection.mContentOuter.getHeight();
+                mActiveSection.mLastKnownHeight = mActiveSection.mContentOuter.getHeight();
             }
 
             // mark this section as closing
-            mActiveSection.mState = STATE_CLOSING;
+            mActiveSection.changeState(STATE_CLOSING);
             mActiveSection = null;
         }
 
         // mark the specified section as opening
+        section.changeState(STATE_OPENING);
         mActiveSection = section;
-        mActiveSection.mState = STATE_OPENING;
-        mActiveSection.mContentOuter.setVisibility(VISIBLE);
 
         // generate a list of all closing sections
         final List<ViewHolder> closing = new ArrayList<>();
@@ -247,12 +275,54 @@ public class AccordionView extends LinearLayout {
             }
         }
 
-        // update state (no animation)
-        mActiveSection.mState = STATE_OPEN;
-        for (final ViewHolder holder : closing) {
-            holder.mContentOuter.setVisibility(GONE);
-            holder.mContentInner.getLayoutParams().height = LayoutParams.MATCH_PARENT;
-            holder.mState = STATE_CLOSED;
+        // handle animation
+        boolean animating = false;
+        if (mAnimate && mAnimationManager != null) {
+            // determine requested height of opening section
+            if (mActiveSection.mLastKnownHeight == HEIGHT_UNKNOWN) {
+                // backup the height of the opening section
+                final int height = mActiveSection.mContentOuter.getLayoutParams().height;
+
+                // set everything to it's final visibility state
+                for (final ViewHolder holder : closing) {
+                    holder.mContentOuter.setVisibility(GONE);
+                }
+                mActiveSection.mContentOuter.setVisibility(VISIBLE);
+                mActiveSection.mContentOuter.getLayoutParams().height = DEFAULT_OUTER_HEIGHT;
+                ((LinearLayout.LayoutParams) mActiveSection.mContentOuter.getLayoutParams()).weight =
+                        DEFAULT_OUTER_WEIGHT;
+
+                // trigger a measurement from this views parent (if possible, otherwise use this view)
+                final ViewParent parent = getParent();
+                final View view = parent instanceof View ? (View) parent : this;
+                view.measure(MeasureSpec.makeMeasureSpec(view.getMeasuredWidth(), MeasureSpec.EXACTLY),
+                             MeasureSpec.makeMeasureSpec(view.getMeasuredHeight(), MeasureSpec.EXACTLY));
+
+                // initialize the last known height to the measured height
+                mActiveSection.mLastKnownHeight = mActiveSection.mContentOuter.getMeasuredHeight();
+
+                // reset visibility of closing sections
+                for (final ViewHolder holder : closing) {
+                    holder.mContentOuter.setVisibility(VISIBLE);
+                }
+
+                // reset the state of the opening section
+                mActiveSection.mContentOuter.getLayoutParams().height = height;
+                mActiveSection.changeState(STATE_OPENING);
+            }
+
+            // animate the state transition
+            animating = mAnimationManager.animate(mActiveSection, closing.toArray(new ViewHolder[closing.size()]));
+        }
+
+        // just update state (not animating)
+        if (!animating) {
+            mActiveSection.changeState(STATE_OPEN);
+
+            // mark all closing sections as closed
+            for (final ViewHolder holder : closing) {
+                holder.changeState(STATE_CLOSED);
+            }
         }
     }
 
@@ -273,13 +343,9 @@ public class AccordionView extends LinearLayout {
         ViewHolder mHolder;
 
         public LayoutParams(@NonNull final LinearLayout.LayoutParams source) {
-//            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
             super((MarginLayoutParams) source);
             this.weight = source.weight;
             this.gravity = source.gravity;
-//            } else {
-//                super(source);
-//            }
         }
 
         public LayoutParams(@NonNull final MarginLayoutParams source) {
@@ -370,7 +436,58 @@ public class AccordionView extends LinearLayout {
         /* final */ ViewGroup mContentInner;
         @State
         int mState = STATE_CLOSED;
-        int mHeight = HEIGHT_UNKNOWN;
+        int mLastKnownHeight = HEIGHT_UNKNOWN;
+
+        void changeState(@State final int state) {
+            final ViewGroup.LayoutParams innerLp = mContentInner.getLayoutParams();
+            final LinearLayout.LayoutParams outerLp = (LinearLayout.LayoutParams) mContentOuter.getLayoutParams();
+            switch (state) {
+                case STATE_OPEN:
+                    mContentOuter.setVisibility(VISIBLE);
+                    innerLp.height = DEFAULT_INNER_HEIGHT;
+                    outerLp.height = DEFAULT_OUTER_HEIGHT;
+                    outerLp.weight = DEFAULT_OUTER_WEIGHT;
+                    break;
+                case STATE_CLOSING:
+                    mContentOuter.setVisibility(VISIBLE);
+                    innerLp.height = mLastKnownHeight;
+                    switch (mState) {
+                        case STATE_CLOSED:
+                            throw new IllegalStateException(
+                                    "Accordion section cannot transition from CLOSED to CLOSING");
+                        case STATE_OPEN:
+                            outerLp.height = mLastKnownHeight;
+                            break;
+                    }
+                    outerLp.weight = 0;
+                    break;
+                case STATE_CLOSED:
+                    mContentOuter.setVisibility(GONE);
+                    innerLp.height = DEFAULT_INNER_HEIGHT;
+                    outerLp.height = DEFAULT_OUTER_HEIGHT;
+                    outerLp.weight = DEFAULT_OUTER_WEIGHT;
+                    break;
+                case STATE_OPENING:
+                    mContentOuter.setVisibility(VISIBLE);
+                    innerLp.height = mLastKnownHeight;
+                    switch (mState) {
+                        case STATE_OPEN:
+                            throw new IllegalStateException("Accordion section cannot transition from OPEN to OPENING");
+                        case STATE_CLOSED:
+                            outerLp.height = 0;
+                            break;
+                    }
+                    outerLp.weight = 0;
+                    break;
+            }
+
+            // set updated LayoutParams, this triggers a fresh layout
+            mContentInner.setLayoutParams(innerLp);
+            mContentOuter.setLayoutParams(outerLp);
+
+            // update the state
+            mState = state;
+        }
     }
 
     private class AccordionHeaderOnClickListener implements OnClickListener {
@@ -381,6 +498,137 @@ public class AccordionView extends LinearLayout {
                 final ViewHolder section = ((LayoutParams) lp).mHolder;
                 if (section != null) {
                     showSection(section);
+                }
+            }
+        }
+    }
+
+    public interface AnimationManager {
+        boolean animate(@NonNull ViewHolder opening, @NonNull ViewHolder... closing);
+
+        void setDuration(long duration);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class AnimatorAnimationManager implements AnimationManager {
+        @Nullable
+        private Animator mCurrentAnimation;
+
+        private long mDuration = DEFAULT_DURATION;
+
+        @Override
+        public boolean animate(@NonNull final ViewHolder opening, @NonNull final ViewHolder... closing) {
+            if (mCurrentAnimation != null) {
+                mCurrentAnimation.cancel();
+            }
+
+            // build the animation
+            final Animator animation = buildAnimation(opening, closing);
+
+            // listen for completion to reset the current animation
+            animation.addListener(new SimpleAnimatorListener() {
+                @Override
+                public void onAnimationEnd(@NonNull final Animator animation) {
+                    if (animation == mCurrentAnimation) {
+                        mCurrentAnimation = null;
+                    }
+                }
+            });
+            mCurrentAnimation = animation;
+
+            // start the animation
+            animation.start();
+
+            return true;
+        }
+
+        public final void setDuration(final long duration) {
+            mDuration = duration;
+        }
+
+        @NonNull
+        protected Animator buildAnimation(@NonNull final ViewHolder opening, @NonNull final ViewHolder... closing) {
+            final AnimatorSet animation = new AnimatorSet();
+            animation.play(getOpeningAnimator(opening));
+            for (final ViewHolder holder : closing) {
+                animation.play(getClosingAnimator(holder));
+            }
+            animation.setDuration(mDuration);
+            return animation;
+        }
+
+        @NonNull
+        protected Animator getOpeningAnimator(@NonNull final ViewHolder holder) {
+            final ValueAnimator animator =
+                    ValueAnimator.ofInt(holder.mContentOuter.getLayoutParams().height, holder.mLastKnownHeight);
+            final DefaultOpeningAnimatorListener listener = new DefaultOpeningAnimatorListener(holder);
+            animator.addListener(listener);
+            animator.addUpdateListener(listener);
+            return animator;
+        }
+
+        @NonNull
+        protected Animator getClosingAnimator(@NonNull final ViewHolder holder) {
+            final ValueAnimator animator = ValueAnimator.ofInt(holder.mContentOuter.getLayoutParams().height, 0);
+            final DefaultClosingAnimatorListener listener = new DefaultClosingAnimatorListener(holder);
+            animator.addListener(listener);
+            animator.addUpdateListener(listener);
+            return animator;
+        }
+
+        public abstract static class AnimatorListener extends SimpleAnimatorListener {
+            @NonNull
+            protected final ViewHolder mHolder;
+            @State
+            private final int mTargetState;
+            private boolean mCanceled = false;
+
+            protected AnimatorListener(@NonNull final ViewHolder holder, @State final int targetState) {
+                mHolder = holder;
+                mTargetState = targetState;
+            }
+
+            @Override
+            public void onAnimationCancel(@NonNull final Animator animation) {
+                mCanceled = true;
+            }
+
+            @Override
+            public void onAnimationEnd(@NonNull final Animator animation) {
+                if (!mCanceled) {
+                    mHolder.changeState(mTargetState);
+                }
+            }
+        }
+
+        private static final class DefaultOpeningAnimatorListener extends AnimatorListener
+                implements ValueAnimator.AnimatorUpdateListener {
+            public DefaultOpeningAnimatorListener(@NonNull final ViewHolder holder) {
+                super(holder, STATE_OPEN);
+            }
+
+            @Override
+            public void onAnimationUpdate(@NonNull final ValueAnimator animation) {
+                final Object value = animation.getAnimatedValue();
+                if (value instanceof Integer) {
+                    mHolder.mContentOuter.getLayoutParams().height = (Integer) value;
+                    mHolder.mContentOuter.requestLayout();
+                }
+            }
+        }
+
+        private static final class DefaultClosingAnimatorListener extends AnimatorListener
+                implements ValueAnimator.AnimatorUpdateListener {
+            public DefaultClosingAnimatorListener(@NonNull final ViewHolder holder) {
+                super(holder, STATE_CLOSED);
+            }
+
+            @Override
+            public void onAnimationUpdate(@NonNull final ValueAnimator animation) {
+                final Object value = animation.getAnimatedValue();
+                if (value instanceof Integer) {
+                    mHolder.mContentOuter.getLayoutParams().height = (Integer) value;
+                    mHolder.mContentOuter.requestLayout();
                 }
             }
         }
