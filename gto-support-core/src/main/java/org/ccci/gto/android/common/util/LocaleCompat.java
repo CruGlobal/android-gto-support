8a23024eec1f58e8678e8b68a56a7baa09689e5d
Daniel Frett <daniel.frett@ccci.org>
2015-09-09 16:52:59 -0400

    add compat method for Locale.forLanguageTag

--- gto-support-core/src/main/java/org/ccci/gto/android/common/util/LocaleCompat.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/util/LocaleCompat.java
@@ -9,31 +9,64 @@ import java.util.Locale;
 
 public class LocaleCompat {
     @NonNull
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public static String toLanguageTag(@NonNull final Locale locale) {
+    public static Locale forLanguageTag(@NonNull final String tag) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            return toLanguageTagPreLollipop(locale);
+            return FroyoLocale.forLanguageTag(tag);
         } else {
-            return locale.toLanguageTag();
+            return LollipopLocale.forLanguageTag(tag);
         }
     }
 
     @NonNull
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static String toLanguageTag(@NonNull final Locale locale) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return FroyoLocale.toLanguageTag(locale);
+        } else {
+            return LollipopLocale.toLanguageTag(locale);
+        }
+    }
+
     @VisibleForTesting
-    static String toLanguageTagPreLollipop(@NonNull final Locale locale) {
-        // just perform simple generation
-        final StringBuilder sb = new StringBuilder(5);
-
-        // append the language
-        sb.append(locale.getLanguage().toLowerCase(Locale.US));
-
-        // append the region
-        final String region = locale.getCountry();
-        if (region != null && region.length() > 0) {
-            sb.append('-').append(region.toUpperCase(Locale.US));
+    static final class FroyoLocale {
+        @NonNull
+        static Locale forLanguageTag(@NonNull final String tag) {
+            // XXX: we are ignoring grandfathered tags unless we really need that support
+            final String[] subtags = tag.split("-");
+            final String language = subtags[0] != null ? subtags[0] : "";
+            final String region = subtags.length > 1 && subtags[1] != null ? subtags[1] : "";
+            return new Locale(language, region);
         }
 
-        // output the language tag
-        return sb.toString();
+        @NonNull
+        static String toLanguageTag(@NonNull final Locale locale) {
+            // just perform simple generation
+            final StringBuilder sb = new StringBuilder(5);
+
+            // append the language
+            sb.append(locale.getLanguage().toLowerCase(Locale.US));
+
+            // append the region
+            final String region = locale.getCountry();
+            if (region != null && region.length() > 0) {
+                sb.append('-').append(region.toUpperCase(Locale.US));
+            }
+
+            // output the language tag
+            return sb.toString();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    static final class LollipopLocale {
+        @NonNull
+        static Locale forLanguageTag(@NonNull final String tag) {
+            return Locale.forLanguageTag(tag);
+        }
+
+        @NonNull
+        static String toLanguageTag(@NonNull final Locale locale) {
+            return locale.toLanguageTag();
+        }
     }
 }
--- gto-support-core/src/test/java/org/ccci/gto/android/common/util/LocaleCompatTest.java
+++ gto-support-core/src/test/java/org/ccci/gto/android/common/util/LocaleCompatTest.java
@@ -2,14 +2,61 @@ package org.ccci.gto.android.common.util;
 
 import static junit.framework.Assert.assertEquals;
 
+import com.google.common.collect.ImmutableMap;
+
+import org.ccci.gto.android.common.util.LocaleCompat.FroyoLocale;
+import org.ccci.gto.android.common.util.LocaleCompat.LollipopLocale;
 import org.junit.Test;
 
 import java.util.Locale;
+import java.util.Map;
 
 public class LocaleCompatTest {
+    private static final Map<Locale, String> LANGUAGETAGS;
+    static {
+        final ImmutableMap.Builder<Locale, String> builder = ImmutableMap.builder();
+        builder.put(Locale.US, "en-US");
+        builder.put(new Locale("EN", "gb"), "en-GB");
+        builder.put(Locale.ENGLISH, "en");
+        builder.put(Locale.SIMPLIFIED_CHINESE, "zh-CN");
+        LANGUAGETAGS = builder.build();
+    }
+
+    private static final Map<String, Locale> LOCALES;
+    static {
+        final ImmutableMap.Builder<String, Locale> builder = ImmutableMap.builder();
+        builder.put("en-US", Locale.US);
+        builder.put("en-GB", Locale.UK);
+        builder.put("en", Locale.ENGLISH);
+        builder.put("EN-us", Locale.US);
+        LOCALES = builder.build();
+    }
+
     @Test
-    public void testToLanguageTagPreLollipop() throws Exception {
-        assertEquals("en-US", LocaleCompat.toLanguageTagPreLollipop(Locale.US));
-        assertEquals("zh-CN", LocaleCompat.toLanguageTagPreLollipop(Locale.SIMPLIFIED_CHINESE));
+    public void testFroyoLocaleForLanguageTag() throws Exception {
+        for (final Map.Entry<String, Locale> entry : LOCALES.entrySet()) {
+            assertEquals(entry.getValue(), FroyoLocale.forLanguageTag(entry.getKey()));
+        }
+    }
+
+    @Test
+    public void testFroyoLocaleToLanguageTag() throws Exception {
+        for (final Map.Entry<Locale, String> entry : LANGUAGETAGS.entrySet()) {
+            assertEquals(entry.getValue(), FroyoLocale.toLanguageTag(entry.getKey()));
+        }
+    }
+
+    @Test
+    public void testLollipopLocaleForLanguageTag() throws Exception {
+        for (final Map.Entry<String, Locale> entry : LOCALES.entrySet()) {
+            assertEquals(entry.getValue(), LollipopLocale.forLanguageTag(entry.getKey()));
+        }
+    }
+
+    @Test
+    public void testLollipopLocaleToLanguageTag() throws Exception {
+        for (final Map.Entry<Locale, String> entry : LANGUAGETAGS.entrySet()) {
+            assertEquals(entry.getValue(), LollipopLocale.toLanguageTag(entry.getKey()));
+        }
     }
 }
