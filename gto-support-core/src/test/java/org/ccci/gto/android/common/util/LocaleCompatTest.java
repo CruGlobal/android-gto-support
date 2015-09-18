4d00d769d895a2c76f66868ed64275b408fcdb90
Daniel Frett <daniel.frett@ccci.org>
2015-09-18 17:43:37 -0400

    add LocaleCompat.getFallbacks method for resolving locale fallbacks

--- gto-support-core/src/test/java/org/ccci/gto/android/common/util/LocaleCompatTest.java
+++ gto-support-core/src/test/java/org/ccci/gto/android/common/util/LocaleCompatTest.java
@@ -1,17 +1,31 @@
 package org.ccci.gto.android.common.util;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import com.google.common.collect.ImmutableMap;
 
-import org.ccci.gto.android.common.util.LocaleCompat.FroyoLocale;
-import org.ccci.gto.android.common.util.LocaleCompat.LollipopLocale;
+import org.ccci.gto.android.common.util.LocaleCompat.Compat;
+import org.ccci.gto.android.common.util.LocaleCompat.FroyoCompat;
+import org.ccci.gto.android.common.util.LocaleCompat.LollipopCompat;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Locale;
 import java.util.Map;
 
+@RunWith(Parameterized.class)
 public class LocaleCompatTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {{new FroyoCompat()}, {new LollipopCompat()}});
+    }
+
+    private final Compat compat;
+
     private static final Map<Locale, String> LANGUAGETAGS;
     static {
         final ImmutableMap.Builder<Locale, String> builder = ImmutableMap.builder();
@@ -32,31 +46,35 @@ public class LocaleCompatTest {
         LOCALES = builder.build();
     }
 
+    public LocaleCompatTest(final Compat compat) {
+        this.compat = compat;
+    }
+
     @Test
-    public void testFroyoLocaleForLanguageTag() throws Exception {
+    public void testForLanguageTag() throws Exception {
         for (final Map.Entry<String, Locale> entry : LOCALES.entrySet()) {
-            assertEquals(entry.getValue(), FroyoLocale.forLanguageTag(entry.getKey()));
+            assertEquals(entry.getValue(), compat.forLanguageTag(entry.getKey()));
         }
     }
 
     @Test
-    public void testFroyoLocaleToLanguageTag() throws Exception {
+    public void testToLanguageTag() throws Exception {
         for (final Map.Entry<Locale, String> entry : LANGUAGETAGS.entrySet()) {
-            assertEquals(entry.getValue(), FroyoLocale.toLanguageTag(entry.getKey()));
+            assertEquals(entry.getValue(), compat.toLanguageTag(entry.getKey()));
         }
     }
 
     @Test
-    public void testLollipopLocaleForLanguageTag() throws Exception {
-        for (final Map.Entry<String, Locale> entry : LOCALES.entrySet()) {
-            assertEquals(entry.getValue(), LollipopLocale.forLanguageTag(entry.getKey()));
-        }
-    }
+    public void testGetFallbacks() throws Exception {
+        assertArrayEquals(new Locale[] {Locale.US, Locale.ENGLISH}, compat.getFallbacks(Locale.US));
+        assertArrayEquals(new Locale[] {Locale.ENGLISH}, compat.getFallbacks(Locale.ENGLISH));
 
-    @Test
-    public void testLollipopLocaleToLanguageTag() throws Exception {
-        for (final Map.Entry<Locale, String> entry : LANGUAGETAGS.entrySet()) {
-            assertEquals(entry.getValue(), LollipopLocale.toLanguageTag(entry.getKey()));
-        }
+        // test batch fallback resolution
+        assertArrayEquals(new Locale[] {Locale.US, Locale.ENGLISH},
+                          LocaleCompat.getFallbacks(Locale.US, Locale.ENGLISH));
+        assertArrayEquals(new Locale[] {Locale.ENGLISH, Locale.US},
+                          LocaleCompat.getFallbacks(Locale.ENGLISH, Locale.US));
+        assertArrayEquals(new Locale[] {Locale.US, Locale.ENGLISH, Locale.CANADA, Locale.CANADA_FRENCH, Locale.FRENCH},
+                          LocaleCompat.getFallbacks(Locale.US, Locale.CANADA, Locale.CANADA_FRENCH));
     }
 }
