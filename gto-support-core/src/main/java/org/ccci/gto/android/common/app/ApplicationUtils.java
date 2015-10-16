d1bf7bbf7e4ae3697cbce1e2e28d59637047d443
Daniel Frett <daniel.frett@ccci.org>
2015-10-16 14:10:57 -0400

    add isDebuggable method

---
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/app/ApplicationUtils.java
@@ -0,0 +1,17 @@
+package org.ccci.gto.android.common.app;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.support.annotation.NonNull;
+
+public class ApplicationUtils {
+    /**
+     * replacement for BuildConfig.DEBUG to allow libraries to check final app debug mode.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isDebuggable(@NonNull final Context context) {
+        return 0 != (context.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE);
+    }
+}
