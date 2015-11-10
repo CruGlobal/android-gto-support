081c1db5dd5c7ae23ffa6769732a111b5d7dfa4a
Daniel Frett <daniel.frett@ccci.org>
2015-11-10 11:13:20 -0500

    implement a load more scroll listener for RecyclerViews

---
+++ gto-support-recyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/listener/LoadMoreOnScrollListener.java
@@ -0,0 +1,55 @@
+package org.ccci.gto.android.common.recyclerview.listener;
+
+import static android.support.v7.widget.RecyclerView.NO_POSITION;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.LayoutManager;
+
+public abstract class LoadMoreOnScrollListener extends RecyclerView.OnScrollListener {
+    private boolean mLoading = false;
+    private int mThreshold = 5;
+
+    public void setThreshold(final int threshold) {
+        mThreshold = threshold;
+    }
+
+    @Override
+    public void onScrolled(@NonNull final RecyclerView view, final int dx, final int dy) {
+        // short-circuit if we are currently loading more data
+        if (mLoading) {
+            return;
+        }
+
+        // short-circuit if there isn't a valid layout manager
+        final LayoutManager layoutManager = view.getLayoutManager();
+        if (layoutManager == null) {
+            return;
+        }
+
+        // determine the last visible item
+        final int lastVisible;
+        if (layoutManager instanceof LinearLayoutManager) {
+            lastVisible = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+        } else {
+            lastVisible = getLastVisiblePosition(layoutManager);
+        }
+
+        // have we passed the threshold to load more data?
+        if (lastVisible + mThreshold > layoutManager.getItemCount() - 1) {
+            mLoading = true;
+            onLoadMore();
+        }
+    }
+
+    protected int getLastVisiblePosition(@NonNull final LayoutManager layoutManager) {
+        return NO_POSITION;
+    }
+
+    protected abstract void onLoadMore();
+
+    public final void doneLoading() {
+        mLoading = false;
+    }
+}
