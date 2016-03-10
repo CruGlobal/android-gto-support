d1d408e4f6b5d6e6b2b1198c40bcd35dc5c038cb
Daniel Frett <daniel.frett@ccci.org>
2016-03-10 15:45:28 -0500

    add a util to define global network interceptors

---
+++ gto-support-api-okhttp3/src/main/java/org/ccci/gto/android/common/api/okhttp3/util/OkHttpClientUtil.java
@@ -0,0 +1,25 @@
+package org.ccci.gto.android.common.api.okhttp3.util;
+
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+
+public class OkHttpClientUtil {
+    private static final List<Interceptor> GLOBAL_NETWORK_INTERCEPTORS = new ArrayList<>();
+
+    public static void addGlobalNetworkInterceptor(@NonNull final Interceptor interceptor) {
+        GLOBAL_NETWORK_INTERCEPTORS.add(interceptor);
+    }
+
+    @NonNull
+    public static OkHttpClient.Builder attachGlobalInterceptors(@NonNull final OkHttpClient.Builder builder) {
+        for (final Interceptor interceptor : GLOBAL_NETWORK_INTERCEPTORS) {
+            builder.addNetworkInterceptor(interceptor);
+        }
+        return builder;
+    }
+}
