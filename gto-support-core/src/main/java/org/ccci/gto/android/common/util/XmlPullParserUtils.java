45626a242ab6534e2ee42e35519516c83a16e4ba
Daniel Frett <daniel.frett@ccci.org>
2016-04-08 18:29:26 -0400

    create a requireAnyName method to aid in verifying that the XmlPullParser is on one of many supported nodes

--- gto-support-core/src/main/java/org/ccci/gto/android/common/util/XmlPullParserUtils.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/util/XmlPullParserUtils.java
@@ -1,6 +1,7 @@
 package org.ccci.gto.android.common.util;
 
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -49,5 +50,27 @@ public class XmlPullParserUtils {
                     break;
             }
         }
+    }
+
+    /**
+     * Similar to {@link XmlPullParser#require(int, String, String)}, but tests to see if the current node name matches
+     * any of the provided names.
+     *
+     * @param parser
+     * @param names
+     * @throws XmlPullParserException
+     */
+    public static void requireAnyName(@NonNull final XmlPullParser parser, @NonNull final String... names)
+            throws XmlPullParserException {
+        final String nodeName = parser.getName();
+        if (nodeName != null) {
+            for (final String name : names) {
+                if (name != null && name.equals(nodeName)) {
+                    return;
+                }
+            }
+        }
+
+        throw new XmlPullParserException("expected: " + TextUtils.join("|", names), parser, null);
     }
 }
