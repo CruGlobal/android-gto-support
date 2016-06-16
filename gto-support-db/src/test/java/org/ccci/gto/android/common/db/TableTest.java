66a8461ab073a4538610cfb9a0422badeffa9f53
Daniel Frett <daniel.frett@ccci.org>
2016-06-16 19:51:30 -0400

    update TableTest to mock TextUtils

--- gto-support-db/src/test/java/org/ccci/gto/android/common/db/TableTest.java
+++ gto-support-db/src/test/java/org/ccci/gto/android/common/db/TableTest.java
@@ -1,25 +1,43 @@
 package org.ccci.gto.android.common.db;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
+import org.ccci.gto.android.common.testing.CommonMocks;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({TextUtils.class})
 public class TableTest {
-    private final TestDao dao = new TestDao();
+    @NonNull
+    private TestDao getDao() {
+        return new TestDao();
+    }
+
+    @Before
+    public void setup() throws Exception {
+        CommonMocks.mockTextUtils();
+    }
 
     @Test
     public void testBuildSqlName() throws Exception {
         final Table<Obj1> t1 = Table.forClass(Obj1.class);
         final Table<Obj2> t2 = Table.forClass(Obj2.class);
 
-        assertEquals(Obj1.TABLE_NAME, t1.sqlTable(dao));
-        assertEquals(Obj1.TABLE_NAME + " AS a", t1.as("a").sqlTable(dao));
-        assertEquals(Obj1.TABLE_NAME + " AS abcde", t1.as("abcde").sqlTable(dao));
-        assertNotEquals(Obj2.TABLE_NAME, t1.sqlTable(dao));
+        assertEquals(Obj1.TABLE_NAME, t1.sqlTable(getDao()));
+        assertEquals(Obj1.TABLE_NAME + " AS a", t1.as("a").sqlTable(getDao()));
+        assertEquals(Obj1.TABLE_NAME + " AS abcde", t1.as("abcde").sqlTable(getDao()));
+        assertNotEquals(Obj2.TABLE_NAME, t1.sqlTable(getDao()));
 
-        assertEquals(Obj2.TABLE_NAME, t2.sqlTable(dao));
-        assertEquals(Obj2.TABLE_NAME + " AS b", t2.as("b").sqlTable(dao));
+        assertEquals(Obj2.TABLE_NAME, t2.sqlTable(getDao()));
+        assertEquals(Obj2.TABLE_NAME + " AS b", t2.as("b").sqlTable(getDao()));
     }
 
     static class Obj1 {
