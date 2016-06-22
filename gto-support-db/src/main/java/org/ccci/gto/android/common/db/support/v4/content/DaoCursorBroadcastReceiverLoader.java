f4d9b94c70689fa4d5debfd460c0525acafcb713
Daniel Frett <daniel.frett@ccci.org>
2016-06-22 15:04:20 -0400

    have DaoCursorBroadcastReceiverLoader inherit from DaoCursorLoader

--- gto-support-db/src/main/java/org/ccci/gto/android/common/db/support/v4/content/DaoCursorBroadcastReceiverLoader.java
+++ gto-support-db/src/main/java/org/ccci/gto/android/common/db/support/v4/content/DaoCursorBroadcastReceiverLoader.java
@@ -1,39 +1,26 @@
 package org.ccci.gto.android.common.db.support.v4.content;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.database.Cursor;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import org.ccci.gto.android.common.db.AbstractDao;
 import org.ccci.gto.android.common.db.Expression;
-import org.ccci.gto.android.common.db.Join;
-import org.ccci.gto.android.common.db.Query;
 import org.ccci.gto.android.common.db.Table;
-import org.ccci.gto.android.common.support.v4.content.CursorBroadcastReceiverLoader;
-import org.ccci.gto.android.common.util.BundleUtils;
+import org.ccci.gto.android.common.support.v4.content.BroadcastReceiverLoaderHelper;
+import org.ccci.gto.android.common.util.BundleCompat;
 
-import static org.ccci.gto.android.common.db.AbstractDao.ARG_DISTINCT;
-import static org.ccci.gto.android.common.db.AbstractDao.ARG_JOINS;
-import static org.ccci.gto.android.common.db.AbstractDao.ARG_ORDER_BY;
-import static org.ccci.gto.android.common.db.AbstractDao.ARG_PROJECTION;
 import static org.ccci.gto.android.common.db.AbstractDao.ARG_WHERE;
 import static org.ccci.gto.android.common.db.AbstractDao.ARG_WHERE_ARGS;
 import static org.ccci.gto.android.common.db.AbstractDao.bindValues;
 
-public class DaoCursorBroadcastReceiverLoader<T> extends CursorBroadcastReceiverLoader {
+public class DaoCursorBroadcastReceiverLoader<T> extends DaoCursorLoader<T>
+        implements BroadcastReceiverLoaderHelper.Interface {
     @NonNull
-    protected final AbstractDao mDao;
-
-    private boolean mDistinct = false;
-    @NonNull
-    private final Table<T> mFrom;
-    @NonNull
-    @SuppressWarnings("unchecked")
-    private Join<T, ?>[] mJoins = Join.NO_JOINS;
-    @Nullable
-    private Expression mWhere;
+    private final BroadcastReceiverLoaderHelper mHelper;
 
     public DaoCursorBroadcastReceiverLoader(@NonNull final Context context, @NonNull final AbstractDao dao,
                                             @NonNull final Class<T> type, @Nullable final Bundle args) {
@@ -43,66 +30,48 @@ public class DaoCursorBroadcastReceiverLoader<T> extends CursorBroadcastReceiver
     @SuppressWarnings("unchecked")
     public DaoCursorBroadcastReceiverLoader(@NonNull final Context context, @NonNull final AbstractDao dao,
                                             @NonNull final Table<T> from, @Nullable final Bundle args) {
-        super(context);
-        mDao = dao;
+        super(context, dao, from, args);
+        mHelper = new BroadcastReceiverLoaderHelper(this);
 
-        mFrom = from;
+        // Handle deprecated WHERE as String
         if (args != null) {
-            setDistinct(args.getBoolean(ARG_DISTINCT, false));
-            setJoins(BundleUtils.getParcelableArray(args, ARG_JOINS, Join.class));
-            setProjection(args.getStringArray(ARG_PROJECTION));
-            final Object where = args.get(ARG_WHERE);
-            if (where instanceof Expression) {
-                setWhere((Expression) where);
-            } else if (where instanceof String) {
-                // where wasn't an Expression, maybe it's a string?
-                setWhere((String) where, args.getStringArray(ARG_WHERE_ARGS));
+            final String where = BundleCompat.getString(args, ARG_WHERE, null);
+            if (where != null) {
+                setWhere(where, args.getStringArray(ARG_WHERE_ARGS));
             }
-            setSortOrder(args.getString(ARG_ORDER_BY));
-        } else {
-            setDistinct(false);
-            setJoins(null);
-            setProjection(null);
-            setWhere(null);
-            setSortOrder(null);
         }
     }
 
-    @Nullable
+    /* BEGIN lifecycle */
+
     @Override
-    protected final Cursor getCursor() {
-        // build query
-        return mDao.getCursor(Query.select(mFrom).distinct(isDistinct()).joins(getJoins()).projection(getProjection())
-                                      .where(getWhere()).orderBy(getSortOrder()));
-    }
-
-    public void setDistinct(final boolean distinct) {
-        mDistinct = distinct;
-    }
-
-    public boolean isDistinct() {
-        return mDistinct;
-    }
-
-    @SuppressWarnings("unchecked")
-    public void setJoins(@Nullable final Join<T, ?>[] joins) {
-        mJoins = joins != null ? joins : Join.NO_JOINS;
-    }
-
-    @NonNull
-    public Join<T, ?>[] getJoins() {
-        return mJoins;
+    protected void onStartLoading() {
+        super.onStartLoading();
+        mHelper.onStartLoading();
     }
 
     @Override
-    public void setProjection(@Nullable final String[] projection) {
-        super.setProjection(projection != null ? projection : mDao.getFullProjection(mFrom));
+    protected void onAbandon() {
+        super.onAbandon();
+        mHelper.onAbandon();
     }
 
-    @NonNull
-    public String[] getProjection() {
-        final String[] projection = super.getProjection();
-        return projection != null ? projection : mDao.getFullProjection(mFrom);
+    @Override
+    protected void onReset() {
+        super.onReset();
+        mHelper.onReset();
+    }
+
+    /* END lifecycle */
+
+    @Override
+    public final void addIntentFilter(@NonNull final IntentFilter filter) {
+        mHelper.addIntentFilter(filter);
+    }
+
+    @Override
+    public final void setBroadcastReceiver(@Nullable final BroadcastReceiver receiver) {
+        mHelper.setBroadcastReceiver(receiver);
     }
 
     /**
@@ -119,14 +88,5 @@ public class DaoCursorBroadcastReceiverLoader<T> extends CursorBroadcastReceiver
     @Deprecated
     public void setWhere(@Nullable final String where, @Nullable final String... args) {
         setWhere(where != null ? Expression.raw(where, args) : null);
-    }
-
-    public void setWhere(@Nullable final Expression where) {
-        mWhere = where;
-    }
-
-    @Nullable
-    public Expression getWhere() {
-        return mWhere;
     }
 }
