3d92ffb6b3be485268f7e205f72aceded1887e26
Daniel Frett <daniel.frett@ccci.org>
2016-06-30 11:39:56 -0400

    fix checkstyle error

--- gto-support-db/src/main/java/org/ccci/gto/android/common/db/AbstractDao.java
+++ gto-support-db/src/main/java/org/ccci/gto/android/common/db/AbstractDao.java
@@ -488,7 +488,8 @@ public abstract class AbstractDao {
      * {@code sample} object. The objects actually updated can be restricted via the {@code where} expression.
      *
      * @param type       the type of objects being updated
-     * @param where      a where clause that restricts which objects get updated. If this is null all objects are updated.
+     * @param where      a where clause that restricts which objects get updated. If this is null all objects are
+     *                   updated.
      * @param sample     a sample object that is used to generated the values being set on other objects.
      * @param projection the fields to update in this call
      * @param <T>        the type of objects being updated
