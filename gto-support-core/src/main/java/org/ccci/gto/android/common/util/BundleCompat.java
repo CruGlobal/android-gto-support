534c213178c82a6ebc8d9a5e35b515d1387b78cc
Daniel Frett <daniel.frett@ccci.org>
2016-12-02 13:29:06 -0500

    checkstyle fix

--- gto-support-core/src/main/java/org/ccci/gto/android/common/util/BundleCompat.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/util/BundleCompat.java
@@ -5,12 +5,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 /**
- * @deprecated Since 1.0.2, use {@link org.ccci.gto.android.common.compat.os.BundleCompat} instead.
+ * @deprecated Since v1.0.2, use {@link org.ccci.gto.android.common.compat.os.BundleCompat} instead.
  */
 @Deprecated
 public class BundleCompat {
     /**
-     * @deprecated Since 1.0.2, use {@link org.ccci.gto.android.common.compat.os.BundleCompat#getString(Bundle, String, String)} instead.
+     * @deprecated Since v1.0.2, use {@link org.ccci.gto.android.common.compat.os.BundleCompat#getString(Bundle, String,
+     * String)} instead.
      */
     @Deprecated
     public static String getString(@NonNull final Bundle bundle, @NonNull final String key,
