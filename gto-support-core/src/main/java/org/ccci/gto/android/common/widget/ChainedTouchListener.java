c9c7dc0c84db0dc21ed4d0ee55eeec51d0109557
Daniel Frett <daniel.frett@ccci.org>
2016-12-07 11:09:44 -0500

    add a couple utility touch listeners

---
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/widget/ChainedTouchListener.java
@@ -0,0 +1,24 @@
+package org.ccci.gto.android.common.widget;
+
+import android.support.annotation.NonNull;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class ChainedTouchListener implements View.OnTouchListener {
+    @NonNull
+    private final View.OnTouchListener[] mListeners;
+
+    public ChainedTouchListener(@NonNull final View.OnTouchListener... listeners) {
+        mListeners = listeners;
+    }
+
+    @Override
+    public boolean onTouch(@NonNull final View v, @NonNull final MotionEvent event) {
+        for (final View.OnTouchListener listener : mListeners) {
+            if (listener.onTouch(v, event)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
