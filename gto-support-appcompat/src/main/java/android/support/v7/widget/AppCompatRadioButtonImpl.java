53feb748c8ea75820b34e3137a1bbd3307e3e195
Daniel Frett <daniel.frett@ccci.org>
2017-03-13 17:25:19 -0400

    override AppCompatRadioButton to support backgroundTint options

---
+++ gto-support-appcompat/src/main/java/android/support/v7/widget/AppCompatRadioButtonImpl.java
@@ -0,0 +1,112 @@
+package android.support.v7.widget;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.RestrictTo;
+import android.support.v4.view.TintableBackgroundView;
+import android.util.AttributeSet;
+
+import static android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP;
+
+@SuppressWarnings("RestrictedApi")
+public class AppCompatRadioButtonImpl extends AppCompatRadioButton implements TintableBackgroundView {
+    private AppCompatBackgroundHelper mBackgroundTintHelper;
+
+    public AppCompatRadioButtonImpl(final Context context) {
+        this(context, null);
+    }
+
+    public AppCompatRadioButtonImpl(final Context context, final AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public AppCompatRadioButtonImpl(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(TintContextWrapper.wrap(context), attrs, defStyleAttr);
+
+        mBackgroundTintHelper = new AppCompatBackgroundHelper(this);
+        mBackgroundTintHelper.loadFromAttributes(attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setBackgroundResource(@DrawableRes final int resId) {
+        super.setBackgroundResource(resId);
+        if (mBackgroundTintHelper != null) {
+            mBackgroundTintHelper.onSetBackgroundResource(resId);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public void setBackgroundDrawable(final Drawable background) {
+        super.setBackgroundDrawable(background);
+        if (mBackgroundTintHelper != null) {
+            mBackgroundTintHelper.onSetBackgroundDrawable(background);
+        }
+    }
+
+    /**
+     * This should be accessed via
+     * {@link android.support.v4.view.ViewCompat#setBackgroundTintList(android.view.View, ColorStateList)}
+     *
+     * @hide
+     */
+    @RestrictTo(LIBRARY_GROUP)
+    @Override
+    public void setSupportBackgroundTintList(@Nullable final ColorStateList tint) {
+        if (mBackgroundTintHelper != null) {
+            mBackgroundTintHelper.setSupportBackgroundTintList(tint);
+        }
+    }
+
+    /**
+     * This should be accessed via
+     * {@link android.support.v4.view.ViewCompat#getBackgroundTintList(android.view.View)}
+     *
+     * @hide
+     */
+    @RestrictTo(LIBRARY_GROUP)
+    @Override
+    @Nullable
+    public ColorStateList getSupportBackgroundTintList() {
+        return mBackgroundTintHelper != null ? mBackgroundTintHelper.getSupportBackgroundTintList() : null;
+    }
+
+    /**
+     * This should be accessed via
+     * {@link android.support.v4.view.ViewCompat#setBackgroundTintMode(android.view.View, PorterDuff.Mode)}
+     *
+     * @hide
+     */
+    @RestrictTo(LIBRARY_GROUP)
+    @Override
+    public void setSupportBackgroundTintMode(@Nullable final PorterDuff.Mode tintMode) {
+        if (mBackgroundTintHelper != null) {
+            mBackgroundTintHelper.setSupportBackgroundTintMode(tintMode);
+        }
+    }
+
+    /**
+     * This should be accessed via
+     * {@link android.support.v4.view.ViewCompat#getBackgroundTintMode(android.view.View)}
+     *
+     * @hide
+     */
+    @RestrictTo(LIBRARY_GROUP)
+    @Override
+    @Nullable
+    public PorterDuff.Mode getSupportBackgroundTintMode() {
+        return mBackgroundTintHelper != null ? mBackgroundTintHelper.getSupportBackgroundTintMode() : null;
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        if (mBackgroundTintHelper != null) {
+            mBackgroundTintHelper.applySupportBackgroundTint();
+        }
+    }
+}
