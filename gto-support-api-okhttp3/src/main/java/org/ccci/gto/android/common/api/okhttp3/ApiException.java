f7ba7691f713adb4a38ecaaba7bb6a32607d5bc9
Daniel Frett <daniel.frett@ccci.org>
2017-04-05 16:33:42 -0400

    deprecated unnecessary exception class

--- gto-support-api-okhttp3/src/main/java/org/ccci/gto/android/common/api/okhttp3/ApiException.java
+++ gto-support-api-okhttp3/src/main/java/org/ccci/gto/android/common/api/okhttp3/ApiException.java
@@ -2,6 +2,10 @@ package org.ccci.gto.android.common.api.okhttp3;
 
 import java.io.IOException;
 
+/**
+ * @deprecated Since 1.0.4, Remove unnecessary unused root exception class.
+ */
+@Deprecated
 public class ApiException extends IOException {
     public ApiException() {
     }
