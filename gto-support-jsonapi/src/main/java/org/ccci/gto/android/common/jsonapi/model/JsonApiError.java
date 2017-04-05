185ca0d2350b88c5dbf09a55930459be291bdb4d
Daniel Frett <daniel.frett@ccci.org>
2017-04-05 13:24:32 -0400

    support source.pointer and meta for jsonapi error objects

--- gto-support-jsonapi/src/main/java/org/ccci/gto/android/common/jsonapi/model/JsonApiError.java
+++ gto-support-jsonapi/src/main/java/org/ccci/gto/android/common/jsonapi/model/JsonApiError.java
@@ -2,13 +2,26 @@ package org.ccci.gto.android.common.jsonapi.model;
 
 import android.support.annotation.Nullable;
 
+import org.json.JSONObject;
+
 public class JsonApiError {
     public static final String JSON_ERROR_STATUS = "status";
     public static final String JSON_ERROR_DETAIL = "detail";
+    public static final String JSON_ERROR_SOURCE = "source";
+    public static final String JSON_ERROR_META = "meta";
 
-    private String mDetail;
+    @Nullable
     private Integer mStatus;
+    @Nullable
+    private String mDetail;
 
+    @Nullable
+    private Source mSource;
+
+    @Nullable
+    private JSONObject mRawMeta;
+
+    @Nullable
     public String getDetail() {
         return mDetail;
     }
@@ -17,11 +30,46 @@ public class JsonApiError {
         mDetail = detail;
     }
 
+    @Nullable
     public Integer getStatus() {
         return mStatus;
     }
 
     public void setStatus(@Nullable final Integer status) {
         mStatus = status;
+    }
+
+    @Nullable
+    public Source getSource() {
+        return mSource;
+    }
+
+    public void setSource(@Nullable final Source source) {
+        mSource = source;
+    }
+
+    @Nullable
+    public JSONObject getRawMeta() {
+        return mRawMeta;
+    }
+
+    public void setRawMeta(@Nullable final JSONObject meta) {
+        mRawMeta = meta;
+    }
+
+    public static class Source {
+        public static final String JSON_ERROR_SOURCE_POINTER = "pointer";
+
+        @Nullable
+        private String mPointer;
+
+        @Nullable
+        public String getPointer() {
+            return mPointer;
+        }
+
+        public void setPointer(@Nullable final String pointer) {
+            mPointer = pointer;
+        }
     }
 }
