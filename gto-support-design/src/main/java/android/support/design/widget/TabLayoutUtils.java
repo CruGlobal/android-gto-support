69576a4c50c0174d87cf20df1a45bc8a4f72c7d2
Daniel Frett <daniel.frett@ccci.org>
2018-04-17 16:26:15 -0400

    add getBackground method to TabLayoutUtils

--- gto-support-design/src/main/java/android/support/design/widget/TabLayoutUtils.java
+++ gto-support-design/src/main/java/android/support/design/widget/TabLayoutUtils.java
@@ -6,11 +6,18 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
 
 public final class TabLayoutUtils {
+    @Nullable
+    public static Drawable getBackground(@NonNull final TabLayout.Tab tab) {
+        return tab.mView.getBackground();
+    }
+
+    @NonNull
     public static TabLayout.Tab setBackground(@NonNull final TabLayout.Tab tab, @Nullable final Drawable background) {
         ViewCompat.setBackground(tab.mView, background);
         return tab;
     }
 
+    @NonNull
     public static TabLayout.Tab setVisibility(@NonNull final TabLayout.Tab tab, final int visibility) {
         tab.mView.setVisibility(visibility);
         return tab;
