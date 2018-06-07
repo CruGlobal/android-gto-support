22ca4cb0a86b3750559139aff2fad4a1935be28a
Daniel Frett <daniel.frett@ccci.org>
2018-06-07 10:42:12 -0400

    relocate TimeConstants to gto-support-base module

---
+++ gto-support-base/src/main/java/org/ccci/gto/android/common/base/TimeConstants.java
@@ -0,0 +1,8 @@
+package org.ccci.gto.android.common.base;
+
+public final class TimeConstants {
+    public static final long MIN_IN_MS = 60 * 1000;
+    public static final long HOUR_IN_MS = 60 * MIN_IN_MS;
+    public static final long DAY_IN_MS = 24 * HOUR_IN_MS;
+    public static final long WEEK_IN_MS = 7 * DAY_IN_MS;
+}
