975c7d31be58ade400bfa14c4c4e66b4d5177713
Daniel Frett <daniel.frett@ccci.org>
2018-09-05 13:43:30 -0400

    JsonApiParams no longer uses TextUtils, so no need to mock it

--- gto-support-jsonapi-retrofit2/src/test/java/org/ccci/gto/android/common/jsonapi/retrofit2/JsonApiParamsTest.java
+++ gto-support-jsonapi-retrofit2/src/test/java/org/ccci/gto/android/common/jsonapi/retrofit2/JsonApiParamsTest.java
@@ -1,26 +1,12 @@
 package org.ccci.gto.android.common.jsonapi.retrofit2;
 
-import android.text.TextUtils;
-
-import org.ccci.gto.android.common.testing.CommonMocks;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({TextUtils.class})
 public class JsonApiParamsTest {
-    @Before
-    public void setup() throws Exception {
-        CommonMocks.mockTextUtils();
-    }
-
     @Test
     public void verifyInclude() throws Exception {
         final JsonApiParams params = new JsonApiParams();
