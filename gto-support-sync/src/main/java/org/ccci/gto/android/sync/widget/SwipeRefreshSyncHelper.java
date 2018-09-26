0436a4f988be6cc4dae793f5b105588b0df9a21a
Daniel Frett <daniel.frett@ccci.org>
2018-09-26 10:19:32 -0400

    update gto-support-sync for AndroidX

--- gto-support-sync/src/main/java/org/ccci/gto/android/sync/widget/SwipeRefreshSyncHelper.java
+++ gto-support-sync/src/main/java/org/ccci/gto/android/sync/widget/SwipeRefreshSyncHelper.java
@@ -1,10 +1,10 @@
 package org.ccci.gto.android.sync.widget;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.UiThread;
-import android.support.v4.widget.SwipeRefreshLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import org.ccci.gto.android.common.util.LongSparseBooleanArray;
 import org.ccci.gto.android.sync.ThreadedSyncIntentService;
