b46d1a76f7446fdf839deffd203400b1b9896f3f
Daniel Frett <daniel.frett@ccci.org>
2019-02-13 10:17:28 -0500

    extract DataBindingViewHolder in order to re-use it for the Realm recycler view adapter
    Also deprecate BaseDataBindingAdapter because it serves no purpose

--- gto-support-recyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/adapter/SimpleDataBindingAdapter.java
+++ gto-support-recyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/adapter/SimpleDataBindingAdapter.java
@@ -2,13 +2,12 @@ package org.ccci.gto.android.common.recyclerview.adapter;
 
 import android.view.ViewGroup;
 
-import org.ccci.gto.android.common.recyclerview.adapter.BaseDataBindingAdapter.DataBindingViewHolder;
-
 import androidx.annotation.NonNull;
 import androidx.databinding.ViewDataBinding;
+import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class SimpleDataBindingAdapter<B extends ViewDataBinding>
-        extends BaseDataBindingAdapter<B, DataBindingViewHolder<B>> {
+        extends RecyclerView.Adapter<DataBindingViewHolder<B>> {
     // region Lifecycle Events
 
     @NonNull
