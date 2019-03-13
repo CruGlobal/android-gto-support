c5cc10cfa0e648f93c87dc75a2a1278c29be61af
Daniel Frett <daniel.frett@ccci.org>
2019-03-13 14:23:19 -0400

    make the method more Kotliny

--- gto-support-lifecycle/src/main/java/org/ccci/gto/android/common/lifecycle/Transformations2.kt
+++ gto-support-lifecycle/src/main/java/org/ccci/gto/android/common/lifecycle/Transformations2.kt
@@ -6,19 +6,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
 
-fun <X, Y, Z> combine(first: LiveData<X>, second: LiveData<Y>, block: (X?, Y?) -> LiveData<Z>): LiveData<Z> {
+/**
+ * This method will combine 2 LiveData objects into a new LiveData object by running the {@param mapFunction} on the
+ * current values of both source LiveData objects.
+ * Returning either of the original LiveData objects will cause an Exception.
+ */
+@JvmName("combine")
+fun <X, Y, Z> LiveData<X>.combineWith(other: LiveData<Y>, mapFunction: (X?, Y?) -> LiveData<Z>): LiveData<Z> {
     val result = MediatorLiveData<Z>()
     val observer = object : Observer<Any?> {
         private var source: LiveData<Z>? = null
         override fun onChanged(t: Any?) {
-            val newSource = block(first.value, second.value)
+            val newSource = mapFunction(value, other.value)
             if (source == newSource) return
             source?.let { result.removeSource(it) }
             source = newSource
             source?.let { result.addSource(it) { value: Z? -> result.value = value } }
         }
     }
-    result.addSource(first, observer)
-    result.addSource(second, observer)
+    result.addSource(this, observer)
+    result.addSource(other, observer)
     return result
 }
