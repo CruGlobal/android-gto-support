8dd59f8256683b723306350667e5d5ee7452ec28
Daniel Frett <daniel.frett@ccci.org>
2019-06-17 11:35:14 -0400

    support passing a LifecycleOwner to RealmDataBindingAdapter

--- gto-support-realm/src/main/java/org/ccci/gto/android/common/realm/adapter/RealmDataBindingAdapter.kt
+++ gto-support-realm/src/main/java/org/ccci/gto/android/common/realm/adapter/RealmDataBindingAdapter.kt
@@ -2,37 +2,39 @@ package org.ccci.gto.android.common.realm.adapter
 
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
+import com.karumi.weak.weak
 import io.realm.OrderedRealmCollection
 import io.realm.RealmModel
 import io.realm.RealmRecyclerViewAdapter
 import org.ccci.gto.android.common.recyclerview.adapter.DataBindingViewHolder
 
-abstract class RealmDataBindingAdapter<T : RealmModel, B : ViewDataBinding>(data: OrderedRealmCollection<T>? = null) :
-    RealmRecyclerViewAdapter<T, DataBindingViewHolder<B>>(data, true), Observer<OrderedRealmCollection<T>?> {
-    // region Lifecycle Events
+abstract class RealmDataBindingAdapter<T : RealmModel, B : ViewDataBinding>(
+    lifecycleOwner: LifecycleOwner? = null,
+    data: OrderedRealmCollection<T>? = null
+) : RealmRecyclerViewAdapter<T, DataBindingViewHolder<B>>(data, true), Observer<OrderedRealmCollection<T>?> {
+    private val lifecycleOwner by weak(lifecycleOwner)
 
-    override fun onChanged(t: OrderedRealmCollection<T>?) {
-        updateData(t)
-    }
+    // region Lifecycle
+    override fun onChanged(t: OrderedRealmCollection<T>?) = updateData(t)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        DataBindingViewHolder(onCreateViewDataBinding(parent, viewType))
-
-    protected abstract fun onCreateViewDataBinding(parent: ViewGroup, viewType: Int): B
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = DataBindingViewHolder(
+        onCreateViewDataBinding(parent, viewType)
+            .also { it.lifecycleOwner = lifecycleOwner }
+    )
 
     override fun onBindViewHolder(holder: DataBindingViewHolder<B>, position: Int) {
         onBindViewDataBinding(holder.binding, position)
         holder.binding.executePendingBindings()
     }
 
-    protected abstract fun onBindViewDataBinding(binding: B, position: Int)
-
     override fun onViewRecycled(holder: DataBindingViewHolder<B>) = onViewDataBindingRecycled(holder.binding)
 
-    protected fun onViewDataBindingRecycled(binding: B) {}
-
-    // end Lifecycle Events
+    protected abstract fun onCreateViewDataBinding(parent: ViewGroup, viewType: Int): B
+    protected abstract fun onBindViewDataBinding(binding: B, position: Int)
+    protected fun onViewDataBindingRecycled(binding: B) = Unit
+    // end Lifecycle
 
     override fun updateData(data: OrderedRealmCollection<T>?) =
         if (data !== this.data || data?.isManaged == false) super.updateData(data) else Unit
