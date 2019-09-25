a90870df8d76f6af775663894cdd374bcefd47ca
Daniel Frett <daniel.frett@ccci.org>
2019-09-25 09:12:53 -0400

    convert MainThreadExecutor to Kotlin

---
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/util/MainThreadExecutor.kt
@@ -0,0 +1,14 @@
+package org.ccci.gto.android.common.util
+
+import android.os.Handler
+import android.os.Looper
+
+import java.util.concurrent.Executor
+
+class MainThreadExecutor : Executor {
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun execute(r: Runnable) {
+        handler.post(r)
+    }
+}
