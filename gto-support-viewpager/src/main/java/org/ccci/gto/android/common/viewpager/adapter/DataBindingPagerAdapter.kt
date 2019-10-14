efd563aa1da0bdb86c51768cc5cc56d6de0d167e
Daniel Frett <daniel.frett@ccci.org>
2019-10-14 13:59:56 -0400

    update DataBindingPagerAdapter to utilize generics

--- gto-support-viewpager/src/main/java/org/ccci/gto/android/common/viewpager/adapter/DataBindingPagerAdapter.kt
+++ gto-support-viewpager/src/main/java/org/ccci/gto/android/common/viewpager/adapter/DataBindingPagerAdapter.kt
@@ -5,18 +5,19 @@ import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
 import com.karumi.weak.weak
 
-abstract class DataBindingPagerAdapter(lifecycleOwner: LifecycleOwner? = null) :
-    BaseDataBindingPagerAdapter<DataBindingViewHolder>(lifecycleOwner) {
+abstract class DataBindingPagerAdapter<B : ViewDataBinding>(lifecycleOwner: LifecycleOwner? = null) :
+    BaseDataBindingPagerAdapter<B, DataBindingViewHolder<B>>(lifecycleOwner) {
     final override fun onCreateViewHolder(parent: ViewGroup) = DataBindingViewHolder(
         onCreateViewDataBinding(parent)
             .also { it.lifecycleOwner = lifecycleOwner }
     )
 
-    abstract fun onCreateViewDataBinding(parent: ViewGroup): ViewDataBinding
+    abstract fun onCreateViewDataBinding(parent: ViewGroup): B
 }
 
-abstract class BaseDataBindingPagerAdapter<VH : DataBindingViewHolder>(lifecycleOwner: LifecycleOwner? = null) :
-    ViewHolderPagerAdapter<VH>() {
+abstract class BaseDataBindingPagerAdapter<B : ViewDataBinding, VH : DataBindingViewHolder<B>>(
+    lifecycleOwner: LifecycleOwner? = null
+) : ViewHolderPagerAdapter<VH>() {
     protected val lifecycleOwner by weak(lifecycleOwner)
 
     final override fun onBindViewHolder(holder: VH, position: Int) {
@@ -29,6 +30,6 @@ abstract class BaseDataBindingPagerAdapter<VH : DataBindingViewHolder>(lifecycle
         onViewDataBindingRecycled(holder, holder.binding)
     }
 
-    protected open fun onBindViewDataBinding(holder: VH, binding: ViewDataBinding, position: Int) = Unit
-    protected open fun onViewDataBindingRecycled(holder: VH, binding: ViewDataBinding) = Unit
+    protected open fun onBindViewDataBinding(holder: VH, binding: B, position: Int) = Unit
+    protected open fun onViewDataBindingRecycled(holder: VH, binding: B) = Unit
 }
--- gto-support-viewpager/src/main/java/org/ccci/gto/android/common/viewpager/adapter/DataBindingViewHolder.kt
+++ gto-support-viewpager/src/main/java/org/ccci/gto/android/common/viewpager/adapter/DataBindingViewHolder.kt
@@ -2,4 +2,4 @@ package org.ccci.gto.android.common.viewpager.adapter
 
 import androidx.databinding.ViewDataBinding
 
-open class DataBindingViewHolder(val binding: ViewDataBinding) : ViewHolderPagerAdapter.ViewHolder(binding.root)
+open class DataBindingViewHolder<B : ViewDataBinding>(val binding: B) : ViewHolderPagerAdapter.ViewHolder(binding.root)
