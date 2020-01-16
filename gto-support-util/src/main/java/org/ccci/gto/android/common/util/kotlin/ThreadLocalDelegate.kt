ffd1bde3ae627305afbf10ebd01de296a99dff9f
Daniel Frett <daniel.frett@ccci.org>
2020-01-16 18:05:47 -0500

    create a threadlocal kotlin delegate

---
+++ gto-support-util/src/main/java/org/ccci/gto/android/common/util/kotlin/ThreadLocalDelegate.kt
@@ -0,0 +1,21 @@
+package org.ccci.gto.android.common.util.kotlin
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+private class ThreadLocalDelegate<T>(initializer: () -> T) : ReadWriteProperty<Any, T> {
+    private val threadLocal = ThreadLocalWithInitializer(initializer)
+
+    @Suppress("UNCHECKED_CAST")
+    override operator fun getValue(thisRef: Any, property: KProperty<*>): T = threadLocal.get() as T
+    override operator fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        threadLocal.set(value)
+    }
+}
+
+private class ThreadLocalWithInitializer<T>(private val initializer: () -> T) : ThreadLocal<T>() {
+    override fun initialValue(): T = initializer()
+}
+
+fun <T> threadLocal(initializer: () -> T): ReadWriteProperty<Any, T> = ThreadLocalDelegate(initializer)
+fun <T> threadLocal() = threadLocal<T?> { null }
