bdc7f8e251290ab5b14588d244df75fb48f9a444
Daniel Frett <daniel.frett@ccci.org>
2020-01-17 09:58:14 -0500

    remove a bunch of deprecated functionality

--- gto-support-design/src/main/java/com/google/android/material/textfield/TextInputLayoutUtils.java
+++ gto-support-design/src/main/java/com/google/android/material/textfield/TextInputLayoutUtils.java
@@ -15,61 +15,6 @@ import timber.log.Timber;
 public final class TextInputLayoutUtils {
     private static final String TAG = "TextInputLayoutUtils";
 
-    /**
-     * @deprecated Since v2.0.0, Use {@link TextInputLayout#setDefaultHintTextColor(ColorStateList)} instead.
-     */
-    @Deprecated
-    public static void setHintTextColor(@NonNull final TextInputLayout layout, @ColorInt final int color) {
-        layout.setDefaultHintTextColor(ColorStateList.valueOf(color));
-    }
-
-    /**
-     * @deprecated Since v2.0.0, Use {@link TextInputLayout#setDefaultHintTextColor(ColorStateList)} instead.
-     */
-    @Deprecated
-    public static void setHintTextColor(@NonNull final TextInputLayout layout, @Nullable final ColorStateList colors) {
-        layout.setDefaultHintTextColor(colors);
-    }
-
-    /**
-     * @deprecated Since v2.0.0, Use {@link TextInputLayout#setDefaultHintTextColor(ColorStateList)} instead.
-     */
-    @Deprecated
-    public static void setExpandedTextColor(@NonNull final TextInputLayout layout, @ColorInt final int color) {
-        setExpandedTextColor(layout, ColorStateList.valueOf(color));
-    }
-
-    /**
-     * @deprecated Since v2.0.0, Use {@link TextInputLayout#setDefaultHintTextColor(ColorStateList)} instead.
-     */
-    @Deprecated
-    public static void setExpandedTextColor(@NonNull final TextInputLayout layout,
-                                            @Nullable final ColorStateList colors) {
-        layout.setDefaultHintTextColor(colors);
-        Timber.tag(TAG)
-                .e(new UnsupportedOperationException(
-                        "Unable to set expanded text color different than collapsed text color. " +
-                                "We will set the default hint text color instead."));
-    }
-
-    /**
-     * @deprecated Since v2.0.0, Use {@link TextInputLayoutUtils#setFocusedTextColor(TextInputLayout, int)} instead.
-     */
-    @Deprecated
-    public static void setCollapsedTextColor(@NonNull final TextInputLayout layout, @ColorInt final int color) {
-        setCollapsedTextColor(layout, ColorStateList.valueOf(color));
-    }
-
-    /**
-     * @deprecated Since v2.0.0, Use {@link TextInputLayoutUtils#setFocusedTextColor(TextInputLayout, ColorStateList)}
-     * instead.
-     */
-    @Deprecated
-    public static void setCollapsedTextColor(@NonNull final TextInputLayout layout,
-                                             @Nullable final ColorStateList colors) {
-        setFocusedTextColor(layout, colors);
-    }
-
     public static void setFocusedTextColor(@NonNull final TextInputLayout layout, @ColorInt final int color) {
         setFocusedTextColor(layout, ColorStateList.valueOf(color));
     }
--- gto-support-recyclerview/proguard-consumer.pro
+++ gto-support-recyclerview/proguard-consumer.pro
@@ -1,4 +1,2 @@
 #These adapters reference ViewDataBinding, which is optional. So we don't warn for these files
--dontwarn org.ccci.gto.android.common.recyclerview.adapter.BaseDataBindingAdapter
--dontwarn org.ccci.gto.android.common.recyclerview.adapter.BaseDataBindingAdapter$DataBindingViewHolder
 -dontwarn org.ccci.gto.android.common.recyclerview.adapter.SimpleDataBindingAdapter
