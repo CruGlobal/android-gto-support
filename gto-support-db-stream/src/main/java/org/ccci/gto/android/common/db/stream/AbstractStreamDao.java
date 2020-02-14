61142c0af18230b24e456bab6370606cbcbd0b1b
Daniel Frett <daniel.frett@ccci.org>
2020-02-14 11:44:46 -0500

    convert StreamDao to Kotlin

--- gto-support-db-stream/src/main/java/org/ccci/gto/android/common/db/stream/AbstractStreamDao.java
+++ gto-support-db-stream/src/main/java/org/ccci/gto/android/common/db/stream/AbstractStreamDao.java
@@ -2,10 +2,7 @@ package org.ccci.gto.android.common.db.stream;
 
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.annimon.stream.Stream;
-
 import org.ccci.gto.android.common.db.AbstractDao;
-import org.ccci.gto.android.common.db.Query;
 import org.ccci.gto.android.common.db.StreamDao;
 
 import androidx.annotation.NonNull;
@@ -13,11 +10,5 @@ import androidx.annotation.NonNull;
 public abstract class AbstractStreamDao extends AbstractDao implements StreamDao {
     protected AbstractStreamDao(@NonNull final SQLiteOpenHelper helper) {
         super(helper);
-    }
-
-    @NonNull
-    @Override
-    public <T> Stream<T> streamCompat(@NonNull final Query<T> query) {
-        return StreamHelper.stream(this, query);
     }
 }
