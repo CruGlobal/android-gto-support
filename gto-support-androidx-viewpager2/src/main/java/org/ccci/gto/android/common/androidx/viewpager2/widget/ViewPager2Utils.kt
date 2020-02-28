0a1351f9ea3a99c6c1e1ae1695d8897790f4c36d
Daniel Frett <daniel.frett@ccci.org>
2020-02-28 18:08:33 -0500

    when pages have inconsistent heights we need to force layout when scrolling
    This isn't ideal, but works for now

--- gto-support-androidx-viewpager2/src/main/java/org/ccci/gto/android/common/androidx/viewpager2/widget/ViewPager2Utils.kt
+++ gto-support-androidx-viewpager2/src/main/java/org/ccci/gto/android/common/androidx/viewpager2/widget/ViewPager2Utils.kt
@@ -4,7 +4,10 @@ import android.view.ViewGroup
 import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.recyclerView
 
-fun ViewPager2.setHeightWrapContent() {
+@JvmOverloads
+fun ViewPager2.setHeightWrapContent(pagesHaveConsistentHeight: Boolean = false) {
+    val recyclerView = recyclerView
+
     with(recyclerView) {
         // update nested RecyclerView to have a wrap_content height
         layoutParams = layoutParams.apply { height = ViewGroup.LayoutParams.WRAP_CONTENT }
@@ -12,5 +15,15 @@ fun ViewPager2.setHeightWrapContent() {
         // remove restriction that children should have match_parent for height and width
         // source: https://gist.github.com/safaorhan/1a541af729c7657426138d18b87d5bd4
         clearOnChildAttachStateChangeListeners()
+    }
+
+    if (!pagesHaveConsistentHeight) {
+        // disable the measurement cache because it breaks differently sized pages that have identical layout params
+        recyclerView.layoutManager!!.isMeasurementCacheEnabled = false
+
+        // request layout whenever we scroll in case the height of new pages will change the current height measurement
+        registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrollStateChanged(state: Int) = recyclerView.requestLayout()
+        })
     }
 }
