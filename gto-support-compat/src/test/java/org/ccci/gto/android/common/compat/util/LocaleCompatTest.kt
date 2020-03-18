c7534f1b7cc2f25ad58667c33ee274aceee65fb9
Daniel Frett <daniel.frett@ccci.org>
2020-03-18 12:27:44 -0400

    test script support in LocaleCompat

--- gto-support-compat/src/test/java/org/ccci/gto/android/common/compat/util/LocaleCompatTest.kt
+++ gto-support-compat/src/test/java/org/ccci/gto/android/common/compat/util/LocaleCompatTest.kt
@@ -1,7 +1,13 @@
 package org.ccci.gto.android.common.compat.util
 
+import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.greaterThanOrEqualTo
+import org.hamcrest.Matchers.lessThan
 import org.junit.Assert.assertEquals
+import org.junit.Assume.assumeThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -11,14 +17,16 @@ private val LANGUAGETAGS = mapOf<Locale, String>(
     Locale.US to "en-US",
     Locale("EN", "gb") to "en-GB",
     Locale.ENGLISH to "en",
-    Locale.SIMPLIFIED_CHINESE to "zh-CN"
+    Locale.SIMPLIFIED_CHINESE to "zh-CN",
+    Locale("es", "419") to "es-419"
 )
 
 private val LOCALES = mapOf<String, Locale>(
     "en-US" to Locale.US,
     "en-GB" to Locale.UK,
     "en" to Locale.ENGLISH,
-    "EN-us" to Locale.US
+    "EN-us" to Locale.US,
+    "es-419" to Locale("es", "419")
 )
 
 @RunWith(AndroidJUnit4::class)
@@ -26,15 +34,42 @@ private val LOCALES = mapOf<String, Locale>(
 internal class LocaleCompatTest {
     @Test
     fun testForLanguageTag() {
-        for ((key, value) in LOCALES) {
-            assertEquals(value, LocaleCompat.forLanguageTag(key))
-        }
+        LOCALES.forEach { (key, value) -> assertEquals(value, LocaleCompat.forLanguageTag(key)) }
+    }
+
+    @Test
+    fun testForLanguageTagWithScript() {
+        // script support is only available starting with Lollipop
+        assumeThat(Build.VERSION.SDK_INT, greaterThanOrEqualTo(Build.VERSION_CODES.LOLLIPOP))
+
+        val locale = LocaleCompat.forLanguageTag("zh-Hans-CN")
+        assertThat(locale, equalTo(Locale.Builder().setLanguage("zh").setScript("Hans").setRegion("CN").build()))
+        assertEquals("zh", locale.language)
+        assertEquals("Hans", locale.script)
+        assertEquals("CN", locale.country)
+    }
+
+    @Test
+    fun testForLanguageTagWithScriptBeforeLollipop() {
+        // script support is only available starting with Lollipop
+        assumeThat(Build.VERSION.SDK_INT, lessThan(Build.VERSION_CODES.LOLLIPOP))
+
+        val locale = LocaleCompat.forLanguageTag("zh-Hans-CN")
+        assertThat(locale, equalTo(Locale("zh", "CN")))
+        assertEquals("zh", locale.language)
+        assertEquals("CN", locale.country)
     }
 
     @Test
     fun testToLanguageTag() {
-        for ((key, value) in LANGUAGETAGS) {
-            assertEquals(value, LocaleCompat.toLanguageTag(key))
-        }
+        LANGUAGETAGS.forEach { (key, value) -> assertEquals(value, LocaleCompat.toLanguageTag(key)) }
+    }
+
+    @Test
+    fun testToLanguageTagWithScript() {
+        // script support is only available starting with Lollipop
+        assumeThat(Build.VERSION.SDK_INT, greaterThanOrEqualTo(Build.VERSION_CODES.LOLLIPOP))
+
+        assertEquals("zh-Hans-CN", LocaleCompat.toLanguageTag(Locale.forLanguageTag("zh-Hans-CN")))
     }
 }
