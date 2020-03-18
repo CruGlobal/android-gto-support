08ecea3ae017686c1b9d79a5d9f0c9767f95c9e6
Daniel Frett <daniel.frett@ccci.org>
2020-03-18 11:26:49 -0400

    convert LocaleCompatTest to Kotlin

--- gto-support-compat/src/test/java/org/ccci/gto/android/common/compat/util/LocaleCompatTest.kt
+++ gto-support-compat/src/test/java/org/ccci/gto/android/common/compat/util/LocaleCompatTest.kt
@@ -1,53 +1,40 @@
 package org.ccci.gto.android.common.compat.util
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
+import org.robolectric.annotation.Config
 import java.util.Locale
 
-@RunWith(Parameterized::class)
-internal class LocaleCompatTest(private val compat: LocaleCompatMethods) {
+private val LANGUAGETAGS = mapOf<Locale, String>(
+    Locale.US to "en-US",
+    Locale("EN", "gb") to "en-GB",
+    Locale.ENGLISH to "en",
+    Locale.SIMPLIFIED_CHINESE to "zh-CN"
+)
 
+private val LOCALES = mapOf<String, Locale>(
+    "en-US" to Locale.US,
+    "en-GB" to Locale.UK,
+    "en" to Locale.ENGLISH,
+    "EN-us" to Locale.US
+)
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [16, 17, 18, 19, 21, 24])
+internal class LocaleCompatTest {
     @Test
-    @Throws(Exception::class)
     fun testForLanguageTag() {
         for ((key, value) in LOCALES) {
-            assertEquals(value, compat.forLanguageTag(key))
+            assertEquals(value, LocaleCompat.forLanguageTag(key))
         }
     }
 
     @Test
-    @Throws(Exception::class)
     fun testToLanguageTag() {
         for ((key, value) in LANGUAGETAGS) {
-            assertEquals(value, compat.toLanguageTag(key))
+            assertEquals(value, LocaleCompat.toLanguageTag(key))
         }
-    }
-
-    companion object {
-        @JvmStatic
-        @Parameterized.Parameters
-        fun data(): Collection<Array<*>> {
-            return listOf(
-                arrayOf(BaseLocaleCompatMethods()),
-                arrayOf(LollipopLocaleCompatMethods()),
-                arrayOf(NougatLocaleCompatMethods())
-            )
-        }
-
-        private val LANGUAGETAGS = mapOf<Locale, String>(
-            Locale.US to "en-US",
-            Locale("EN", "gb") to "en-GB",
-            Locale.ENGLISH to "en",
-            Locale.SIMPLIFIED_CHINESE to "zh-CN"
-        )
-
-        private val LOCALES = mapOf<String, Locale>(
-            "en-US" to Locale.US,
-            "en-GB" to Locale.UK,
-            "en" to Locale.ENGLISH,
-            "EN-us" to Locale.US
-        )
     }
 }
