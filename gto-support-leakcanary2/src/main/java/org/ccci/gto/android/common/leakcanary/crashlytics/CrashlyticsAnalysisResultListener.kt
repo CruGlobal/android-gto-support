a44d9cfe8642302136ab435cf56b39fc4ffe63ef
Daniel Frett <daniel.frett@ccci.org>
2020-03-19 16:03:55 -0400

    create a new leakcanary2 result listener that uses the new firebase crashlytics library

---
+++ gto-support-leakcanary2/src/main/java/org/ccci/gto/android/common/leakcanary/crashlytics/CrashlyticsAnalysisResultListener.kt
@@ -0,0 +1,33 @@
+package org.ccci.gto.android.common.leakcanary.crashlytics
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import leakcanary.DefaultOnHeapAnalyzedListener
+import leakcanary.OnHeapAnalyzedListener
+import org.ccci.gto.android.common.leakcanary.util.asFakeException
+import shark.HeapAnalysis
+import shark.HeapAnalysisSuccess
+
+private const val LOG_LIMIT = 64 * 1024
+
+object CrashlyticsOnHeapAnalyzedListener : OnHeapAnalyzedListener {
+    private val crashlytics get() = FirebaseCrashlytics.getInstance()
+    private val defaultListener = DefaultOnHeapAnalyzedListener.create()
+
+    override fun onHeapAnalyzed(heapAnalysis: HeapAnalysis) {
+        if (heapAnalysis is HeapAnalysisSuccess) {
+            with(crashlytics) {
+                heapAnalysis.allLeaks.asSequence().flatMap { it.leakTraces.asSequence() }.forEach { leak ->
+                    // log the memory leak to Crashlytics
+                    crashlytics.log("*** Memory Leak ***")
+                    leak.toString().take(LOG_LIMIT).lines().asSequence().filterNot { it.isEmpty() }.forEach {
+                        crashlytics.log(it)
+                    }
+
+                    crashlytics.recordException(leak.asFakeException())
+                }
+            }
+        }
+
+        defaultListener.onHeapAnalyzed(heapAnalysis)
+    }
+}
