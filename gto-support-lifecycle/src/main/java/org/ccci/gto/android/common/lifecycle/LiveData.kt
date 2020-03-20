cf666f476facd6e32594dced2dd96a337e8f598c
Daniel Frett <daniel.frett@ccci.org>
2020-03-20 18:28:04 -0400

    move observeOnce functionality to gto-support-androidx-lifecycle

--- gto-support-lifecycle/src/main/java/org/ccci/gto/android/common/lifecycle/LiveData.kt
+++ gto-support-lifecycle/src/main/java/org/ccci/gto/android/common/lifecycle/LiveData.kt
@@ -3,20 +3,21 @@ package org.ccci.gto.android.common.lifecycle
 import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
+import org.ccci.gto.android.common.androidx.lifecycle.observeOnce
 
 @MainThread
+@Deprecated(
+    "Since v3.4.0, use version from gto-support-androidx-lifecycle instead", ReplaceWith(
+        "observeOnce(owner, onChanged)",
+        "org.ccci.gto.android.common.androidx.lifecycle.observeOnce"
+    )
+)
 inline fun <T> LiveData<T>.observeOnce(owner: LifecycleOwner, crossinline onChanged: (T) -> Unit) =
-    observeOnceObserver(onChanged).also { observe(owner, it) }
+    observeOnce(owner, onChanged)
 
 @MainThread
-inline fun <T> LiveData<T>.observeOnce(crossinline onChanged: (T) -> Unit) =
-    observeOnceObserver(onChanged).also { observeForever(it) }
-
-@JvmSynthetic
-inline fun <T> LiveData<T>.observeOnceObserver(crossinline onChanged: (T) -> Unit) = object : Observer<T> {
-    override fun onChanged(t: T) {
-        onChanged.invoke(t)
-        removeObserver(this)
-    }
-}
+@Deprecated(
+    "Since v3.4.0, use version from gto-support-androidx-lifecycle instead",
+    ReplaceWith("observeOnce(onChanged)", "org.ccci.gto.android.common.androidx.lifecycle.observeOnce")
+)
+inline fun <T> LiveData<T>.observeOnce(crossinline onChanged: (T) -> Unit) = observeOnce(onChanged)
