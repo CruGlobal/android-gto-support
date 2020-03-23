6cc4872ea7f766b5e41b7f927a0e6e78b7f0bf5f
Daniel Frett <daniel.frett@ccci.org>
2020-03-23 17:25:34 -0400

    create a MutableKeyIterator for LongSparseArrays

---
+++ gto-support-androidx-collection/src/main/java/org/ccci/gto/android/common/androidx/collection/LongSparseArray.kt
@@ -0,0 +1,20 @@
+package org.ccci.gto.android.common.androidx.collection
+
+import androidx.collection.LongSparseArray
+
+fun <T> LongSparseArray<T>.mutableKeyIterator(): MutableIterator<Long> =
+    object : LongIterator(), MutableIterator<Long> {
+        var index = 0
+        var nextCalled = false
+        override fun hasNext() = index < size()
+        override fun nextLong() = keyAt(index++).also { nextCalled = true }
+        override fun remove() {
+            check(nextCalled) {
+                "next() was not called or remove() was called more than once for the most recent next() call"
+            }
+
+            nextCalled = false
+            removeAt(index)
+            index--
+        }
+    }
