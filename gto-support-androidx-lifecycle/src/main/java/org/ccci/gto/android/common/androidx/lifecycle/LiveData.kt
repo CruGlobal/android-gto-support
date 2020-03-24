1773181441b4f8d7f54bfd33ef9e5fabded90ab5
Daniel Frett <daniel.frett@ccci.org>
2020-03-24 10:18:19 -0400

    create a LiveData observe method that associates the observer with the lifespan of the ViewModel

--- gto-support-androidx-lifecycle/src/main/java/org/ccci/gto/android/common/androidx/lifecycle/LiveData.kt
+++ gto-support-androidx-lifecycle/src/main/java/org/ccci/gto/android/common/androidx/lifecycle/LiveData.kt
@@ -4,6 +4,11 @@ import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.setTagIfAbsent
+import com.karumi.weak.weak
+import java.io.Closeable
+import java.util.concurrent.atomic.AtomicInteger
 
 @MainThread
 inline fun <T> LiveData<T>.observeOnce(owner: LifecycleOwner, crossinline onChanged: (T) -> Unit) =
@@ -18,5 +23,25 @@ inline fun <T> LiveData<T>.observeOnceObserver(crossinline onChanged: (T) -> Uni
     override fun onChanged(t: T) {
         onChanged.invoke(t)
         removeObserver(this)
+    }
+}
+
+private val OBSERVER_INDEX = AtomicInteger(0)
+fun <T> LiveData<T>.observe(viewModel: ViewModel, onChanged: (T) -> Unit): Observer<T> {
+    val observer = Observer<T> { t -> onChanged.invoke(t) }
+    observeForever(observer)
+    viewModel.setTagIfAbsent(
+        "LiveDataObserver-${OBSERVER_INDEX.getAndIncrement()}",
+        WeakCloseableObserverWrapper(this, observer)
+    )
+    return observer
+}
+
+private class WeakCloseableObserverWrapper<T>(liveData: LiveData<T>, observer: Observer<T>) : Closeable {
+    private val liveData: LiveData<T>? by weak(liveData)
+    private val observer: Observer<T>? by weak(observer)
+
+    override fun close() {
+        observer?.let { liveData?.removeObserver(it) }
     }
 }
