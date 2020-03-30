324a48b41376cc6f35c3080319f1221d1feb8b1d
Daniel Frett <daniel.frett@ccci.org>
2020-03-30 11:43:11 -0400

    update DaggerViewModelFactory to fallback to the AndroidViewModelFactory

--- gto-support-dagger/src/main/java/org/ccci/gto/android/common/dagger/viewmodel/DaggerViewModelFactory.kt
+++ gto-support-dagger/src/main/java/org/ccci/gto/android/common/dagger/viewmodel/DaggerViewModelFactory.kt
@@ -1,5 +1,6 @@
 package org.ccci.gto.android.common.dagger.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dagger.Reusable
@@ -8,12 +9,15 @@ import javax.inject.Provider
 
 @Reusable
 class DaggerViewModelFactory @Inject constructor(
+    app: Application,
     private val viewModelProviders: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
+    private val androidViewModelFactory = ViewModelProvider.AndroidViewModelFactory.getInstance(app)
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val creator = viewModelProviders[modelClass]
             ?: viewModelProviders.asIterable().firstOrNull { modelClass.isAssignableFrom(it.key) }?.value
-            ?: throw IllegalArgumentException("Unknown model class $modelClass")
+            ?: return androidViewModelFactory.create(modelClass)
 
         @Suppress("UNCHECKED_CAST")
         return creator.get() as T
