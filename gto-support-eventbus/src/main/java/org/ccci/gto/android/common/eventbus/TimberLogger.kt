42042c82f37307ea64b93c52cf9294ab9a66219b
Daniel Frett <daniel.frett@ccci.org>
2020-04-03 14:21:19 -0400

    remove dependence on AndroidLogger within EventBus

--- gto-support-eventbus/src/main/java/org/ccci/gto/android/common/eventbus/TimberLogger.kt
+++ gto-support-eventbus/src/main/java/org/ccci/gto/android/common/eventbus/TimberLogger.kt
@@ -1,12 +1,26 @@
 package org.ccci.gto.android.common.eventbus
 
+import android.util.Log
+import androidx.annotation.VisibleForTesting
 import org.greenrobot.eventbus.Logger
 import timber.log.Timber
 import java.util.logging.Level
 
 private const val TAG = "EventBus"
 
-class TimberLogger : Logger.AndroidLogger(TAG) {
+object TimberLogger : Logger {
     override fun log(level: Level, msg: String?) = Timber.tag(TAG).log(mapLevel(level), msg)
     override fun log(level: Level, msg: String?, th: Throwable?) = Timber.tag(TAG).log(mapLevel(level), th, msg)
+
+    @VisibleForTesting
+    internal fun mapLevel(level: Level): Int {
+        val value = level.intValue()
+        return when {
+            value >= Level.SEVERE.intValue() -> Log.ERROR
+            value >= Level.WARNING.intValue() -> Log.WARN
+            value >= Level.INFO.intValue() -> Log.INFO
+            value >= Level.CONFIG.intValue() -> Log.DEBUG
+            else -> Log.VERBOSE
+        }
+    }
 }
