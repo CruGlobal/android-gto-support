d709b82b0c3b2c48838697cb1ec9b93cee8d4fb6
Daniel Frett <daniel.frett@ccci.org>
2020-04-15 17:59:50 -0400

    add DraggableItemAdapter interface to SimpleDataBindingDraggableItemAdapter

--- gto-support-recyclerview-advrecyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/advrecyclerview/draggable/SimpleDataBindingDraggableItemAdapter.kt
+++ gto-support-recyclerview-advrecyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/advrecyclerview/draggable/SimpleDataBindingDraggableItemAdapter.kt
@@ -2,10 +2,12 @@ package org.ccci.gto.android.common.recyclerview.advrecyclerview.draggable
 
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
+import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemAdapter
 import org.ccci.gto.android.common.recyclerview.adapter.AbstractDataBindingAdapter
 
 abstract class SimpleDataBindingDraggableItemAdapter<B : ViewDataBinding>(lifecycleOwner: LifecycleOwner? = null) :
-    AbstractDataBindingAdapter<B, DataBindingDraggableItemViewHolder<B>>(lifecycleOwner) {
+    AbstractDataBindingAdapter<B, DataBindingDraggableItemViewHolder<B>>(lifecycleOwner),
+    DraggableItemAdapter<DataBindingDraggableItemViewHolder<B>> {
     final override fun onCreateViewHolder(binding: B, viewType: Int) = DataBindingDraggableItemViewHolder(binding)
     final override fun onBindViewHolder(holder: DataBindingDraggableItemViewHolder<B>, binding: B, position: Int) =
         onBindViewDataBinding(binding, position)
