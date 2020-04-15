a1cdcface30f6cccb36e2c0fabc07764e8a387f7
Daniel Frett <daniel.frett@ccci.org>
2020-04-15 16:51:30 -0400

    create a SimpleDataBindingDraggableItemAdapter

---
+++ gto-support-recyclerview-advrecyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/advrecyclerview/draggable/SimpleDataBindingDraggableItemAdapter.kt
@@ -0,0 +1,17 @@
+package org.ccci.gto.android.common.recyclerview.advrecyclerview.draggable
+
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import org.ccci.gto.android.common.recyclerview.adapter.AbstractDataBindingAdapter
+
+abstract class SimpleDataBindingDraggableItemAdapter<B : ViewDataBinding>(lifecycleOwner: LifecycleOwner? = null) :
+    AbstractDataBindingAdapter<B, DataBindingDraggableItemViewHolder<B>>(lifecycleOwner) {
+    final override fun onCreateViewHolder(binding: B, viewType: Int) = DataBindingDraggableItemViewHolder(binding)
+    final override fun onBindViewHolder(holder: DataBindingDraggableItemViewHolder<B>, binding: B, position: Int) =
+        onBindViewDataBinding(binding, position)
+    final override fun onViewRecycled(holder: DataBindingDraggableItemViewHolder<B>, binding: B) =
+        onViewDataBindingRecycled(binding)
+
+    protected abstract fun onBindViewDataBinding(binding: B, position: Int)
+    protected open fun onViewDataBindingRecycled(binding: B) = Unit
+}
