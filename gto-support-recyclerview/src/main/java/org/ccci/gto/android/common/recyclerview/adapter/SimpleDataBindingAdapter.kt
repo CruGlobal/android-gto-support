02ead2481eaf6350428cb0a4856bee9c35defe6a
Daniel Frett <daniel.frett@ccci.org>
2020-04-15 16:31:46 -0400

    extract an AbstractDataBindingAdapter that supports other ViewHolder customizations

--- gto-support-recyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/adapter/SimpleDataBindingAdapter.kt
+++ gto-support-recyclerview/src/main/java/org/ccci/gto/android/common/recyclerview/adapter/SimpleDataBindingAdapter.kt
@@ -1,31 +1,15 @@
 package org.ccci.gto.android.common.recyclerview.adapter
 
-import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.RecyclerView
-import com.karumi.weak.weak
 
 abstract class SimpleDataBindingAdapter<B : ViewDataBinding>(lifecycleOwner: LifecycleOwner? = null) :
-    RecyclerView.Adapter<DataBindingViewHolder<B>>() {
-    private val lifecycleOwner by weak(lifecycleOwner)
+    AbstractDataBindingAdapter<B, DataBindingViewHolder<B>>(lifecycleOwner) {
+    final override fun onCreateViewHolder(binding: B, viewType: Int) = DataBindingViewHolder(binding)
+    final override fun onBindViewHolder(holder: DataBindingViewHolder<B>, binding: B, position: Int) =
+        onBindViewDataBinding(binding, position)
+    final override fun onViewRecycled(holder: DataBindingViewHolder<B>, binding: B) = onViewDataBindingRecycled(binding)
 
-    // region Lifecycle
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        onCreateViewDataBinding(parent, viewType)
-            .also { it.lifecycleOwner = lifecycleOwner }
-            .let { DataBindingViewHolder(it) }
-
-    override fun onBindViewHolder(holder: DataBindingViewHolder<B>, position: Int) {
-        onBindViewDataBinding(holder.binding, position)
-        holder.binding.executePendingBindings()
-    }
-
-    protected abstract fun onCreateViewDataBinding(parent: ViewGroup, viewType: Int): B
     protected abstract fun onBindViewDataBinding(binding: B, position: Int)
-    override fun onViewRecycled(holder: DataBindingViewHolder<B>) = onViewDataBindingRecycled(holder.binding)
     protected open fun onViewDataBindingRecycled(binding: B) = Unit
-
-    // end Lifecycle
 }
