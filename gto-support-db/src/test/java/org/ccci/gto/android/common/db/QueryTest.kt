5f446817bace5876a027fb776abf163589bdb60f
Daniel Frett <daniel.frett@ccci.org>
2020-05-06 14:37:05 -0400

    update QueryTest to not use testCommon

--- gto-support-db/src/test/java/org/ccci/gto/android/common/db/QueryTest.kt
+++ gto-support-db/src/test/java/org/ccci/gto/android/common/db/QueryTest.kt
@@ -2,38 +2,39 @@ package org.ccci.gto.android.common.db
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
-import org.ccci.gto.android.common.db.Contract.RootTable
-import org.ccci.gto.android.common.db.Query.Companion.select
-import org.ccci.gto.android.common.db.model.Root
+import org.ccci.gto.android.common.db.model.Model1
 import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.AnyOf.anyOf
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
 import org.robolectric.annotation.Config
+
+private const val TABLE_NAME = "root"
+private const val FIELD_NAME = "test"
+private val FIELD = Table.forClass(Model1::class.java).field(FIELD_NAME)
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [28])
 class QueryTest {
     @Test
     fun testHavingSql() {
-        val dao = Mockito.mock(AbstractDao::class.java)
-        whenever(dao.tableName(eq(Root::class.java))).thenReturn(RootTable.TABLE_NAME)
-        val having: Expression = RootTable.FIELD_TEST.count().eq(1)
-        val query = select(RootTable::class.java).groupBy(RootTable.FIELD_TEST).having(having)
-        val sqlPair = query.buildSqlHaving(dao)
-        assertThat(sqlPair.first, equalTo("(COUNT (root.test) == 1)"))
+        val dao: AbstractDao = mock()
+        whenever(dao.tableName(eq(Model1::class.java))).thenReturn(TABLE_NAME)
+        val query = Query.select(Model1::class.java).groupBy(FIELD).having(FIELD.count().eq(1))
+        assertEquals("(COUNT ($TABLE_NAME.$FIELD_NAME) == 1)", query.buildSqlHaving(dao).first)
     }
 
     @Test
     fun verifyLimitSql() {
-        val query = select(RootTable::class.java)
-        assertThat<Any?>(query.limit(null).sqlLimit, nullValue())
-        assertThat<Any?>(query.limit(null).offset(10).sqlLimit, nullValue())
-        assertThat(query.limit(5).offset(null).sqlLimit, equalTo("5"))
+        val query = Query.select(Model1::class.java)
+        assertNull(query.limit(null).sqlLimit)
+        assertNull(query.offset(10).limit(null).sqlLimit)
+        assertEquals("5", query.limit(5).offset(null).sqlLimit)
         assertThat(query.limit(5).offset(15).sqlLimit, anyOf(equalTo("5 OFFSET 15"), equalTo("15, 5")))
     }
 }
