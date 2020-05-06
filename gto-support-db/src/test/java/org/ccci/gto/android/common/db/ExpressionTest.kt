4e2c8e72c78567177d65dd0c74c34bc4ebfd31b5
Daniel Frett <daniel.frett@ccci.org>
2020-05-06 14:30:08 -0400

    don't rely on testCommon for ExpressionTest

--- gto-support-db/src/test/java/org/ccci/gto/android/common/db/ExpressionTest.kt
+++ gto-support-db/src/test/java/org/ccci/gto/android/common/db/ExpressionTest.kt
@@ -4,14 +4,16 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
-import org.ccci.gto.android.common.db.Contract.RootTable
-import org.ccci.gto.android.common.db.model.Root
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
+import org.ccci.gto.android.common.db.model.Model1
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+
+private const val TABLE_NAME = "root"
+private const val FIELD_NAME = "test"
+private val FIELD = Table.forClass(Model1::class.java).field("test")
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [28])
@@ -19,33 +21,28 @@ class ExpressionTest {
     private lateinit var dao: AbstractDao
 
     @Before
-    @Throws(Exception::class)
     fun setup() {
         dao = mock()
-        whenever(dao.tableName(eq(Root::class.java))).thenReturn(RootTable.TABLE_NAME)
+        whenever(dao.tableName(eq(Model1::class.java))).thenReturn(TABLE_NAME)
     }
 
     @Test
     fun testEqualsSql() {
-        val equalsExpression: Expression = RootTable.FIELD_TEST.eq("1")
-        assertThat(equalsExpression.buildSql(dao).first, equalTo("(root.test == ?)"))
+        assertEquals("($TABLE_NAME.$FIELD_NAME == ?)", FIELD.eq("1").buildSql(dao).first)
     }
 
     @Test
     fun testNotEqualsSql() {
-        val notEqualsExpression: Expression = RootTable.FIELD_TEST.ne("1")
-        assertThat(notEqualsExpression.buildSql(dao).first, equalTo("(root.test != ?)"))
+        assertEquals("($TABLE_NAME.$FIELD_NAME != ?)", FIELD.ne("1").buildSql(dao).first)
     }
 
     @Test
     fun testCount() {
-        val countExpression: Expression = RootTable.FIELD_TEST.count()
-        assertThat(countExpression.buildSql(dao).first, equalTo("COUNT (root.test)"))
+        assertEquals("COUNT ($TABLE_NAME.$FIELD_NAME)", FIELD.count().buildSql(dao).first)
     }
 
     @Test
     fun testCountInHaving() {
-        val countExpression: Expression = RootTable.FIELD_TEST.count().eq(1)
-        assertThat(countExpression.buildSql(dao).first, equalTo("(COUNT (root.test) == 1)"))
+        assertEquals("(COUNT ($TABLE_NAME.$FIELD_NAME) == 1)", FIELD.count().eq(1).buildSql(dao).first)
     }
 }
