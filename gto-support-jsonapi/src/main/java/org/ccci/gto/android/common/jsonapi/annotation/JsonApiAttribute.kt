90b2a563131155f3addabe114223edaa85ed558b
Daniel Frett <daniel.frett@ccci.org>
2020-05-06 10:09:27 -0400

    convert Annotations to Kotlin

---
+++ gto-support-jsonapi/src/main/java/org/ccci/gto/android/common/jsonapi/annotation/JsonApiAttribute.kt
@@ -0,0 +1,22 @@
+package org.ccci.gto.android.common.jsonapi.annotation
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class JsonApiAttribute(
+    /**
+     * Specify the name of this attribute when serialized to/deserialized from jsonapi.
+     */
+    val name: String = "",
+    /**
+     * Alias for [JsonApiAttribute.name].
+     */
+    val value: String = "",
+    /**
+     * Should this attribute be serialized by the [JsonApiConverter]
+     */
+    val serialize: Boolean = true,
+    /**
+     * Should this attribute be deserialized by the [JsonApiConverter]
+     */
+    val deserialize: Boolean = true
+)
---
+++ gto-support-jsonapi/src/main/java/org/ccci/gto/android/common/jsonapi/annotation/JsonApiIgnore.kt
@@ -0,0 +1,5 @@
+package org.ccci.gto.android.common.jsonapi.annotation
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class JsonApiIgnore
