6b6f1ee969818c024ea52e171cde5698d15d0027
Daniel Frett <daniel.frett@ccci.org>
2020-05-12 17:18:55 -0400

    reorganize Transformations

--- gto-support-androidx-lifecycle/src/main/java/org/ccci/gto/android/common/androidx/lifecycle/Transformations.kt
+++ gto-support-androidx-lifecycle/src/main/java/org/ccci/gto/android/common/androidx/lifecycle/Transformations.kt
@@ -12,6 +12,57 @@ import androidx.lifecycle.map
 /**
  * This method will combine 2 LiveData objects into a new LiveData object by running the {@param mapFunction} on the
  * current values of both source LiveData objects.
+ *
+ * @see androidx.lifecycle.Transformations.map
+ */
+@JvmName("combine")
+fun <IN1, IN2, OUT> LiveData<IN1>.combineWith(
+    other: LiveData<IN2>,
+    mapFunction: (IN1, IN2) -> OUT
+) = combineWithInt(this, other) {
+    @Suppress("UNCHECKED_CAST")
+    mapFunction(value as IN1, other.value as IN2)
+}
+
+/**
+ * This method will combine 3 LiveData objects into a new LiveData object by running the {@param mapFunction} on the
+ * current values of both source LiveData objects.
+ *
+ * @see androidx.lifecycle.Transformations.map
+ */
+@JvmName("combine")
+fun <IN1, IN2, IN3, OUT> LiveData<IN1>.combineWith(
+    other: LiveData<IN2>,
+    other2: LiveData<IN3>,
+    mapFunction: (IN1, IN2, IN3) -> OUT
+) = combineWithInt(this, other, other2) {
+    @Suppress("UNCHECKED_CAST")
+    mapFunction(value as IN1, other.value as IN2, other2.value as IN3)
+}
+
+private inline fun <OUT> combineWithInt(
+    vararg input: LiveData<*>,
+    crossinline mapFunction: () -> OUT
+): LiveData<OUT> {
+    val result = MediatorLiveData<OUT>()
+    val observer = object : Observer<Any?> {
+        private var isInitialized = false
+            get() = field || input.all { it.isInitialized }.also { field = it }
+
+        override fun onChanged(t: Any?) {
+            if (!isInitialized) return
+            result.value = mapFunction()
+        }
+    }
+    input.forEach { result.addSource(it, observer) }
+    return result
+}
+
+fun <T> LiveData<out Iterable<T>>.sortedWith(comparator: Comparator<in T>) = map { it.sortedWith(comparator) }
+
+/**
+ * This method will combine 2 LiveData objects into a new LiveData object by running the {@param mapFunction} on the
+ * current values of both source LiveData objects.
  * Returning either of the original LiveData objects will cause an Exception.
  *
  * @see androidx.lifecycle.Transformations.switchMap
@@ -82,57 +133,6 @@ private inline fun <OUT> switchCombineWithInt(
     input.forEach { result.addSource(it, observer) }
     return result
 }
-
-/**
- * This method will combine 2 LiveData objects into a new LiveData object by running the {@param mapFunction} on the
- * current values of both source LiveData objects.
- *
- * @see androidx.lifecycle.Transformations.map
- */
-@JvmName("combine")
-fun <IN1, IN2, OUT> LiveData<IN1>.combineWith(
-    other: LiveData<IN2>,
-    mapFunction: (IN1, IN2) -> OUT
-) = combineWithInt(this, other) {
-    @Suppress("UNCHECKED_CAST")
-    mapFunction(value as IN1, other.value as IN2)
-}
-
-/**
- * This method will combine 3 LiveData objects into a new LiveData object by running the {@param mapFunction} on the
- * current values of both source LiveData objects.
- *
- * @see androidx.lifecycle.Transformations.map
- */
-@JvmName("combine")
-fun <IN1, IN2, IN3, OUT> LiveData<IN1>.combineWith(
-    other: LiveData<IN2>,
-    other2: LiveData<IN3>,
-    mapFunction: (IN1, IN2, IN3) -> OUT
-) = combineWithInt(this, other, other2) {
-    @Suppress("UNCHECKED_CAST")
-    mapFunction(value as IN1, other.value as IN2, other2.value as IN3)
-}
-
-private inline fun <OUT> combineWithInt(
-    vararg input: LiveData<*>,
-    crossinline mapFunction: () -> OUT
-): LiveData<OUT> {
-    val result = MediatorLiveData<OUT>()
-    val observer = object : Observer<Any?> {
-        private var isInitialized = false
-            get() = field || input.all { it.isInitialized }.also { field = it }
-
-        override fun onChanged(t: Any?) {
-            if (!isInitialized) return
-            result.value = mapFunction()
-        }
-    }
-    input.forEach { result.addSource(it, observer) }
-    return result
-}
-
-fun <T> LiveData<out Iterable<T>>.sortedWith(comparator: Comparator<in T>) = map { it.sortedWith(comparator) }
 
 /**
  * Transform a LiveData to return an initial value before it has had a chance to resolve it's actual value.
