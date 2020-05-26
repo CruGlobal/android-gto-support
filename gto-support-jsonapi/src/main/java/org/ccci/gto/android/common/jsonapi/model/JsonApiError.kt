8244a31c25933e6ea117de9ca53e652ce7250d9c
Daniel Frett <daniel.frett@ccci.org>
2020-05-26 17:02:36 -0400

    convert JsonApiError to Kotlin

---
+++ gto-support-jsonapi/src/main/java/org/ccci/gto/android/common/jsonapi/model/JsonApiError.kt
@@ -0,0 +1,25 @@
+package org.ccci.gto.android.common.jsonapi.model
+
+import org.json.JSONObject
+
+class JsonApiError {
+    companion object {
+        const val JSON_ERROR_STATUS = "status"
+        const val JSON_ERROR_DETAIL = "detail"
+        const val JSON_ERROR_SOURCE = "source"
+        const val JSON_ERROR_META = "meta"
+    }
+
+    var status: Int? = null
+    var detail: String? = null
+    var source: Source? = null
+    var rawMeta: JSONObject? = null
+
+    class Source {
+        companion object {
+            const val JSON_ERROR_SOURCE_POINTER = "pointer"
+        }
+
+        var pointer: String? = null
+    }
+}
