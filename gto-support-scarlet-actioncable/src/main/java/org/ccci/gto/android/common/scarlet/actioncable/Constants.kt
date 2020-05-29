2d45bd04cda1004f67ae6bbeabfa2b0c87861321
Daniel Frett <daniel.frett@ccci.org>
2020-05-29 11:51:02 -0400

    create an ActionCableRequestFactory for use with the okhttp socket factory

---
+++ gto-support-scarlet-actioncable/src/main/java/org/ccci/gto/android/common/scarlet/actioncable/Constants.kt
@@ -0,0 +1,4 @@
+package org.ccci.gto.android.common.scarlet.actioncable
+
+internal const val WEBSOCKET_PROTOCOL_ACTIONCABLE_V1 = "actioncable-v1-json"
+internal const val WEBSOCKET_PROTOCOL_ACTIONCABLE = WEBSOCKET_PROTOCOL_ACTIONCABLE_V1
