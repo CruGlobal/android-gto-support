86fcc6e92cfaf20943a72e04668ab1fc3a59eec6
Daniel Frett <daniel.frett@ccci.org>
2020-06-09 13:58:45 -0400

    use a LinkedHashSet() instead of buildList
    This combines the 2 pieces of feedback that Bill gave.
    It leverages a Set to accomplish the `distinct()` functionality without needing to create an intermediate collection, but still maintains order.
    It also avoids using the experimental `buildList` builder by replicating the `buildList` behavior directly on a `LinkedHashSet`.

--- gto-support-util/src/main/java/org/ccci/gto/android/common/util/content/Context.kt
+++ gto-support-util/src/main/java/org/ccci/gto/android/common/util/content/Context.kt
@@ -7,17 +7,16 @@ import android.os.LocaleList
 import org.ccci.gto.android.common.util.os.locales
 import java.util.Locale
 
-@OptIn(ExperimentalStdlibApi::class)
 fun Context.localize(vararg locales: Locale, includeExisting: Boolean = true): Context = when {
     locales.isEmpty() -> this
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 -> createConfigurationContext(Configuration().apply {
         when {
             Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> setLocales(
                 LocaleList(
-                    *buildList<Locale> {
+                    *LinkedHashSet<Locale>().apply {
                         addAll(locales)
                         if (includeExisting) addAll(resources.configuration.locales.locales)
-                    }.distinct().toTypedArray()
+                    }.toTypedArray()
                 )
             )
             else -> setLocale(locales.first())
