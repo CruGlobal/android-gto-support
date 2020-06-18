5e55368a552da9e304e0f96dbd329a0002872aa1
Daniel Frett <daniel.frett@ccci.org>
2020-06-18 15:05:46 -0400

    create a CloseableXmlPullParser

---
+++ gto-support-util/src/main/java/org/ccci/gto/android/common/util/xmlpull/CloseableXmlPullParser.kt
@@ -0,0 +1,31 @@
+package org.ccci.gto.android.common.util.xmlpull
+
+import android.util.Xml
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import java.io.Closeable
+import java.io.InputStream
+import java.io.Reader
+
+class CloseableXmlPullParser private constructor(private val parser: XmlPullParser) :
+        XmlPullParser by parser, Closeable {
+    constructor() : this(Xml.newPullParser())
+
+    private var input: Closeable? = null
+
+    @Throws(XmlPullParserException::class)
+    override fun setInput(reader: Reader?) {
+        input = reader
+        parser.setInput(reader)
+    }
+
+    @Throws(XmlPullParserException::class)
+    override fun setInput(inputStream: InputStream?, inputEncoding: String?) {
+        input = inputStream
+        parser.setInput(inputStream, inputEncoding)
+    }
+
+    override fun close() {
+        input?.close()
+    }
+}
