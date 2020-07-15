68f94b1d55bc1105f5179dfd90a2a76a0f15fdd7
Daniel Frett <daniel.frett@ccci.org>
2020-07-15 10:22:18 -0400

    cleanup some return types

--- gto-support-core/src/main/java/org/ccci/gto/android/common/content/StubContentProvider.kt
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/content/StubContentProvider.kt
@@ -2,20 +2,21 @@ package org.ccci.gto.android.common.content
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.database.Cursor
 import android.net.Uri
 
 class StubContentProvider : ContentProvider() {
     override fun onCreate() = true
-    override fun getType(uri: Uri) = null
+    override fun getType(uri: Uri): String? = null
     override fun query(
         uri: Uri,
         projection: Array<String>?,
         selection: String?,
         selectionArgs: Array<String>?,
         sortOrder: String?
-    ) = null
+    ): Cursor? = null
 
-    override fun insert(uri: Uri, values: ContentValues?) = null
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?) = 0
     override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?) = 0
 }
