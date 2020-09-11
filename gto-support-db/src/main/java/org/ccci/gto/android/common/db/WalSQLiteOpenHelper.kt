32bba6e1cbbf078c40a4c1b0d5315e6416937937
Daniel Frett <daniel.frett@ccci.org>
2020-09-11 17:31:36 -0400

    convert WalSQLiteOpenHelper to Kotlin

---
+++ gto-support-db/src/main/java/org/ccci/gto/android/common/db/WalSQLiteOpenHelper.kt
@@ -0,0 +1,20 @@
+package org.ccci.gto.android.common.db
+
+import android.content.Context
+import android.database.DatabaseErrorHandler
+import android.database.sqlite.SQLiteDatabase.CursorFactory
+import android.database.sqlite.SQLiteOpenHelper
+
+abstract class WalSQLiteOpenHelper @JvmOverloads constructor(
+    context: Context?,
+    name: String?,
+    factory: CursorFactory?,
+    version: Int,
+    errorHandler: DatabaseErrorHandler? = null
+) : SQLiteOpenHelper(context, name, factory, version, errorHandler) {
+    init {
+        setWriteAheadLoggingEnabled(true)
+    }
+
+    final override fun setWriteAheadLoggingEnabled(enabled: Boolean) = super.setWriteAheadLoggingEnabled(enabled)
+}
