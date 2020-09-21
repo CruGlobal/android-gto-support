eb4efbad2abe20f53686255686554ad7a6b22fb1
Daniel Frett <daniel.frett@ccci.org>
2020-09-21 18:26:12 -0400

    add some unit tests for reflection utils

---
+++ gto-support-util/src/test/java/org/ccci/gto/android/common/util/lang/ClassTest.kt
@@ -0,0 +1,34 @@
+package org.ccci.gto.android.common.util.lang
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class ClassTest {
+    @Test
+    fun verifyGetClassOrNull() {
+        assertNull(getClassOrNull("org.ccci.gto.android.common.util.lang.ClassTest\$Invalid"))
+        assertEquals(Test1::class.java, getClassOrNull("org.ccci.gto.android.common.util.lang.ClassTest\$Test1"))
+    }
+
+    @Test
+    fun verifyGetMethod() {
+        assertNull(Test1::class.java.getDeclaredMethodOrNull("invalid"))
+        assertEquals(
+            Test1::class.java.getDeclaredMethod("method1"),
+            Test1::class.java.getDeclaredMethodOrNull("method1")
+        )
+        assertNull(Test1::class.java.getDeclaredMethodOrNull("method1", String::class.java))
+        assertEquals(
+            Test1::class.java.getDeclaredMethod("method2", String::class.java),
+            Test1::class.java.getDeclaredMethodOrNull("method2", String::class.java)
+        )
+        assertNull(Test1::class.java.getDeclaredMethodOrNull("method2"))
+        assertNull(Test1::class.java.getDeclaredMethodOrNull("method2", Int::class.java))
+    }
+
+    class Test1 {
+        private fun method1() = Unit
+        private fun method2(param: String) = Unit
+    }
+}
