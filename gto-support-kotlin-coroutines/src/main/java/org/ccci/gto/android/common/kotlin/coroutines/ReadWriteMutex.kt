aa05bad34024605ee74e06a88a551e1092904631
Daniel Frett <daniel.frett@ccci.org>
2020-10-07 17:42:55 -0400

    create a read Mutex that only supports locking and unlocking

--- gto-support-kotlin-coroutines/src/main/java/org/ccci/gto/android/common/kotlin/coroutines/ReadWriteMutex.kt
+++ gto-support-kotlin-coroutines/src/main/java/org/ccci/gto/android/common/kotlin/coroutines/ReadWriteMutex.kt
@@ -1,10 +1,12 @@
 package org.ccci.gto.android.common.kotlin.coroutines
 
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
 interface ReadWriteMutex {
     val write: Mutex
+    val read: Mutex
 
     suspend fun <T> withReadLock(action: suspend () -> T): T
 }
@@ -16,22 +18,30 @@ private class ReadWriteMutexImpl : ReadWriteMutex {
     private var readers = 0
 
     override val write = Mutex()
-
-    override suspend fun <T> withReadLock(action: suspend () -> T): T {
-        stateMutex.withLock {
-            // first reader should lock the write mutex
-            if (readers == 0) write.lock()
-            readers++
-        }
-
-        try {
-            return action()
-        } finally {
-            stateMutex.withLock {
-                readers--
-                // release the write mutex lock when this is the last reader
-                if (readers == 0) write.unlock()
+    override val read = object : Mutex {
+        override suspend fun lock(owner: Any?) {
+            stateMutex.withLock(owner) {
+                // first reader should lock the write mutex
+                if (readers == 0) write.lock(owner)
+                readers++
             }
         }
+
+        override fun unlock(owner: Any?) {
+            runBlocking {
+                stateMutex.withLock {
+                    readers--
+                    // release the write mutex lock when this is the last reader
+                    if (readers == 0) write.unlock()
+                }
+            }
+        }
+
+        override val isLocked get() = TODO("Not supported")
+        override val onLock get() = TODO("Not supported")
+        override fun holdsLock(owner: Any) = TODO("Not supported")
+        override fun tryLock(owner: Any?) = TODO("Not supported")
     }
+
+    override suspend fun <T> withReadLock(action: suspend () -> T): T = read.withLock { action() }
 }
