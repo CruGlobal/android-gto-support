ab9c99f8fcf39b435b7cc82b98ee52ebe4337898
Daniel Frett <daniel.frett@ccci.org>
2020-10-12 13:26:37 -0400

    make the Table class a data class

--- gto-support-db/src/main/java/org/ccci/gto/android/common/db/Table.kt
+++ gto-support-db/src/main/java/org/ccci/gto/android/common/db/Table.kt
@@ -6,17 +6,17 @@ import kotlinx.android.parcel.IgnoredOnParcel
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class Table<T : Any> private constructor(
+data class Table<T : Any> internal constructor(
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP) val type: Class<T>,
-    private val alias: String?
+    private val alias: String? = null
 ) : Parcelable {
     companion object {
         @JvmStatic
-        fun <T : Any> forClass(type: Class<T>) = Table(type, null)
+        fun <T : Any> forClass(type: Class<T>) = Table(type)
         inline fun <reified T : Any> forClass() = forClass(T::class.java)
     }
 
-    fun `as`(alias: String?) = Table(type, alias)
+    fun `as`(alias: String?) = copy(alias = alias)
     fun field(field: String) = Expression.Field(this, field)
 
     fun <T2 : Any> join(target: Table<T2>) = Join.create<T, T2>(target)
