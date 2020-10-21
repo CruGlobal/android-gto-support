76533e685ab5f10dd6add6913fb729ac415526f6
Daniel Frett <daniel.frett@ccci.org>
2020-10-21 12:09:03 -0400

    expose an idToken LiveData object

--- gto-support-okta/src/main/java/org/ccci/gto/android/common/okta/oidc/clients/sessions/SessionClientLiveData.kt
+++ gto-support-okta/src/main/java/org/ccci/gto/android/common/okta/oidc/clients/sessions/SessionClientLiveData.kt
@@ -2,6 +2,7 @@ package org.ccci.gto.android.common.okta.oidc.clients.sessions
 
 import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.map
+import com.okta.oidc.OktaIdToken
 import com.okta.oidc.clients.sessions.SessionClient
 import org.ccci.gto.android.common.okta.oidc.storage.ChangeAwareOktaStorage
 import org.ccci.gto.android.common.okta.oidc.storage.changeLiveData
@@ -9,3 +10,6 @@ import org.ccci.gto.android.common.okta.oidc.storage.changeLiveData
 private inline val SessionClient.changeLiveData get() = (oktaStorage as ChangeAwareOktaStorage).changeLiveData
 
 val SessionClient.isAuthenticatedLiveData get() = changeLiveData.map { isAuthenticated }.distinctUntilChanged()
+private val SessionClient.tokensLiveData get() = changeLiveData.map { tokens }
+val SessionClient.idTokenLiveData
+    get() = tokensLiveData.map { it?.idToken }.distinctUntilChanged().map { it?.let { OktaIdToken.parseIdToken(it) } }
