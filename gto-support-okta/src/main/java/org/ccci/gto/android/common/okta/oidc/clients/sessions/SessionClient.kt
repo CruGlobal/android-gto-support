79345064b17bfde0c36ed71c704a699295599d9b
Daniel Frett <daniel.frett@ccci.org>
2020-10-26 14:11:15 -0400

    add extension methods to access the idToken and oktaUserId from the session client

--- gto-support-okta/src/main/java/org/ccci/gto/android/common/okta/oidc/clients/sessions/SessionClient.kt
+++ gto-support-okta/src/main/java/org/ccci/gto/android/common/okta/oidc/clients/sessions/SessionClient.kt
@@ -1,5 +1,6 @@
 package org.ccci.gto.android.common.okta.oidc.clients.sessions
 
+import com.okta.oidc.OktaIdToken
 import com.okta.oidc.clients.sessions.SessionClient
 import com.okta.oidc.clients.sessions.oktaState
 import com.okta.oidc.clients.sessions.syncSessionClient
@@ -8,3 +9,6 @@ import com.okta.oidc.storage.storage
 
 internal val SessionClient.oktaRepo get() = syncSessionClient!!.oktaState!!.oktaRepo!!
 internal val SessionClient.oktaStorage get() = oktaRepo.storage!!
+
+val SessionClient.idToken get() = tokens?.idToken?.let { OktaIdToken.parseIdToken(it) }
+val SessionClient.oktaUserId get() = idToken?.claims?.sub
