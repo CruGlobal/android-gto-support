6fdc5848b9793d43085da54414c555121c7cce5e
Daniel Frett <daniel.frett@ccci.org>
2020-11-05 11:59:06 -0500

    create a Timber LoggerDelegate

---
+++ gto-support-snowplow/src/main/java/org/ccci/gto/android/common/snowplow/utils/TimberLoggerDelegate.kt
@@ -0,0 +1,10 @@
+package org.ccci.gto.android.common.snowplow.utils
+
+import com.snowplowanalytics.snowplow.tracker.LoggerDelegate
+import timber.log.Timber
+
+object TimberLoggerDelegate : LoggerDelegate {
+    override fun error(tag: String, msg: String) = Timber.tag(tag).e(msg)
+    override fun debug(tag: String, msg: String) = Timber.tag(tag).d(msg)
+    override fun verbose(tag: String, msg: String) = Timber.tag(tag).v(msg)
+}
