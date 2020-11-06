e3ae51910b61a7d8925d9bd4ab34bee161612cea
Daniel Frett <daniel.frett@ccci.org>
2020-11-06 10:19:08 -0500

    create an EventSynchronizer that will help synchronize the subject attribute hack

---
+++ gto-support-snowplow/src/main/java/org/ccci/gto/android/common/snowplow/events/EventSynchronizer.kt
@@ -0,0 +1,35 @@
+package org.ccci.gto.android.common.snowplow.events
+
+import androidx.annotation.VisibleForTesting
+import com.snowplowanalytics.snowplow.tracker.events.Event
+import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
+
+internal object EventSynchronizer {
+    @VisibleForTesting
+    internal val semaphore = Semaphore(1, true)
+    private val currentEvent = AtomicReference<Event?>()
+
+    @VisibleForTesting
+    internal var lockTimeout = 2000L
+
+    fun lockFor(event: Event) {
+        while (true) {
+            val curr = currentEvent.get()
+            if (semaphore.tryAcquire(lockTimeout, TimeUnit.MILLISECONDS)) {
+                if (!currentEvent.compareAndSet(null, event)) {
+                    semaphore.release()
+                    continue
+                }
+                break
+            } else if (curr != null && currentEvent.compareAndSet(curr, event)) {
+                break
+            }
+        }
+    }
+
+    fun unlockFor(event: Event) {
+        if (currentEvent.compareAndSet(event, null)) semaphore.release()
+    }
+}
