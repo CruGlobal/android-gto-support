fc71a9bf8234440d9dba9153687792005e3cd9dc
Daniel Frett <daniel.frett@ccci.org>
2020-11-06 10:20:56 -0500

    create Custom events that allow setting arbitrary top level attributes

---
+++ gto-support-snowplow/src/main/java/org/ccci/gto/android/common/snowplow/events/CustomEventBuilder.kt
@@ -0,0 +1,7 @@
+package org.ccci.gto.android.common.snowplow.events
+
+import com.snowplowanalytics.snowplow.tracker.events.AbstractEvent
+
+interface CustomEventBuilder<B : AbstractEvent.Builder<B>> {
+    fun attribute(key: String, value: String): B
+}
---
+++ gto-support-snowplow/src/main/java/org/ccci/gto/android/common/snowplow/events/CustomStructured.kt
@@ -0,0 +1,37 @@
+package org.ccci.gto.android.common.snowplow.events
+
+import com.snowplowanalytics.snowplow.tracker.Tracker
+import com.snowplowanalytics.snowplow.tracker.events.Structured
+
+class CustomStructured(builder: Builder) : Structured(builder) {
+    private val attributes = builder.attributes
+
+    override fun beginProcessing(tracker: Tracker) {
+        EventSynchronizer.lockFor(this)
+        requireNotNull(tracker.subject) { "CustomStructured requires the Tracker to have a subject to work" }
+            .subject.putAll(attributes)
+    }
+
+    override fun endProcessing(tracker: Tracker) {
+        tracker.subject?.subject?.apply {
+            attributes.keys.forEach { remove(it) }
+        }
+        EventSynchronizer.unlockFor(this)
+    }
+
+    companion object {
+        fun builder() = Builder()
+    }
+
+    class Builder : Structured.Builder<Builder>(), CustomEventBuilder<Builder> {
+        internal val attributes = mutableMapOf<String, String>()
+
+        override fun attribute(key: String, value: String): Builder {
+            attributes[key] = value
+            return this
+        }
+
+        override fun self() = this
+        override fun build() = CustomStructured(this)
+    }
+}
