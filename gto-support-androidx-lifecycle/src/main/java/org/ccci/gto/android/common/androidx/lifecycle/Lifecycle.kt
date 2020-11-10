377eb7c83f316170c350d08688cbf1d9ff8056cb
Daniel Frett <daniel.frett@ccci.org>
2020-11-10 10:57:13 -0500

    add lambda functions for onResume and onPause lifecycle events

--- gto-support-androidx-lifecycle/src/main/java/org/ccci/gto/android/common/androidx/lifecycle/Lifecycle.kt
+++ gto-support-androidx-lifecycle/src/main/java/org/ccci/gto/android/common/androidx/lifecycle/Lifecycle.kt
@@ -8,6 +8,12 @@ import androidx.lifecycle.LifecycleOwner
 fun Lifecycle.onStart(block: (owner: LifecycleOwner) -> Unit): LifecycleObserver =
     LambdaLifecycleObserver(onStart = block).also { addObserver(it) }
 
+fun Lifecycle.onResume(block: (owner: LifecycleOwner) -> Unit): LifecycleObserver =
+    LambdaLifecycleObserver(onResume = block).also { addObserver(it) }
+
+fun Lifecycle.onPause(block: (owner: LifecycleOwner) -> Unit): LifecycleObserver =
+    LambdaLifecycleObserver(onPause = block).also { addObserver(it) }
+
 fun Lifecycle.onStop(block: (owner: LifecycleOwner) -> Unit): LifecycleObserver =
     LambdaLifecycleObserver(onStop = block).also { addObserver(it) }
 
@@ -16,11 +22,21 @@ fun Lifecycle.onDestroy(block: (owner: LifecycleOwner) -> Unit): LifecycleObserv
 
 internal class LambdaLifecycleObserver(
     private val onStart: ((owner: LifecycleOwner) -> Unit)? = null,
+    private val onResume: ((owner: LifecycleOwner) -> Unit)? = null,
+    private val onPause: ((owner: LifecycleOwner) -> Unit)? = null,
     private val onStop: ((owner: LifecycleOwner) -> Unit)? = null,
     private val onDestroy: ((owner: LifecycleOwner) -> Unit)? = null
 ) : DefaultLifecycleObserver {
     override fun onStart(owner: LifecycleOwner) {
         onStart?.invoke(owner)
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        onResume?.invoke(owner)
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        onPause?.invoke(owner)
     }
 
     override fun onStop(owner: LifecycleOwner) {
