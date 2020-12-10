f712e93c0f8a5279738b397d1379e823da89eaad
Daniel Frett <daniel.frett@ccci.org>
2020-12-10 17:02:55 -0500

    add an autoPlay binding adapter for LottieAnimationView

--- gto-support-lottie/src/main/java/org/ccci/gto/android/common/lottie/databinding/LottieAnimationViewBindingAdapter.kt
+++ gto-support-lottie/src/main/java/org/ccci/gto/android/common/lottie/databinding/LottieAnimationViewBindingAdapter.kt
@@ -3,8 +3,22 @@ package org.ccci.gto.android.common.lottie.databinding
 import androidx.databinding.BindingAdapter
 import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.LottieDrawable
+import org.ccci.gto.android.common.lottie.R
 
 @BindingAdapter("lottie_loop")
 internal fun LottieAnimationView.bindLoop(loop: Boolean?) {
     repeatCount = if (loop == true) LottieDrawable.INFINITE else 0
+}
+
+@BindingAdapter("lottie_autoPlay")
+internal fun LottieAnimationView.bindAutoPlay(autoPlay: Boolean) {
+    // short-circuit if autoPlay setting isn't changing
+    if (getTag(R.id.lottie_autoplay_enabled) == autoPlay) return
+
+    if (autoPlay) {
+        playAnimation()
+    } else {
+        pauseAnimation()
+    }
+    setTag(R.id.lottie_autoplay_enabled, autoPlay)
 }
--- gto-support-lottie/src/test/java/org/ccci/gto/android/common/lottie/databinding/LottieAnimationViewBindingAdapterTest.kt
+++ gto-support-lottie/src/test/java/org/ccci/gto/android/common/lottie/databinding/LottieAnimationViewBindingAdapterTest.kt
@@ -2,9 +2,14 @@ package org.ccci.gto.android.common.lottie.databinding
 
 import com.airbnb.lottie.LottieAnimationView
 import com.airbnb.lottie.LottieDrawable
+import com.nhaarman.mockitokotlin2.clearInvocations
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.stub
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.verifyNoMoreInteractions
+import org.ccci.gto.android.common.lottie.R
 import org.junit.Before
 import org.junit.Test
 
@@ -16,6 +21,7 @@ class LottieAnimationViewBindingAdapterTest {
         view = mock()
     }
 
+    // region bindLoop()
     @Test
     fun testBindLoopEnabled() {
         view.bindLoop(true)
@@ -36,4 +42,49 @@ class LottieAnimationViewBindingAdapterTest {
         verify(view).repeatCount = 0
         verifyNoMoreInteractions(view)
     }
+    // endregion bindLoop()
+
+    // region bindAutoPlay()
+    @Test
+    fun testBindAutoPlayInitial() {
+        view.bindAutoPlay(false)
+        verify(view, never()).playAnimation()
+        verify(view).pauseAnimation()
+        verify(view).setTag(R.id.lottie_autoplay_enabled, false)
+        clearInvocations(view)
+
+        view.bindAutoPlay(true)
+        verify(view).playAnimation()
+        verify(view, never()).pauseAnimation()
+        verify(view).setTag(R.id.lottie_autoplay_enabled, true)
+    }
+
+    @Test
+    fun testBindAutoPlayCurrentlyDisabled() {
+        view.stub { on { getTag(R.id.lottie_autoplay_enabled) } doReturn false }
+
+        view.bindAutoPlay(false)
+        verify(view).getTag(R.id.lottie_autoplay_enabled)
+        verifyNoMoreInteractions(view)
+
+        view.bindAutoPlay(true)
+        verify(view).playAnimation()
+        verify(view, never()).pauseAnimation()
+        verify(view).setTag(R.id.lottie_autoplay_enabled, true)
+    }
+
+    @Test
+    fun testBindAutoPlayCurrentlyEnabled() {
+        view.stub { on { getTag(R.id.lottie_autoplay_enabled) } doReturn true }
+
+        view.bindAutoPlay(true)
+        verify(view).getTag(R.id.lottie_autoplay_enabled)
+        verifyNoMoreInteractions(view)
+
+        view.bindAutoPlay(false)
+        verify(view, never()).playAnimation()
+        verify(view).pauseAnimation()
+        verify(view).setTag(R.id.lottie_autoplay_enabled, false)
+    }
+    // endregion bindAutoPlay()
 }
