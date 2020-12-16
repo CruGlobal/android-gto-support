8da853400be682f67a91858e5882c1e7f220aa3c
Daniel Frett <daniel.frett@ccci.org>
2020-12-16 16:15:54 -0500

    rethrow the first seen exception when uprooting ExceptionRaisingTree
    this will resurface any exceptions that might have been swallowed elsewhere

--- testing/gto-support-timber/src/main/java/org/ccci/gto/android/common/testing/timber/ExceptionRaisingTree.kt
+++ testing/gto-support-timber/src/main/java/org/ccci/gto/android/common/testing/timber/ExceptionRaisingTree.kt
@@ -3,10 +3,19 @@ package org.ccci.gto.android.common.testing.timber
 import timber.log.Timber
 
 object ExceptionRaisingTree : Timber.Tree() {
+    private var seen: Throwable? = null
+
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        seen = seen ?: t
         if (t != null) throw t
     }
 
     fun plant() = Timber.plant(this)
-    fun uproot() = Timber.uproot(this)
+    fun uproot() {
+        Timber.uproot(this)
+        seen?.let {
+            seen = null
+            throw it
+        }
+    }
 }
