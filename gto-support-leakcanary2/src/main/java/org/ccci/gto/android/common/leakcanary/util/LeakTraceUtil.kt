652ec81eae251b70572e0e08015193c6fa9d5c37
Daniel Frett <daniel.frett@ccci.org>
2021-01-13 10:10:49 -0500

    reverse the reference path when generating a fake Exception

--- gto-support-leakcanary2/src/main/java/org/ccci/gto/android/common/leakcanary/util/LeakTraceUtil.kt
+++ gto-support-leakcanary2/src/main/java/org/ccci/gto/android/common/leakcanary/util/LeakTraceUtil.kt
@@ -3,13 +3,14 @@ package org.ccci.gto.android.common.leakcanary.util
 import shark.LeakTrace
 
 fun LeakTrace.asFakeException(): Exception {
-    val firstElement = referencePath[0]
+    val lastElement = referencePath.last()
 
     return RuntimeException(
-        "${leakingObject.classSimpleName} leak from ${firstElement.originObject.classSimpleName} " +
-            "(name=${firstElement.referenceName}, type=${firstElement.referenceType})"
+        "${leakingObject.classSimpleName} leak from ${lastElement.originObject.classSimpleName} " +
+            "(name=${lastElement.referenceName}, type=${lastElement.referenceType})"
     ).also { exception ->
         exception.stackTrace = referencePath
+            .reversed()
             .map {
                 StackTraceElement(
                     it.originObject.className, it.referenceName, "${it.originObject.classSimpleName}.java", 42
