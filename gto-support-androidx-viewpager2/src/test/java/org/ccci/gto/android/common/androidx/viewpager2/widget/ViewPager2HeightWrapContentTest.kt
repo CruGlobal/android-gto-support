82cc64509e54201bcd795c1726eb20a187016606
Daniel Frett <daniel.frett@ccci.org>
2021-03-04 13:08:28 -0500

    simplify the ViewPager2HeightWrapContentTest

--- gto-support-androidx-viewpager2/src/test/java/org/ccci/gto/android/common/androidx/viewpager2/widget/ViewPager2HeightWrapContentTest.kt
+++ gto-support-androidx-viewpager2/src/test/java/org/ccci/gto/android/common/androidx/viewpager2/widget/ViewPager2HeightWrapContentTest.kt
@@ -1,6 +1,5 @@
 package org.ccci.gto.android.common.androidx.viewpager2.widget
 
-import android.app.Activity
 import android.view.View
 import android.view.View.MeasureSpec.makeMeasureSpec
 import android.view.ViewGroup
@@ -10,6 +9,7 @@ import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.viewpager2.widget.ViewPager2
 import org.junit.Assert.assertEquals
@@ -17,26 +17,18 @@ import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
-import org.robolectric.android.controller.ActivityController
 import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @Config(sdk = [16, 17, 21, 28])
 class ViewPager2HeightWrapContentTest {
-    private lateinit var activityController: ActivityController<Activity>
-    private lateinit var activity: Activity
     private lateinit var framelayout: FrameLayout
     private lateinit var viewpager: ViewPager2
 
     @Before
     fun setUp() {
-        activityController = Robolectric.buildActivity(Activity::class.java)
-        activity = activityController.get()
-
-        framelayout = FrameLayout(activity)
+        framelayout = FrameLayout(ApplicationProvider.getApplicationContext())
         framelayout.layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
-        activity.setContentView(framelayout)
 
         viewpager = ViewPager2(framelayout.context)
         viewpager.layoutParams = LayoutParams(MATCH_PARENT, WRAP_CONTENT)
