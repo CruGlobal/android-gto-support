0322e35f484ac1192d8236a87e8568221b7b379c
Daniel Frett <daniel.frett@ccci.org>
2021-03-04 14:53:48 -0500

    add a not about this being a bug workaround

--- gto-support-androidx-viewpager2/src/main/java/org/ccci/gto/android/common/androidx/viewpager2/widget/ViewPager2.kt
+++ gto-support-androidx-viewpager2/src/main/java/org/ccci/gto/android/common/androidx/viewpager2/widget/ViewPager2.kt
@@ -3,6 +3,9 @@ package org.ccci.gto.android.common.androidx.viewpager2.widget
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 
+/**
+ * Provides a work-around for https://issuetracker.google.com/issues/181785654
+ */
 fun ViewPager2.whileMaintainingVisibleCurrentItem(block: (RecyclerView.Adapter<*>?) -> Unit) {
     val adapter = adapter
     val visible = adapter?.takeIf { it.hasStableIds() }?.getItemId(currentItem)
