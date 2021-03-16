1c514a78e5b1e47258ebb73fecce6f4e0a53cc02
Daniel Frett <daniel.frett@ccci.org>
2021-03-16 11:17:14 -0400

    keep a reference to the Picasso Target so it doesn't get garbage collected before it is processed

--- gto-support-picasso/src/main/java/org/ccci/gto/android/common/picasso/RequestCreatorCoroutines.kt
+++ gto-support-picasso/src/main/java/org/ccci/gto/android/common/picasso/RequestCreatorCoroutines.kt
@@ -5,17 +5,20 @@ import android.graphics.drawable.Drawable
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.RequestCreator
 import com.squareup.picasso.Target
-import kotlin.coroutines.Continuation
+import java.util.concurrent.atomic.AtomicReference
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
+import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 
-suspend fun RequestCreator.getBitmap(): Bitmap =
-    withContext(Dispatchers.Main) { suspendCoroutine { into(BitmapContinuationTarget(it)) } }
+suspend fun RequestCreator.getBitmap(): Bitmap = withContext(Dispatchers.Main) {
+    val target = AtomicReference<BitmapContinuationTarget>()
+    suspendCancellableCoroutine { cont -> into(BitmapContinuationTarget(cont).also { target.set(it) }) }
+}
 
-private class BitmapContinuationTarget(private val cont: Continuation<Bitmap>) : Target {
+private class BitmapContinuationTarget(private val cont: CancellableContinuation<Bitmap>) : Target {
     override fun onPrepareLoad(placeHolderDrawable: Drawable?) = Unit
     override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom?) = cont.resume(bitmap)
     override fun onBitmapFailed(e: Exception, errorDrawable: Drawable?) = cont.resumeWithException(e)
