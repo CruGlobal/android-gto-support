8c37a729f61d1e4fae9938a703d47b6421b08f0b
Daniel Frett <daniel.frett@ccci.org>
2021-05-13 14:49:55 -0400

    remove unnecessary pre-Lollipop LocaleUtil support

--- gto-support-util/src/test/java/org/ccci/gto/android/common/util/LocaleUtilsFallbackTest.kt
+++ gto-support-util/src/test/java/org/ccci/gto/android/common/util/LocaleUtilsFallbackTest.kt
@@ -1,22 +1,18 @@
 package org.ccci.gto.android.common.util
 
-import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import java.util.Locale
-import org.ccci.gto.android.common.compat.util.LocaleCompat
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.greaterThanOrEqualTo
-import org.junit.Assume.assumeThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
-private val MALAY = LocaleCompat.forLanguageTag("ms")
-private val BENGKULU = LocaleCompat.forLanguageTag("pse")
+private val MALAY = Locale.forLanguageTag("ms")
+private val BENGKULU = Locale.forLanguageTag("pse")
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk = [16, 17, 18, 19, 21, 24])
+@Config(sdk = [21, 24])
 class LocaleUtilsFallbackTest {
     @Test
     fun verifyGetFallback() {
@@ -26,16 +22,12 @@ class LocaleUtilsFallbackTest {
 
     @Test
     fun verifyGetFallbackWithScript() {
-        assumeScriptSupported()
-
-        assertThat(LocaleUtils.getFallback(LocaleCompat.forLanguageTag("zh-Hans")), equalTo(Locale.CHINESE))
+        assertThat(LocaleUtils.getFallback(Locale.forLanguageTag("zh-Hans")), equalTo(Locale.CHINESE))
     }
 
     @Test
     fun verifyGetFallbackWithExtensions() {
-        assumeExtensionSupported()
-
-        assertThat(LocaleUtils.getFallback(LocaleCompat.forLanguageTag("en-x-private")), equalTo(Locale.ENGLISH))
+        assertThat(LocaleUtils.getFallback(Locale.forLanguageTag("en-x-private")), equalTo(Locale.ENGLISH))
     }
 
     @Test
@@ -47,12 +39,10 @@ class LocaleUtilsFallbackTest {
 
     @Test
     fun verifyGetFallbacksWithScript() {
-        assumeScriptSupported()
-
-        val locale = LocaleCompat.forLanguageTag("zh-Hans-CN")
+        val locale = Locale.forLanguageTag("zh-Hans-CN")
         assertThat(
             LocaleUtils.getFallbacks(locale),
-            equalTo(arrayOf(locale, LocaleCompat.forLanguageTag("zh-Hans"), Locale.CHINESE))
+            equalTo(arrayOf(locale, Locale.forLanguageTag("zh-Hans"), Locale.CHINESE))
         )
     }
 
@@ -66,8 +56,3 @@ class LocaleUtilsFallbackTest {
         )
     }
 }
-
-// script & extension support is only available starting with Lollipop
-private fun assumeLollipop() = assumeThat(Build.VERSION.SDK_INT, greaterThanOrEqualTo(Build.VERSION_CODES.LOLLIPOP))
-private fun assumeScriptSupported() = assumeLollipop()
-private fun assumeExtensionSupported() = assumeLollipop()
