eb572ca989ada61613c68d15021b433819f875d4
Daniel Frett <daniel.frett@ccci.org>
2021-06-18 14:49:07 -0400

    remove v3.6.2 deprecated Flipper simple db objects

--- gto-support-facebook-flipper/src/main/kotlin/org/ccci/gto/android/common/facebook/flipper/plugins/databases/DefaultSqliteDatabaseProvider.kt
+++
@@ -1,7 +0,0 @@
-package org.ccci.gto.android.common.facebook.flipper.plugins.databases
-
-import android.content.Context
-import com.facebook.flipper.plugins.databases.impl.DefaultSqliteDatabaseProvider
-
-@Deprecated("Since v3.6.2, use DefaultSqliteDatabaseProvider from flipper directly")
-class DefaultSqliteDatabaseProvider(context: Context) : DefaultSqliteDatabaseProvider(context)
