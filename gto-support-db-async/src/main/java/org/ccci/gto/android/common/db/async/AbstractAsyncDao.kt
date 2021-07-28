a1c86b6891c3e79f24b4e81b6ccea270920403b6
Daniel Frett <daniel.frett@ccci.org>
2021-07-28 16:23:54 -0400

    remove v3.4.0 deprecated AbstractAsyncDao

--- gto-support-db-async/src/main/java/org/ccci/gto/android/common/db/async/AbstractAsyncDao.kt
+++
@@ -1,8 +0,0 @@
-package org.ccci.gto.android.common.db.async
-
-import android.database.sqlite.SQLiteOpenHelper
-import org.ccci.gto.android.common.db.AbstractDao
-import org.ccci.gto.android.common.db.AsyncDao
-
-@Deprecated("Since v3.4.0, extend AbstractDao and implement AsyncDao instead.")
-abstract class AbstractAsyncDao protected constructor(helper: SQLiteOpenHelper) : AbstractDao(helper), AsyncDao
