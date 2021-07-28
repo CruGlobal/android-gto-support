2639623b4a46e241a881a304549aea45b7d511f1
Daniel Frett <daniel.frett@ccci.org>
2021-07-28 16:25:40 -0400

    remove deprecated `@JvmDefault` annotations

--- gto-support-db-async/src/main/java/org/ccci/gto/android/common/db/AsyncDao.kt
+++ gto-support-db-async/src/main/java/org/ccci/gto/android/common/db/AsyncDao.kt
@@ -5,35 +5,22 @@ import androidx.concurrent.futures.CallbackToFutureAdapter
 import com.google.common.util.concurrent.ListenableFuture
 
 interface AsyncDao : Dao {
-    @JvmDefault
     fun <T : Any> getAsync(query: Query<T>) = runAsync { get(query) }
 
-    @JvmDefault
     fun getCursorAsync(query: Query<*>) = runAsync { getCursor(query) }
 
-    @JvmDefault
     fun <T : Any> findAsync(clazz: Class<T>, vararg key: Any) = runAsync { find(clazz, *key) }
 
-    @JvmDefault
     fun insertAsync(obj: Any) = insertAsync(obj, SQLiteDatabase.CONFLICT_NONE)
-
-    @JvmDefault
     fun insertAsync(obj: Any, conflictAlgorithm: Int) = runAsync { insert(obj, conflictAlgorithm) }
 
-    @JvmDefault
     fun updateAsync(obj: Any) = updateAsync(obj, *getFullProjection(obj.javaClass))
-
-    @JvmDefault
     fun updateAsync(obj: Any, vararg projection: String) = runAsync { update(obj, *projection) }
 
-    @JvmDefault
     fun <T : Any> updateAsync(sample: T, where: Expression?, vararg projection: String) =
         runAsync { update(sample, where, *projection) }
 
-    @JvmDefault
     fun updateOrInsertAsync(obj: Any) = updateOrInsertAsync(obj, *getFullProjection(obj.javaClass))
-
-    @JvmDefault
     fun updateOrInsertAsync(obj: Any, vararg projection: String) = runAsync { updateOrInsert(obj, *projection) }
 
     companion object {
