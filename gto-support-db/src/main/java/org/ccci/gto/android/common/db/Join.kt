43ea51655c39f1c5ee1afefb55e6a9adb64db368
Daniel Frett <daniel.frett@ccci.org>
2021-07-28 20:58:00 -0400

    remove v3.3.0 deprecated Join.create method

--- gto-support-db/src/main/java/org/ccci/gto/android/common/db/Join.kt
+++ gto-support-db/src/main/java/org/ccci/gto/android/common/db/Join.kt
@@ -19,10 +19,6 @@ data class Join<S : Any, T : Any> private constructor(
         val NO_JOINS = emptyArray<Join<*, *>>()
 
         @JvmStatic
-        @Deprecated("Since v3.3.0, use source.join() instead", ReplaceWith("source.join(target)"))
-        fun <S : Any, T : Any> create(source: Table<S>, target: Table<T>) = source.join(target)
-
-        @JvmStatic
         fun <S : Any, T : Any> create(target: Table<T>) = Join<S, T>(target = target)
     }
 
