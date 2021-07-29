6a499cc687e9c0e5d730489d7cd55405d81473c6
Daniel Frett <daniel.frett@ccci.org>
2021-07-28 20:55:19 -0400

    remove unnecessary `@JvmDefault` annotation

--- gto-support-db-stream/src/main/java/org/ccci/gto/android/common/db/StreamDao.kt
+++ gto-support-db-stream/src/main/java/org/ccci/gto/android/common/db/StreamDao.kt
@@ -6,7 +6,6 @@ import com.annimon.stream.Stream
  * Stream DAO interface relying on backwards compatible Lightweight Stream API.
  */
 interface StreamDao : Dao {
-    @JvmDefault
     fun <T : Any> streamCompat(query: Query<T>): Stream<T> = Stream.of(get(query))
 
     object StreamHelper {
