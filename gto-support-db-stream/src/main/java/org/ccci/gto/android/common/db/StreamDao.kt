1b6fd91d3136bd23dcf89dabaf2c2d810906c2c7
Daniel Frett <daniel.frett@ccci.org>
2021-07-28 20:56:16 -0400

    remove v3.4.0 deprecated StreamDao helper

--- gto-support-db-stream/src/main/java/org/ccci/gto/android/common/db/StreamDao.kt
+++ gto-support-db-stream/src/main/java/org/ccci/gto/android/common/db/StreamDao.kt
@@ -7,10 +7,4 @@ import com.annimon.stream.Stream
  */
 interface StreamDao : Dao {
     fun <T : Any> streamCompat(query: Query<T>): Stream<T> = Stream.of(get(query))
-
-    object StreamHelper {
-        @JvmStatic
-        @Deprecated("Since v3.4.0, use default implementation of streamCompat instead.")
-        fun <T : Any> stream(dao: Dao, query: Query<T>): Stream<T> = Stream.of(dao.get(query))
-    }
 }
