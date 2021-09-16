56b202483cb2e989bcf0cf7a00870fd24e930777
Daniel Frett <daniel.frett@ccci.org>
2021-09-16 16:21:37 -0400

    add binding adapter for Slider values

---
+++ gto-support-material-components/src/main/java/org/ccci/gto/android/common/material/slider/databinding/SliderAdapters.kt
@@ -0,0 +1,21 @@
+@file:InverseBindingMethods(InverseBindingMethod(type = Slider::class, attribute = "value"))
+package org.ccci.gto.android.common.material.slider.databinding
+
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingListener
+import androidx.databinding.InverseBindingMethod
+import androidx.databinding.InverseBindingMethods
+import androidx.databinding.adapters.ListenerUtil
+import com.google.android.material.slider.Slider
+import org.ccci.gto.android.common.material.components.R
+
+private const val VALUE_ATTR_CHANGED = "valueAttrChanged"
+
+@BindingAdapter(VALUE_ATTR_CHANGED)
+internal fun Slider.setValueAttrChangedListener(listener: InverseBindingListener?) =
+    listener?.let { Slider.OnChangeListener { _, _, _ -> listener.onChange() } }
+        .also {
+            ListenerUtil.trackListener(this, it, R.id.gto_support_material_components_Slider_valueAttrChangedListener)
+                ?.let { old -> removeOnChangeListener(old) }
+        }
+        ?.let { addOnChangeListener(it) }
