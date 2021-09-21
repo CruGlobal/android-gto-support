60dd13a7ef97e747327335391fce49c01c67e9cf
Daniel Frett <daniel.frett@ccci.org>
2021-09-21 19:10:34 -0400

    convert gto-support-db-coroutines build script to Kotlin

--- gto-support-db-coroutines/build.gradle.kts
+++ gto-support-db-coroutines/build.gradle.kts
@@ -1,10 +1,10 @@
 dependencies {
-    api project(':gto-support-db')
+    api(project(":gto-support-db"))
 
-    api libs.kotlin.coroutines
+    api(libs.kotlin.coroutines)
 
     // TODO: These dependencies are temporary for coroutines flow support.
     //       They should go away once we implement proper flow support
-    api project(':gto-support-db-livedata')
-    api libs.androidx.lifecycle.livedata.ktx
+    api(project(":gto-support-db-livedata"))
+    api(libs.androidx.lifecycle.livedata.ktx)
 }
