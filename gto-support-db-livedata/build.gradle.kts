31b0ba0a203074c60d13b6fcc6d17df97ae19f19
Daniel Frett <daniel.frett@ccci.org>
2021-09-21 19:05:20 -0400

    convert gto-support-db-livedata build script to Kotlin

---
+++ gto-support-db-livedata/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    api(project(":gto-support-db"))
+
+    implementation(libs.androidx.collection)
+    api(libs.androidx.lifecycle.livedata)
+
+    testImplementation(libs.androidx.arch.core.testing)
+    testImplementation(libs.kotlin.coroutines)
+}
