1c35777c8828a04c7becf13a46eb3621cbb59936
Daniel Frett <daniel.frett@ccci.org>
2021-09-22 10:02:12 -0400

    convert gto-support-picasso build script to Kotlin

---
+++ gto-support-picasso/build.gradle.kts
@@ -0,0 +1,40 @@
+android {
+    defaultConfig.consumerProguardFiles(
+        "proguard-consumer.pro",
+        "proguard-consumer-requestcreator.pro"
+    )
+
+    buildFeatures.dataBinding = true
+    dataBinding.addDefaultAdapters = false
+}
+
+dependencies {
+    api(libs.picasso)
+
+    implementation(project(":gto-support-base"))
+    implementation(project(":gto-support-compat"))
+    implementation(project(":gto-support-util"))
+
+    implementation(libs.timber)
+
+    compileOnly(libs.androidx.core.ktx)
+
+    // region Data Binding Adapters
+    compileOnly(libs.androidx.databinding.runtime)
+    // endregion Data Binding Adapters
+
+    // region Material Component Targets
+    compileOnly(libs.materialComponents)
+    // endregion Material Component Targets
+
+    // region SimplePicassoImageView
+    compileOnly(libs.androidx.appcompat)
+    // endregion SimplePicassoImageView
+
+    compileOnly(libs.kotlin.coroutines)
+
+    testImplementation(project(":testing:gto-support-picasso"))
+    testImplementation(libs.materialComponents)
+    testImplementation(libs.kotlin.coroutines.android)
+    testImplementation(libs.kotlin.coroutines.test)
+}
