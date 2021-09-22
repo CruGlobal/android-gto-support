5f01921c4f434ec5575f3fe692a4e875612048db
Daniel Frett <daniel.frett@ccci.org>
2021-09-22 13:26:25 -0400

    convert gto-support-viewpager build script to Kotlin

---
+++ gto-support-viewpager/build.gradle.kts
@@ -0,0 +1,23 @@
+android {
+    defaultConfig.consumerProguardFiles("proguard-consumer.pro")
+}
+
+dependencies {
+    api(libs.androidx.viewpager)
+
+    implementation(project(":gto-support-androidx-collection"))
+    implementation(project(":gto-support-util"))
+
+    implementation(libs.timber)
+    implementation(libs.weakdelegate)
+
+    compileOnly(libs.androidx.fragment)
+
+    // region Data Binding PagerAdapter dependencies
+    compileOnly(libs.androidx.databinding.runtime)
+    // endregion Data Binding PagerAdapter dependencies
+
+    // region SwipeRefreshLayoutViewPagerHelper
+    compileOnly(libs.androidx.swiperefreshlayout)
+    // endregion SwipeRefreshLayoutViewPagerHelper
+}
