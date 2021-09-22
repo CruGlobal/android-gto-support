dcf1a90fd096de46243e136c951a3eb5accda4a9
Daniel Frett <daniel.frett@ccci.org>
2021-09-22 09:01:13 -0400

    convert gto-support-recyclerview build script to Kotlin

---
+++ gto-support-recyclerview/build.gradle.kts
@@ -0,0 +1,12 @@
+android {
+    defaultConfig.consumerProguardFiles("proguard-consumer.pro")
+}
+
+dependencies {
+    api(libs.androidx.recyclerview)
+    implementation(libs.weakdelegate)
+
+    // region *DataBindingAdapter
+    compileOnly(libs.androidx.databinding.runtime)
+    // endregion *DataBindingAdapter
+}
