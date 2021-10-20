854b8e940c6980a466700d7603abb8e1e5d1dbaf
Daniel Frett <daniel.frett@ccci.org>
2021-10-20 11:13:53 -0400

    remove unnecessary override for older versions of Android we no longer support

--- gto-support-core/src/main/java/org/ccci/gto/android/common/preference/ListPreferenceCompat.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/preference/ListPreferenceCompat.java
@@ -4,7 +4,6 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.preference.ListPreference;
-import android.text.TextUtils;
 import android.util.AttributeSet;
 
 public class ListPreferenceCompat extends ListPreference {
@@ -25,26 +24,5 @@ public class ListPreferenceCompat extends ListPreference {
     public ListPreferenceCompat(final Context context, final AttributeSet attrs, final int defStyleAttr,
                                 final int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-    }
-
-    /**
-     * Sets the value of the key. This should be one of the entries in
-     * {@link #getEntryValues()}.
-     *
-     * @param value The value to set for the key.
-     * @see <a href="http://stackoverflow.com/a/21642401/4721910">http://stackoverflow.com/a/21642401/4721910</a>
-     */
-    @Override
-    public void setValue(final String value) {
-        // Android pre-KitKat didn't let the preference know that data had changed, which caused stale Summaries.
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            String oldValue = getValue();
-            super.setValue(value);
-            if (!TextUtils.equals(value, oldValue)) {
-                notifyChanged();
-            }
-        } else {
-            super.setValue(value);
-        }
     }
 }
