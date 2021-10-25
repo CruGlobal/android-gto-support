4d2e5660736b243493bd2fe58d2cc9c886b32ec8
Daniel Frett <daniel.frett@ccci.org>
2021-10-25 11:41:08 -0400

    deprecate ViewCompat.setClipToOutline
    this compat method is no longer necessary

--- gto-support-compat/src/main/java/org/ccci/gto/android/common/compat/view/ViewCompat.java
+++ gto-support-compat/src/main/java/org/ccci/gto/android/common/compat/view/ViewCompat.java
@@ -1,14 +1,15 @@
 package org.ccci.gto.android.common.compat.view;
 
-import android.os.Build;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 
 public final class ViewCompat {
+    /**
+     * @deprecated Since v3.10.0, use {@link View#setClipToOutline(boolean)} directly.
+     */
+    @Deprecated
     public static void setClipToOutline(@NonNull final View view, final boolean clip) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            view.setClipToOutline(clip);
-        }
+        view.setClipToOutline(clip);
     }
 }
