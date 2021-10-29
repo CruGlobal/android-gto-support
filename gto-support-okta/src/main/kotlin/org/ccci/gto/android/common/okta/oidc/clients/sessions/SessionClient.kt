2d882fefd7a56e40bd5edc0bc91b256242f18429
Daniel Frett <daniel.frett@ccci.org>
2021-10-29 15:00:22 -0400

    add oktaUserId extension vals for a few other objects

--- gto-support-okta/src/main/kotlin/org/ccci/gto/android/common/okta/oidc/clients/sessions/SessionClient.kt
+++ gto-support-okta/src/main/kotlin/org/ccci/gto/android/common/okta/oidc/clients/sessions/SessionClient.kt
@@ -1,14 +1,15 @@
 package org.ccci.gto.android.common.okta.oidc.clients.sessions
 
-import com.okta.oidc.OktaIdToken
 import com.okta.oidc.clients.sessions.SessionClient
 import com.okta.oidc.clients.sessions.oktaState
 import com.okta.oidc.clients.sessions.syncSessionClient
 import com.okta.oidc.oktaRepo
 import com.okta.oidc.storage.storage
+import org.ccci.gto.android.common.okta.oidc.oktaUserId
+import org.ccci.gto.android.common.okta.oidc.parseIdToken
 
 internal val SessionClient.oktaRepo get() = syncSessionClient!!.oktaState!!.oktaRepo!!
 internal val SessionClient.oktaStorage get() = oktaRepo.storage!!
 
-val SessionClient.idToken get() = tokens?.idToken?.let { OktaIdToken.parseIdToken(it) }
-val SessionClient.oktaUserId get() = idToken?.claims?.sub
+val SessionClient.idToken get() = tokens?.idToken?.parseIdToken()
+val SessionClient.oktaUserId get() = tokens?.oktaUserId
