d58689e6911cffb28920763748dc4fef05686af3
Daniel Frett <daniel.frett@ccci.org>
2021-10-29 07:51:38 -0400

    create a new generic UserIdSession to replace the old TheKeySession
    They are functionaly the same thing, but this moves away from TheKey naming and conventions

---
+++ gto-support-api-base/src/main/kotlin/org/ccci/gto/android/common/api/UserIdSession.kt
@@ -0,0 +1,32 @@
+package org.ccci.gto.android.common.api
+
+import android.content.SharedPreferences
+
+class UserIdSession private constructor(
+    prefs: SharedPreferences?,
+    val userId: String,
+    sessionId: String?,
+    baseAttrName: String
+) : Session(prefs = prefs, id = sessionId, baseAttrName = "$baseAttrName|$userId") {
+    @JvmOverloads
+    constructor(
+        userId: String,
+        sessionId: String?,
+        baseAttrName: String = PREF_SESSION_BASE_NAME
+    ) : this(prefs = null, userId = userId, sessionId = sessionId, baseAttrName = baseAttrName)
+
+    @JvmOverloads
+    constructor(
+        prefs: SharedPreferences,
+        userId: String,
+        baseAttrName: String = PREF_SESSION_BASE_NAME
+    ) : this(prefs = prefs, userId = userId, sessionId = null, baseAttrName = baseAttrName)
+
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        !(other is UserIdSession && javaClass == other.javaClass) -> false
+        else -> userId == other.userId && super.equals(other)
+    }
+
+    override fun hashCode() = super.hashCode() * 31 + userId.hashCode()
+}
