b41ad89619f3246effaf8f664c0d18ffe2c2e8a7
Daniel Frett <daniel.frett@ccci.org>
2021-10-29 11:32:01 -0400

    add unit tests for establishing a session when the loaded session is invalid

--- gto-support-api-okhttp3/src/test/kotlin/org/ccci/gto/android/common/api/okhttp3/interceptor/SessionInterceptorTest.kt
+++ gto-support-api-okhttp3/src/test/kotlin/org/ccci/gto/android/common/api/okhttp3/interceptor/SessionInterceptorTest.kt
@@ -6,15 +6,20 @@ import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import org.ccci.gto.android.common.api.Session
+import org.ccci.gto.android.common.api.okhttp3.InvalidSessionApiException
+import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.RETURNS_DEEP_STUBS
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.stub
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
 
 class SessionInterceptorTest {
     private lateinit var response: Response
@@ -39,23 +44,64 @@ class SessionInterceptorTest {
     @Test
     fun `Existing session is valid`() {
         sessionInterceptor.stub {
-            on { loadSession() } doReturn validSession
+            on { loadSession(any()) } doReturn validSession
         }
 
         sessionInterceptor.intercept(chain)
-        verify(sessionInterceptor.loadSession).invoke(any())
+        verify(sessionInterceptor.establishSession, never()).invoke()
         verify(sessionInterceptor.attachSession).invoke(any(), any())
+    }
+
+    @Test
+    fun `Existing session is invalid - establishSession() returns valid session`() {
+        sessionInterceptor.stub {
+            on { loadSession(any()) } doReturn invalidSession
+            on { establishSession() } doReturn validSession
+        }
+
+        sessionInterceptor.intercept(chain)
+        verify(sessionInterceptor.establishSession).invoke()
+        verify(sessionInterceptor.attachSession).invoke(any(), eq(validSession))
+        verifyNoMoreInteractions(sessionInterceptor.attachSession)
+    }
+
+    @Test
+    fun `Existing session is invalid - establishSession() returns null`() {
+        sessionInterceptor.stub {
+            on { loadSession(any()) } doReturn invalidSession
+            on { establishSession() } doReturn null
+        }
+
+        assertThrows(InvalidSessionApiException::class.java) {
+            sessionInterceptor.intercept(chain)
+        }
+        verify(sessionInterceptor.establishSession).invoke()
+        verify(sessionInterceptor.attachSession, never()).invoke(any(), any())
+    }
+
+    @Test
+    fun `Existing session is null - establishSession() returns valid session`() {
+        sessionInterceptor.stub {
+            on { loadSession(any()) } doReturn null
+            on { establishSession() } doReturn validSession
+        }
+
+        sessionInterceptor.intercept(chain)
+        verify(sessionInterceptor.establishSession).invoke()
+        verify(sessionInterceptor.attachSession).invoke(any(), eq(validSession))
+        verifyNoMoreInteractions(sessionInterceptor.attachSession)
     }
 
     private class MockSession(id: String?) : Session(id)
     private class MockSessionInterceptor(context: Context) : SessionInterceptor<MockSession>(context) {
         val loadSession = mock<(SharedPreferences) -> MockSession?>()
+        val establishSession = mock<() -> MockSession?>()
         val attachSession = mock<(Request, MockSession) -> Request> {
             on { invoke(any(), any()) } doAnswer { it.getArgument(0) }
         }
 
-        fun loadSession() = loadSession(prefs)
         override fun loadSession(prefs: SharedPreferences) = loadSession.invoke(prefs)
-        public override fun attachSession(request: Request, session: MockSession) = attachSession.invoke(request, session)
+        override fun establishSession() = establishSession.invoke()
+        override fun attachSession(request: Request, session: MockSession) = attachSession.invoke(request, session)
     }
 }
