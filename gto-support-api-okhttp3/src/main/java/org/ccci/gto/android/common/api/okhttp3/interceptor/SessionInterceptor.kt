51d04ec4bf2c80f62977c8a047ba82e89e028d87
Daniel Frett <daniel.frett@ccci.org>
2021-10-29 12:27:57 -0400

    cleanup SessionInterceptor logic

--- gto-support-api-okhttp3/src/main/java/org/ccci/gto/android/common/api/okhttp3/interceptor/SessionInterceptor.kt
+++ gto-support-api-okhttp3/src/main/java/org/ccci/gto/android/common/api/okhttp3/interceptor/SessionInterceptor.kt
@@ -12,121 +12,79 @@ import org.ccci.gto.android.common.api.Session
 import org.ccci.gto.android.common.api.okhttp3.EstablishSessionApiException
 import org.ccci.gto.android.common.api.okhttp3.InvalidSessionApiException
 
-abstract class SessionInterceptor<S : Session?> protected constructor(
-    context: Context, returnInvalidSessionResponses: Boolean = DEFAULT_RETURN_INVALID_SESSION_RESPONSES,
+private const val DEFAULT_RETURN_INVALID_SESSION_RESPONSES = false
+
+abstract class SessionInterceptor<S : Session> @JvmOverloads protected constructor(
+    context: Context,
+    private val returnInvalidSessionResponses: Boolean = DEFAULT_RETURN_INVALID_SESSION_RESPONSES,
     prefFile: String? = null
 ) : Interceptor {
-    protected val mLockSession = Any()
-    protected val mContext: Context
-    private val mReturnInvalidSessionResponses: Boolean
-    private val mPrefFile: String
-
-    protected constructor(context: Context, prefFile: String?) : this(
-        context,
-        DEFAULT_RETURN_INVALID_SESSION_RESPONSES,
-        prefFile
-    ) {
+    protected val context: Context = context.applicationContext
+    protected val prefs by lazy {
+        this.context.getSharedPreferences(prefFile ?: javaClass.simpleName, Context.MODE_PRIVATE)
     }
 
-    protected val prefs: SharedPreferences
-        protected get() = mContext.getSharedPreferences(mPrefFile, Context.MODE_PRIVATE)
+    protected val lockSession = Any()
+
+    @JvmField
+    @Deprecated("Since v3.10.0, use lockSession property instead", ReplaceWith("lockSession"))
+    protected val mLockSession = lockSession
+    @JvmField
+    @Deprecated("Since v3.10.0, use context property instead", ReplaceWith("context"))
+    protected val mContext = this.context
+
+    @Deprecated("Since v3.10.0, use named parameters for the constructor instead.")
+    protected constructor(context: Context, prefFile: String?) :
+        this(context, DEFAULT_RETURN_INVALID_SESSION_RESPONSES, prefFile)
 
     @Throws(IOException::class)
     override fun intercept(chain: Chain): Response {
-        // get the session, establish a session if one doesn't exist or if we have a stale session
-        var session: S?
-        synchronized(mLockSession) {
-            session = loadSession()
-            if (session == null) {
-                session = try {
-                    establishSession()
+        val session = synchronized(lockSession) {
+            // load a valid session
+            loadSession()?.takeIf { it.isValid }
+                // otherwise try establishing a session
+                ?: try {
+                    establishSession()?.takeIf { it.isValid }?.also { saveSession(it) }
                 } catch (e: IOException) {
-                    // wrap establish session IOExceptions
                     throw EstablishSessionApiException(e)
                 }
+        } ?: throw InvalidSessionApiException()
 
-                // save the newly established session
-                if (session != null && session!!.isValid) {
-                    saveSession(session!!)
-                }
-            }
-        }
-
-        // throw an exception if we don't have a valid session
-        if (session == null) {
-            throw InvalidSessionApiException()
-        }
-
-        // process request & response
-        return processResponse(chain.proceed(attachSession(chain.request(), session!!)), session!!)
+        return processResponse(chain.proceed(attachSession(chain.request(), session)), session)
     }
 
     protected abstract fun attachSession(request: Request, session: S): Request
 
     @CallSuper
     @Throws(IOException::class)
-    protected fun processResponse(response: Response, session: S): Response {
-        // make sure the response is valid
+    protected open fun processResponse(response: Response, session: S): Response {
         if (isSessionInvalid(response)) {
-            // reset the session
-            synchronized(mLockSession) {
-
-                // only reset if this is still the same session
-                val active = loadSession()
-                if (active != null && active.equals(session)) {
-                    deleteSession(session)
-                }
+            // reset the session if this is still the same session
+            synchronized(lockSession) {
+                loadSession()?.takeIf { it == session }?.apply { deleteSession(this) }
             }
 
-            // throw an invalid session exception because our session was invalid
-            if (!mReturnInvalidSessionResponses) {
-                throw InvalidSessionApiException()
-            }
+            if (!returnInvalidSessionResponses) throw InvalidSessionApiException()
         }
         return response
     }
 
     @Throws(IOException::class)
-    fun isSessionInvalid(response: Response): Boolean {
-        return false
-    }
+    open fun isSessionInvalid(response: Response) = false
 
-    private fun loadSession(): S? {
-        // load a pre-existing session
-        val prefs = prefs
-        val session: S?
-        synchronized(mLockSession) { session = loadSession(prefs) }
-
-        // only return valid sessions
-        return if (session != null && session.isValid) session else null
-    }
-
+    private fun loadSession() = synchronized(lockSession) { loadSession(prefs) }?.takeIf { it.isValid }
     protected abstract fun loadSession(prefs: SharedPreferences): S?
 
     @Throws(IOException::class)
-    protected open fun establishSession(): S? {
-        return null
-    }
+    protected open fun establishSession(): S? = null
 
-    protected fun saveSession(session: S) {
-        val prefs = prefs.edit()
-        session!!.save(prefs)
-        synchronized(mLockSession) { prefs.apply() }
+    private fun saveSession(session: S) {
+        val changes = prefs.edit().apply { session.save(this) }
+        synchronized(lockSession) { changes.apply() }
     }
 
     private fun deleteSession(session: S) {
-        val prefs = prefs.edit()
-        session!!.delete(prefs)
-        synchronized(mLockSession) { prefs.apply() }
-    }
-
-    companion object {
-        private const val DEFAULT_RETURN_INVALID_SESSION_RESPONSES = false
-    }
-
-    init {
-        mContext = context.applicationContext
-        mReturnInvalidSessionResponses = returnInvalidSessionResponses
-        mPrefFile = prefFile ?: javaClass.simpleName
+        val changes = prefs.edit().apply { session.delete(this) }
+        synchronized(lockSession) { changes.apply() }
     }
 }
