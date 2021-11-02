9a3e91a28ea8c24b3838ecd5bc356b73be51f092
Daniel Frett <daniel.frett@ccci.org>
2021-11-02 12:12:50 -0400

    create some SavedStateHandle delegates

---
+++ gto-support-androidx-lifecycle/src/test/kotlin/org/ccci/gto/android/common/androidx/lifecycle/SavedStateHandleDelegatesTest.kt
@@ -0,0 +1,35 @@
+package org.ccci.gto.android.common.androidx.lifecycle
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class SavedStateHandleDelegatesTest {
+    @get:Rule
+    val instantTaskRule = InstantTaskExecutorRule()
+
+    private val savedState = SavedStateHandle()
+    private val delegates = object {
+        var propStr: String? by savedState.delegate()
+        val propStrLiveData: MutableLiveData<String?> by savedState.livedata("propStr")
+    }
+
+    @Test
+    fun testPropertyDelegate() {
+        delegates.propStr = "test"
+        assertEquals("test", savedState["propStr"])
+        assertEquals("test", delegates.propStr)
+        assertEquals("test", delegates.propStrLiveData.value)
+    }
+
+    @Test
+    fun testLiveDataDelegate() {
+        delegates.propStrLiveData.value = "livedata"
+        assertEquals("livedata", savedState["propStr"])
+        assertEquals("livedata", delegates.propStr)
+        assertEquals("livedata", delegates.propStrLiveData.value)
+    }
+}
