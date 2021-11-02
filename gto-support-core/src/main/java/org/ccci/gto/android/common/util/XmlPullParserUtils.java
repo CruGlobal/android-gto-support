96fa175f019841641375a74f14dae295fca86207
Daniel Frett <daniel.frett@ccci.org>
2021-11-02 13:55:13 -0400

    remove v3.6.1 deprecated XmlPullParserUtils.skipTag method

--- gto-support-core/src/main/java/org/ccci/gto/android/common/util/XmlPullParserUtils.java
+++ gto-support-core/src/main/java/org/ccci/gto/android/common/util/XmlPullParserUtils.java
@@ -4,22 +4,10 @@ import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 
-import org.ccci.gto.android.common.util.xmlpull.XmlPullParserKt;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
-import java.io.IOException;
-
 public class XmlPullParserUtils {
-    /**
-     * Skip the current XML tag (and all of it's children)
-     * @deprecated Since v3.6.1, Use the Kotlin extension function instead.
-     */
-    @Deprecated
-    public static void skipTag(@NonNull final XmlPullParser parser) throws XmlPullParserException, IOException {
-        XmlPullParserKt.skipTag(parser);
-    }
-
     /**
      * Similar to {@link XmlPullParser#require(int, String, String)}, but tests to see if the current node name matches
      * any of the provided names.
