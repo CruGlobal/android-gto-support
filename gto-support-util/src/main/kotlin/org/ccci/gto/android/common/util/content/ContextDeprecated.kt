a232b093ed6c43d5968d749f946660fe9a778cd4
Daniel Frett <daniel.frett@ccci.org>
2021-12-03 09:33:29 -0500

    remove v3.10.1 deprecated isApplicationDebuggable()

--- gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/content/ContextDeprecated.kt
+++
@@ -1,6 +0,0 @@
-package org.ccci.gto.android.common.util.content
-
-import android.content.Context
-
-@Deprecated("Since v3.10.1, use extension property instead.", ReplaceWith("isApplicationDebuggable"))
-fun Context.isApplicationDebuggable() = isApplicationDebuggable
