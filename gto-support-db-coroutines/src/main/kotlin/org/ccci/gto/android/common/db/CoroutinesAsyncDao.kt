b63d39eeefafe83f7756328c830f5cfa8514724b
Daniel Frett <daniel.frett@ccci.org>
2022-01-13 17:31:54 -0500

    create a CoroutinesAsyncDao that will launch various actions within a coroutinesScope

---
+++ gto-support-db-coroutines/src/main/kotlin/org/ccci/gto/android/common/db/CoroutinesAsyncDao.kt
@@ -0,0 +1,24 @@
+package org.ccci.gto.android.common.db
+
+import android.database.sqlite.SQLiteDatabase
+import kotlinx.coroutines.async
+
+interface CoroutinesAsyncDao : CoroutinesDao {
+    fun <T : Any> findAsync(clazz: Class<T>, vararg key: Any) = coroutinesScope.async { find(clazz, *key) }
+    fun <T : Any> getAsync(query: Query<T>) = coroutinesScope.async { get(query) }
+    fun getCursorAsync(query: Query<*>) = coroutinesScope.async { getCursor(query) }
+
+    fun insertAsync(obj: Any) = insertAsync(obj, SQLiteDatabase.CONFLICT_NONE)
+    fun insertAsync(obj: Any, conflictAlgorithm: Int) = coroutinesScope.async { insert(obj, conflictAlgorithm) }
+
+    fun updateAsync(obj: Any) = updateAsync(obj, *getFullProjection(obj.javaClass))
+    fun updateAsync(obj: Any, vararg projection: String) = coroutinesScope.async { update(obj, *projection) }
+    fun <T : Any> updateAsync(sample: T, where: Expression?, vararg projection: String) =
+        coroutinesScope.async { update(sample, where, *projection) }
+
+    fun updateOrInsertAsync(obj: Any) = updateOrInsertAsync(obj, *getFullProjection(obj.javaClass))
+    fun updateOrInsertAsync(obj: Any, vararg projection: String) =
+        coroutinesScope.async { updateOrInsert(obj, *projection) }
+}
+
+inline fun <reified T : Any> CoroutinesAsyncDao.findAsync(vararg key: Any) = findAsync(T::class.java, *key)
--- gto-support-db-coroutines/src/main/kotlin/org/ccci/gto/android/common/db/CoroutinesDao.kt
+++ gto-support-db-coroutines/src/main/kotlin/org/ccci/gto/android/common/db/CoroutinesDao.kt
@@ -1,8 +1,10 @@
 package org.ccci.gto.android.common.db
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.SupervisorJob
 
 private const val COROUTINES_PARALLELISM = 4
 
@@ -10,4 +12,6 @@ interface CoroutinesDao : Dao {
     @OptIn(ExperimentalCoroutinesApi::class)
     val coroutineDispatcher: CoroutineDispatcher
         get() = getService { Dispatchers.IO.limitedParallelism(COROUTINES_PARALLELISM) }
+    val coroutinesScope: CoroutineScope
+        get() = getService { CoroutineScope(coroutineDispatcher + SupervisorJob()) }
 }
