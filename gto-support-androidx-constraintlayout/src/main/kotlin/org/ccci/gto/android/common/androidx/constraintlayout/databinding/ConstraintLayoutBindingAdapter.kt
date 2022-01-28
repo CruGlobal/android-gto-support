dbce762b2c447ac1ed7d728a491c4c1dc1c8ed5a
Daniel Frett <daniel.frett@ccci.org>
2022-01-28 16:38:09 -0500

    provide a Double dimensionRatio setter

--- gto-support-androidx-constraintlayout/src/main/kotlin/org/ccci/gto/android/common/androidx/constraintlayout/databinding/ConstraintLayoutBindingAdapter.kt
+++ gto-support-androidx-constraintlayout/src/main/kotlin/org/ccci/gto/android/common/androidx/constraintlayout/databinding/ConstraintLayoutBindingAdapter.kt
@@ -6,6 +6,10 @@ import androidx.databinding.BindingAdapter
 import org.ccci.gto.android.common.androidx.constraintlayout.widget.setDimensionRatio
 
 @BindingAdapter("layout_constraintDimensionRatio")
+internal fun View.setConstraintLayoutDimensionRatio(ratio: Double?) =
+    setConstraintLayoutDimensionRatio(ratio?.toString())
+
+@BindingAdapter("layout_constraintDimensionRatio")
 internal fun View.setConstraintLayoutDimensionRatio(ratio: String?) {
     val lp = layoutParams as? ConstraintLayout.LayoutParams ?: return
     if (ratio != lp.dimensionRatio) {
