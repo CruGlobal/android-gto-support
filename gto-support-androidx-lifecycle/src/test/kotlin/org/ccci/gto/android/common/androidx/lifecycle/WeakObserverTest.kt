a3cb6a0298d9edfd8d1ac76c4cd1522d427dc1f2
Daniel Frett <daniel.frett@ccci.org>
2022-02-02 10:26:26 -0500

    create a WeakObserver for LiveData
    this will allow us to observe a LiveData as long as a specified object isn't garbage collected

---
+++ gto-support-androidx-lifecycle/src/test/kotlin/org/ccci/gto/android/common/androidx/lifecycle/WeakObserverTest.kt
@@ -0,0 +1,71 @@
+package org.ccci.gto.android.common.androidx.lifecycle
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.testing.TestLifecycleOwner
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class WeakObserverTest : BaseLiveDataTest() {
+    var counter = 0
+    val liveData = MutableLiveData(counter)
+
+    private val lifecycleOwner = TestLifecycleOwner(coroutineDispatcher = UnconfinedTestDispatcher())
+
+    @Test
+    fun `remove observer manually`() {
+        val obj = Any()
+        assertFalse(liveData.hasActiveObservers())
+        val observer = liveData.observeWeak(lifecycleOwner, obj) { counter++ }
+        assertTrue(liveData.hasActiveObservers())
+        assertEquals(1, counter)
+
+        liveData.value = counter
+        assertEquals(2, counter)
+
+        liveData.removeObserver(observer)
+        liveData.value = counter
+        assertFalse(liveData.hasActiveObservers())
+        assertEquals(2, counter)
+    }
+
+    @Test
+    fun `remove observer automatically when obj is garbage collected`() {
+        var obj = Any()
+        assertFalse(liveData.hasActiveObservers())
+        liveData.observeWeak(lifecycleOwner, obj) { counter++ }
+        assertTrue(liveData.hasActiveObservers())
+        assertEquals(1, counter)
+
+        liveData.value = counter
+        assertEquals(2, counter)
+
+        obj = Any()
+        System.gc()
+        liveData.value = counter
+        assertFalse(liveData.hasActiveObservers())
+        assertEquals(2, counter)
+    }
+
+    @Test
+    fun `remove observer automatically when lifecycle is destroyed`() {
+        val obj = Any()
+        assertFalse(liveData.hasActiveObservers())
+        liveData.observeWeak(lifecycleOwner, obj) { counter++ }
+        assertTrue(liveData.hasActiveObservers())
+        assertEquals(1, counter)
+
+        liveData.value = counter
+        assertEquals(2, counter)
+
+        lifecycleOwner.currentState = Lifecycle.State.DESTROYED
+        liveData.value = counter
+        assertFalse(liveData.hasActiveObservers())
+        assertEquals(2, counter)
+    }
+}
