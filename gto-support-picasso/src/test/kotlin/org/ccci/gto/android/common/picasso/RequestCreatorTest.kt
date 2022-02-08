79fccb303db6fff2e38ca608f255d0af27220bd4
Daniel Frett <daniel.frett@ccci.org>
2022-02-08 15:31:24 -0500

    cleanup RequestCreatorTest to better leverage kotlin coroutines test

--- gto-support-picasso/src/test/kotlin/org/ccci/gto/android/common/picasso/RequestCreatorTest.kt
+++ gto-support-picasso/src/test/kotlin/org/ccci/gto/android/common/picasso/RequestCreatorTest.kt
@@ -10,21 +10,19 @@ import com.squareup.picasso.StubRequestCreator
 import com.squareup.picasso.Target
 import java.lang.ref.Reference
 import java.lang.ref.WeakReference
-import java.util.concurrent.Executors
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.android.asCoroutineDispatcher
-import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNotSame
 import org.junit.Assert.assertSame
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -40,92 +38,78 @@ import org.mockito.kotlin.whenever
 @RunWith(AndroidJUnit4::class)
 @OptIn(ExperimentalCoroutinesApi::class)
 class RequestCreatorTest {
-    private lateinit var request: RequestCreator
-    private lateinit var dispatcher: ExecutorCoroutineDispatcher
-    private lateinit var testDispatcher: TestCoroutineDispatcher
-
+    private lateinit var mainThread: HandlerThread
     private lateinit var bitmap: Bitmap
+    private lateinit var request: RequestCreator
 
     @Before
     fun setup() {
-        request = mock()
-        dispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
-        testDispatcher = TestCoroutineDispatcher()
-        Dispatchers.setMain(testDispatcher)
+        mainThread = HandlerThread("").apply { start() }
+        Dispatchers.setMain(Handler(mainThread.looper).asCoroutineDispatcher())
 
         bitmap = mock()
+        request = mock()
     }
 
     @After
     fun cleanup() {
         Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
+        mainThread.quit()
+        mainThread.join()
     }
 
     @Test
-    fun testGetBitmap() {
+    fun testGetBitmap() = runTest {
         whenever(request.into(any<Target>()))
             .thenAnswer { it.getArgument<Target>(0).onBitmapLoaded(bitmap, Picasso.LoadedFrom.DISK) }
 
-        assertSame(bitmap, runBlocking { request.getBitmap() })
+        assertSame(bitmap, request.getBitmap())
     }
 
     @Test(expected = ExpectedException::class)
-    fun testGetBitmapError() {
+    fun testGetBitmapError() = runTest {
         whenever(request.into(any<Target>()))
             .thenAnswer { it.getArgument<Target>(0).onBitmapFailed(ExpectedException(), null) }
 
-        runBlocking { request.getBitmap() }
+        request.getBitmap()
     }
 
     @Test
-    fun testGetBitmapMainThreadUsage() {
-        val thread = HandlerThread("").apply { start() }
-        Dispatchers.setMain(Handler(thread.looper).asCoroutineDispatcher())
+    fun testGetBitmapMainThreadUsage() = runTest {
         whenever(request.into(any<Target>())).thenAnswer {
-            check(thread === Thread.currentThread()) { "Not executing on the Main Thread" }
+            assertSame("Not executing on the Main Thread", mainThread, Thread.currentThread())
             it.getArgument<Target>(0).onBitmapLoaded(bitmap, Picasso.LoadedFrom.DISK)
         }
 
-        runBlocking {
-            launch(Dispatchers.Default) { request.getBitmap() }
-            request.getBitmap()
-        }
-        thread.quit()
+        assertNotSame(mainThread, Thread.currentThread())
+        request.getBitmap()
     }
 
     @Test(timeout = 5000)
-    fun `getBitmap() should protect it's target from garbage collection`() {
+    fun `getBitmap() should protect it's target from garbage collection`() = runTest {
         val request = TargetCapturingRequestCreator()
-        runBlocking {
-            val task = launch(Dispatchers.Default) { request.getBitmap() }
-            val ref = request.targets.receive()
-            System.gc()
-            val target = ref.get()
-            if (target == null) task.cancel()
-            assertNotNull(target)
-            target!!.onBitmapLoaded(bitmap, Picasso.LoadedFrom.DISK)
-        }
+        val task = launch { request.getBitmap() }
+        val ref = request.targets.receive()
+        System.gc()
+        val target = ref.get()
+        if (target == null) task.cancel()
+        assertNotNull(target)
+        target!!.onBitmapLoaded(bitmap, Picasso.LoadedFrom.DISK)
     }
 
     @Test
-    fun `getBitmap() should cancel Picasso request on task cancellation`() {
+    fun `getBitmap() should cancel Picasso request on task cancellation`() = runTest {
         val picasso = mock<Picasso>()
         val request = TargetCapturingRequestCreator(picasso)
-        val target = runBlocking {
-            val task = launch(Dispatchers.Default) { request.getBitmap() }
-            val target = request.targets.receive().get()!!
-            task.cancel()
-            return@runBlocking target
-        }
+
+        val task = launch { request.getBitmap() }
+        val target = request.targets.receive().get()!!
+        task.cancelAndJoin()
         verify(picasso).cancelRequest(target)
     }
 
     @Test
-    fun `getBitmap() should cancel Picasso request on main thread`() {
-        val thread = HandlerThread("").apply { start() }
-        assertTrue(thread.isAlive)
-        Dispatchers.setMain(Handler(thread.looper).asCoroutineDispatcher())
+    fun `getBitmap() should cancel Picasso request on main thread`() = runTest {
         var cancelRequestThread: Thread? = null
         val picasso = mock<Picasso> {
             // we capture the thread that cancelRequest is called on, that way we can assert later that it was the
@@ -138,15 +122,10 @@ class RequestCreatorTest {
         }
 
         val request = TargetCapturingRequestCreator(picasso)
-        val target = runBlocking {
-            val task = launch(TestCoroutineDispatcher()) { request.getBitmap() }
-            val target = request.targets.receive().get()!!
-            task.cancel()
-            return@runBlocking target
-        }
-        thread.quitSafely()
-        thread.join()
-        assertEquals(thread, cancelRequestThread)
+        val task = launch(UnconfinedTestDispatcher()) { request.getBitmap() }
+        val target = request.targets.receive().get()!!
+        task.cancelAndJoin()
+        assertSame(mainThread, cancelRequestThread)
         verify(picasso).cancelRequest(target)
         verifyNoMoreInteractions(picasso)
     }
