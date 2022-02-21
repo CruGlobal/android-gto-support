ae319540c86f0a697c31f59d3fee4dde668a6ea2
Daniel Frett <daniel.frett@ccci.org>
2022-02-21 11:18:46 -0500

    add an extension method to add an arbitrary layout to a ConcatAdapter

---
+++ gto-support-androidx-recyclerview/src/main/kotlin/org/ccci/gto/android/common/androidx/recyclerview/widget/ConcatAdapter.kt
@@ -0,0 +1,12 @@
+package org.ccci.gto.android.common.androidx.recyclerview.widget
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.ConcatAdapter
+import org.ccci.gto.android.common.androidx.recyclerview.adapter.SimpleLayoutAdapter
+
+fun ConcatAdapter.addLayout(
+    @LayoutRes layoutId: Int,
+    repeat: Int = 1,
+    initializeLayout: (View) -> Unit = {}
+) = SimpleLayoutAdapter(layoutId, repeat, initializeLayout).also { addAdapter(it) }
