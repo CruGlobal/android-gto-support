fc4d17e7ff3ce5bbaf3712332fb0daf5112c2f8d
Daniel Frett <daniel.frett@ccci.org>
2022-02-21 11:05:45 -0500

    create a SimpleLayoutAdapter for a RecyclerView

---
+++ gto-support-androidx-recyclerview/src/main/kotlin/org/ccci/gto/android/common/androidx/recyclerview/adapter/SimpleLayoutAdapter.kt
@@ -0,0 +1,39 @@
+package org.ccci.gto.android.common.androidx.recyclerview.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+
+class SimpleLayoutAdapter(
+    @LayoutRes private val layoutId: Int,
+    repeat: Int = 1,
+    private val initializeLayout: (View) -> Unit = {}
+) : RecyclerView.Adapter<SimpleLayoutAdapter.ViewHolder>() {
+    init {
+        setHasStableIds(true)
+    }
+
+    var repeat = repeat
+        set(value) {
+            val old = field
+            field = value
+            when {
+                value < old -> notifyItemRangeRemoved(value, old - value)
+                value > old -> notifyItemRangeInserted(value, value - old)
+            }
+        }
+
+    override fun getItemCount() = repeat
+    override fun getItemId(position: Int) = position.toLong()
+    override fun getItemViewType(position: Int) = layoutId
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        LayoutInflater.from(parent.context).inflate(viewType, parent, false)
+            .also { initializeLayout(it) }
+            .let { ViewHolder(it) }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = Unit
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
