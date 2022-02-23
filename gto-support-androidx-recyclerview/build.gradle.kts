6880b0904f4205843568a2ac22139108e79823fc
Daniel Frett <daniel.frett@ccci.org>
2022-02-23 16:46:24 -0500

    create a binding adapter to adjust the spanCount for recyclerview grid layout managers

--- gto-support-androidx-recyclerview/build.gradle.kts
+++ gto-support-androidx-recyclerview/build.gradle.kts
@@ -1,17 +1,24 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    kotlin("kapt")
 }
 
 configureAndroidLibrary()
+
+android {
+    buildFeatures.dataBinding = true
+    dataBinding.addDefaultAdapters = false
+}
 
 dependencies {
     api(libs.androidx.recyclerview)
     implementation(libs.weakdelegate)
 
-    // region *DataBindingAdapter
+    // region Data Binding
     compileOnly(libs.androidx.databinding.runtime)
-    // endregion *DataBindingAdapter
+    compileOnly(libs.androidx.databinding.adapters)
+    // endregion Data Binding
 
     testImplementation(libs.junitParams)
 }
