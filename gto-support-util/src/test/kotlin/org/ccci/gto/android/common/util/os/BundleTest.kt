91247b32eab2da9718eca268c3a6dddbd39a85fc
Daniel Frett <daniel.frett@ccci.org>
2022-03-03 13:14:56 -0500

    create a Bundle+Locale kotlin file to contain all the Bundle Locale extensions

--- gto-support-util/src/test/kotlin/org/ccci/gto/android/common/util/os/BundleTest.kt
+++ gto-support-util/src/test/kotlin/org/ccci/gto/android/common/util/os/BundleTest.kt
@@ -3,36 +3,17 @@ package org.ccci.gto.android.common.util.os
 import android.graphics.Point
 import android.os.Bundle
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import java.util.Locale
 import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.annotation.Config
 
 private const val KEY1 = "key1"
 private const val KEY2 = "key2"
-private const val KEY3 = "key3"
 
 @RunWith(AndroidJUnit4::class)
 class BundleTest {
-    // region Locales
-    @Test
-    fun verifyPutLocaleAndGetLocale() {
-        val bundle = Bundle().apply {
-            putLocale(KEY1, Locale.ENGLISH)
-            putLocale(KEY2, Locale.FRENCH)
-            putLocale(KEY3, null)
-        }
-
-        assertEquals(3, bundle.size())
-        assertEquals(Locale.ENGLISH, bundle.getLocale(KEY1))
-        assertEquals(Locale.FRENCH, bundle.getLocale(KEY2))
-        assertNull(bundle.getLocale(KEY3))
-    }
-    // endregion Locales
-
     // region ParcelableArrays
     @Test
     fun verifyGetParcelableArray() {
