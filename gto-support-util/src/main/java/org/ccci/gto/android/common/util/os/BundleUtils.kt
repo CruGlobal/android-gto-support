00b976031f169351bf66acde71d339ba2eb31e7b
Daniel Frett <daniel.frett@ccci.org>
2022-03-03 14:10:58 -0500

    remove v3.11.2 deprecated BundleUtils methods

--- gto-support-util/src/main/java/org/ccci/gto/android/common/util/os/BundleUtils.kt
+++ gto-support-util/src/main/java/org/ccci/gto/android/common/util/os/BundleUtils.kt
@@ -3,7 +3,6 @@
 package org.ccci.gto.android.common.util.os
 
 import android.os.Bundle
-import java.util.Locale
 import org.jetbrains.annotations.Contract
 
 // region Enums
@@ -25,44 +24,3 @@ inline fun <reified T : Enum<T>> Bundle.getEnum(key: String?, defValue: T? = nul
     getEnum(T::class.java, key, defValue)
 
 // endregion Enums
-
-// region Locales
-@Deprecated(
-    "Since v3.11.2, use BundleKt.putLocale() instead",
-    ReplaceWith("putLocale(key, locale)"),
-    DeprecationLevel.ERROR
-)
-@JvmName("putLocale")
-@Suppress("FunctionName")
-internal fun Bundle.`-putLocale`(key: String?, locale: Locale?) = putLocale(key, locale)
-
-@Deprecated(
-    "Since v3.11.2, use BundleKt.getLocale() instead",
-    ReplaceWith("getLocale(key, defValue)"),
-    DeprecationLevel.ERROR
-)
-@JvmOverloads
-@JvmName("getLocale")
-@Suppress("FunctionName")
-fun Bundle.`-getLocale`(key: String?, defValue: Locale? = null) = getLocale(key, defValue)
-
-@Deprecated(
-    "Since v3.11.2, use BundleKt.putLocaleArray() instead",
-    ReplaceWith("putLocaleArray(key, locales, singleString)"),
-    DeprecationLevel.ERROR
-)
-@JvmOverloads
-@JvmName("putLocaleArray")
-@Suppress("FunctionName")
-fun Bundle.`-putLocaleArray`(key: String?, locales: Array<Locale>?, singleString: Boolean = false) =
-    putLocaleArray(key, locales, singleString)
-
-@Deprecated(
-    "Since v3.11.2, use BundleKt.getLocaleArray() instead",
-    ReplaceWith("getLocaleArray(key)"),
-    DeprecationLevel.ERROR
-)
-@JvmName("getLocaleArray")
-@Suppress("FunctionName")
-fun Bundle.`-getLocaleArray`(key: String?) = getLocaleArray(key)
-// endregion Locales
