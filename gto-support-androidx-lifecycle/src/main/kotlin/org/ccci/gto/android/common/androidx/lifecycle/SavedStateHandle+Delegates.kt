944bbb6a8d2136ba100b8fe83deab037ee34d624
Daniel Frett <daniel.frett@ccci.org>
2022-03-07 18:42:12 -0500

    create stateFlow delegates for SavedStateHandle

--- gto-support-androidx-lifecycle/src/main/kotlin/org/ccci/gto/android/common/androidx/lifecycle/SavedStateHandle+Delegates.kt
+++ gto-support-androidx-lifecycle/src/main/kotlin/org/ccci/gto/android/common/androidx/lifecycle/SavedStateHandle+Delegates.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.SavedStateHandle
 import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
 
 fun <T> SavedStateHandle.delegate(key: String? = null) = object : ReadWriteProperty<Any, T?> {
     override fun getValue(thisRef: Any, property: KProperty<*>): T? = get(key ?: property.name)
@@ -23,4 +25,10 @@ fun <T> SavedStateHandle.livedata(key: String? = null) = ReadOnlyProperty<Any, M
 fun <T> SavedStateHandle.livedata(key: String? = null, initialValue: T) =
     ReadOnlyProperty<Any, MutableLiveData<T>> { _, property ->
         getLiveData(key ?: property.name, initialValue)
+    }
+
+fun <T> SavedStateHandle.stateFlow(scope: CoroutineScope, key: String? = null) = stateFlow<T?>(scope, key, null)
+fun <T> SavedStateHandle.stateFlow(scope: CoroutineScope, key: String? = null, initialValue: T) =
+    ReadOnlyProperty<Any, MutableStateFlow<T>> { _, property ->
+        getStateFlow(scope, key ?: property.name, initialValue)
     }
