1e766e16df0ec6e83fb059b7108a50b343fab039
Daniel Frett <daniel.frett@ccci.org>
2022-03-28 16:36:29 -0400

    add a unit test for new collectInto extension method

--- gto-support-kotlin-coroutines/build.gradle.kts
+++ gto-support-kotlin-coroutines/build.gradle.kts
@@ -12,5 +12,7 @@ dependencies {
     compileOnly(libs.androidx.lifecycle.livedata.core)
     // endregion LiveData extensions
 
+    testImplementation(libs.androidx.arch.core.testing)
+    testImplementation(libs.androidx.lifecycle.livedata.core)
     testImplementation(libs.kotlin.coroutines.test)
 }
