0e81e856e04263894cf5ec2beccca274828d5b53
Daniel Frett <daniel.frett@ccci.org>
2022-04-08 15:08:24 -0400

    expose the internal method that clears a composition

--- gto-support-lottie/proguard-consumer.pro
+++ gto-support-lottie/proguard-consumer.pro
@@ -1,4 +1,5 @@
 # LottieAnimationViewInternals
 -keepclassmembernames class com.airbnb.lottie.LottieAnimationView {
+    void clearComposition();
     void setCompositionTask(com.airbnb.lottie.LottieTask);
 }
--- gto-support-lottie/src/main/kotlin/org/ccci/gto/android/common/lottie/LottieAnimationView.kt
+++ gto-support-lottie/src/main/kotlin/org/ccci/gto/android/common/lottie/LottieAnimationView.kt
@@ -1,7 +1,9 @@
 package org.ccci.gto.android.common.lottie
 
 import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.clearCompositionMethod
 import com.airbnb.lottie.setCompositionTask
 import java.io.File
 
+fun LottieAnimationView.clearComposition() = clearCompositionMethod?.invoke(this)
 fun LottieAnimationView.setAnimationFromFile(file: File) = setCompositionTask(file.loadLottieComposition())
