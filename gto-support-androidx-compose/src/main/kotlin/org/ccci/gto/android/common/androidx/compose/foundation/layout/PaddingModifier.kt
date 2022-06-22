a7933a722c2f6f74d70565f752748690656f9e11
Daniel Frett <daniel.frett@ccci.org>
2022-06-22 15:54:03 -0400

    create a padding modifier that can combine multiple types of padding modifiers

---
+++ gto-support-androidx-compose/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/foundation/layout/PaddingModifier.kt
@@ -0,0 +1,16 @@
+package org.ccci.gto.android.common.androidx.compose.foundation.layout
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+fun Modifier.padding(
+    all: Dp = 0.dp,
+    horizontal: Dp = all,
+    vertical: Dp = all,
+    start: Dp = horizontal,
+    top: Dp = vertical,
+    end: Dp = horizontal,
+    bottom: Dp = vertical
+) = padding(start = start, top = top, end = end, bottom = bottom)
