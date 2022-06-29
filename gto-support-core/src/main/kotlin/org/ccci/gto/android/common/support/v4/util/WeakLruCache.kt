aa42031d2316e19eb313e6c6dec4643c0d333f2a
Daniel Frett <daniel.frett@ccci.org>
2022-06-29 13:53:59 -0400

    remove v3.11.2 deprecated WeakLruCache

--- gto-support-core/src/main/kotlin/org/ccci/gto/android/common/support/v4/util/WeakLruCache.kt
+++
@@ -1,4 +0,0 @@
-package org.ccci.gto.android.common.support.v4.util
-
-@Deprecated("Since v3.11.2, This class has been moved to gto-support-androidx-collection")
-typealias WeakLruCache<K, V> = org.ccci.gto.android.common.androidx.collection.WeakLruCache<K, V>
