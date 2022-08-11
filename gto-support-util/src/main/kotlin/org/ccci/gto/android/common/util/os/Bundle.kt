4bd9625780f871c8cc1bd9eab422fd5a92eb54c5
Daniel Frett <daniel.frett@ccci.org>
2022-08-11 16:39:59 -0400

    create getParcelableArrayCompat method to provide fallback behavior for new API added in Android 13

--- gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/os/Bundle.kt
+++ gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/os/Bundle.kt
@@ -5,15 +5,17 @@ package org.ccci.gto.android.common.util.os
 
 import android.os.Bundle
 import android.os.Parcelable
+import org.ccci.gto.android.common.compat.os.getParcelableArrayCompat
 
 // region Parcelables
-@Suppress("UNCHECKED_CAST")
-fun <T : Parcelable> Bundle.getParcelableArray(key: String?, clazz: Class<T>) =
-    getParcelableArray(key)?.let {
-        val arr = java.lang.reflect.Array.newInstance(clazz, it.size) as Array<T?>
-        System.arraycopy(it, 0, arr, 0, it.size)
-        arr
-    }
+@Deprecated(
+    "Since v3.13.0, use Bundle.getParcelableArrayCompat(key, clazz) instead.",
+    ReplaceWith(
+        "getParcelableArrayCompat(key, clazz)",
+        "org.ccci.gto.android.common.compat.os.getParcelableArrayCompat"
+    )
+)
+fun <T : Parcelable> Bundle.getParcelableArray(key: String?, clazz: Class<T>) = getParcelableArrayCompat(key, clazz)
 
 inline fun <reified T : Parcelable> Bundle.getTypedParcelableArray(key: String?) =
     getParcelableArray(key)?.let { it: Array<Parcelable?> ->
