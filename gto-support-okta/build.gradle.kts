3202ee91e725d40b00ac43a01fb917bcdededae1
Daniel Frett <daniel.frett@ccci.org>
2022-09-02 15:58:04 -0400

    create a new gto-support-okta module which loads the okta-oidc module for now

---
+++ gto-support-okta/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+configureAndroidLibrary()
+
+android {
+    namespace = "org.ccci.gto.android.common.okta"
+
+    testFixtures.enable = true
+}
+
+dependencies {
+    // import legacy okta-oidc module. this transitive dependency will be removed over the next several releases of gto-support
+    // v3.14.0 - api dependency
+    // v3.15.0 - implementation dependency
+    // v3.16.0 - removed
+    api(project(":gto-support-okta-oidc"))
+    testFixturesApi(testFixtures(project(":gto-support-okta-oidc")))
+}
