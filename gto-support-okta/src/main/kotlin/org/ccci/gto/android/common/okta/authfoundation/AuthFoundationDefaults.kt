2e8713037ecd01ddcab92e6e9b2a660d805b4fab
Daniel Frett <daniel.frett@ccci.org>
2022-09-06 15:03:36 -0400

    add a compat OidcClock for older versions of android

---
+++ gto-support-okta/src/main/kotlin/org/ccci/gto/android/common/okta/authfoundation/AuthFoundationDefaults.kt
@@ -0,0 +1,14 @@
+package org.ccci.gto.android.common.okta.authfoundation
+
+import android.os.Build
+import androidx.annotation.DeprecatedSinceApi
+import com.okta.authfoundation.AuthFoundationDefaults
+import com.okta.authfoundation.client.OidcClock
+
+@DeprecatedSinceApi(Build.VERSION_CODES.O)
+fun AuthFoundationDefaults.enableClockCompat() = when {
+    Build.VERSION.SDK_INT < Build.VERSION_CODES.O -> {
+        clock = OidcClock { System.currentTimeMillis() / 1000 }
+    }
+    else -> Unit
+}
