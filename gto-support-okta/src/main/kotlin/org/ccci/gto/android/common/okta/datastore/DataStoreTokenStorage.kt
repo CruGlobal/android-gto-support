bf8a98e181c7a0bd8247a9a7aedc9c7c29e26bb9
Daniel Frett <daniel.frett@ccci.org>
2022-09-07 14:51:43 -0400

    create a DataStore implementation of TokenStorage
    this implementation doesn't support encryption, but will work for API < 23

---
+++ gto-support-okta/src/main/kotlin/org/ccci/gto/android/common/okta/datastore/DataStoreTokenStorage.kt
@@ -0,0 +1,42 @@
+package org.ccci.gto.android.common.okta.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import com.okta.authfoundation.credential.TokenStorage
+import kotlinx.coroutines.flow.first
+
+class DataStoreTokenStorage(private val dataStore: DataStore<List<TokenStorage.Entry>>) : TokenStorage {
+    constructor(context: Context) : this(context.defaultTokenStorage)
+
+    companion object {
+        private const val FILE_NAME = "org.ccci.gto.android.common.okta.datastore.tokens.json"
+
+        private val Context.defaultTokenStorage: DataStore<List<TokenStorage.Entry>> by dataStore(
+            fileName = FILE_NAME,
+            serializer = TokenStorageEntriesSerializer
+        )
+    }
+
+    override suspend fun entries() = dataStore.data.first()
+
+    override suspend fun add(id: String) {
+        dataStore.updateData { entries ->
+            require(entries.none { it.identifier == id }) { "Error adding a new Credential to TokenStorage" }
+            entries + TokenStorage.Entry(id, null, emptyMap())
+        }
+    }
+
+    override suspend fun remove(id: String) {
+        dataStore.updateData { entries -> entries.filter { it.identifier != id } }
+    }
+
+    override suspend fun replace(updatedEntry: TokenStorage.Entry) {
+        dataStore.updateData { entries ->
+            entries.map {
+                if (it.identifier == updatedEntry.identifier) updatedEntry
+                else it
+            }
+        }
+    }
+}
