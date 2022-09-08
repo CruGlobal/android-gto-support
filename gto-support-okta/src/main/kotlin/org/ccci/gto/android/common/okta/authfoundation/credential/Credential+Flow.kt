488310e77eb1bdb1f98d67be3e4f7a4cc147c6cd
Daniel Frett <daniel.frett@ccci.org>
2022-09-08 12:40:58 -0400

    expose a idTokenFlow for an Okta Credential

---
+++ gto-support-okta/src/main/kotlin/org/ccci/gto/android/common/okta/authfoundation/credential/Credential+Flow.kt
@@ -0,0 +1,8 @@
+package org.ccci.gto.android.common.okta.authfoundation.credential
+
+import com.okta.authfoundation.credential.Credential
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+
+fun Credential.idTokenFlow(): Flow<Any?> = getTokenFlow().map { idToken() }.distinctUntilChanged()
