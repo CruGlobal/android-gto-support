2bfc19674c684a2de6c625a9aa07976719c1c494
Daniel Frett <daniel.frett@cru.org>
2022-10-10 15:55:57 -0400

    utilize getSerializableExtraCompat method

--- gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/content/Intent+Locale.kt
+++ gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/content/Intent+Locale.kt
@@ -6,9 +6,10 @@ package org.ccci.gto.android.common.util.content
 import android.content.Intent
 import java.io.Serializable
 import java.util.Locale
+import org.ccci.gto.android.common.compat.content.getSerializableExtraCompat
 
 fun Intent.putExtra(key: String, value: Locale?, asString: Boolean = false) =
     if (asString) putExtra(key, value?.toLanguageTag()) else putExtra(key, value as? Serializable)
 
 fun Intent.getLocaleExtra(key: String) =
-    getSerializableExtra(key) as? Locale ?: getStringExtra(key)?.let { Locale.forLanguageTag(it) }
+    getSerializableExtraCompat(key, Locale::class.java) ?: getStringExtra(key)?.let { Locale.forLanguageTag(it) }
