52b35ffee1b926e72ca1e2ea7e6923673e3ed292
Daniel Frett <daniel.frett@cru.org>
2022-10-13 17:22:51 -0400

    mark libraries as using core library desugaring instead of requiring a certain SDK level

--- gto-support-androidx-room/src/main/kotlin/org/ccci/gto/android/common/androidx/room/converter/Java8TimeConverters.kt
+++ gto-support-androidx-room/src/main/kotlin/org/ccci/gto/android/common/androidx/room/converter/Java8TimeConverters.kt
@@ -1,18 +1,14 @@
 package org.ccci.gto.android.common.androidx.room.converter
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.room.TypeConverter
 import java.time.Instant
 
 object Java8TimeConverters {
     @JvmStatic
     @TypeConverter
-    @RequiresApi(Build.VERSION_CODES.O)
     internal fun Long?.toInstant() = this?.let { Instant.ofEpochMilli(it) }
 
     @JvmStatic
     @TypeConverter
-    @RequiresApi(Build.VERSION_CODES.O)
     internal fun Instant?.toLong() = this?.toEpochMilli()
 }
