e3bbff28f04ab71c09e697577794f93e64ac15d9
Daniel Frett <daniel.frett@cru.org>
2022-10-13 13:50:52 -0400

    update SessionInterceptorTest to utilize MockK instead of Mockito

--- gto-support-api-okhttp3/src/test/kotlin/org/ccci/gto/android/common/api/okhttp3/interceptor/SessionInterceptorTest.kt
+++ gto-support-api-okhttp3/src/test/kotlin/org/ccci/gto/android/common/api/okhttp3/interceptor/SessionInterceptorTest.kt
@@ -2,6 +2,13 @@ package org.ccci.gto.android.common.api.okhttp3.interceptor
 
 import android.content.Context
 import android.content.SharedPreferences
+import io.mockk.Called
+import io.mockk.every
+import io.mockk.excludeRecords
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
+import io.mockk.verifyAll
 import java.io.IOException
 import java.util.concurrent.Callable
 import okhttp3.Interceptor
@@ -11,115 +18,97 @@ import org.ccci.gto.android.common.api.Session
 import org.ccci.gto.android.common.api.okhttp3.EstablishSessionApiException
 import org.ccci.gto.android.common.api.okhttp3.InvalidSessionApiException
 import org.junit.Assert.assertThrows
-import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.RETURNS_DEEP_STUBS
-import org.mockito.kotlin.any
-import org.mockito.kotlin.doAnswer
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.doThrow
-import org.mockito.kotlin.eq
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
-import org.mockito.kotlin.spy
-import org.mockito.kotlin.stub
-import org.mockito.kotlin.verify
 
 class SessionInterceptorTest {
-    private lateinit var response: Response
-    private lateinit var chain: Interceptor.Chain
-    private lateinit var context: Context
-    private lateinit var sessionInterceptor: MockSessionInterceptor
-
-    private val validSession = spy(MockSession("valid"))
-    private val invalidSession = spy(MockSession(null))
-
-    @Before
-    fun setup() {
-        response = mock()
-        chain = mock {
-            on { request() } doReturn Request.Builder().url("https://example.com").build()
-            on { proceed(any()) } doReturn response
-        }
-        context = mock(defaultAnswer = RETURNS_DEEP_STUBS)
-        sessionInterceptor = MockSessionInterceptor(context)
+    private val response: Response = mockk()
+    private val chain: Interceptor.Chain = mockk {
+        every { request() } returns Request.Builder().url("https://example.com").build()
+        every { proceed(any()) } returns response
     }
+    private val context: Context = mockk(relaxed = true)
+    private val sessionInterceptor = MockSessionInterceptor(context)
+
+    private val validSession = spyk(MockSession("valid")) {
+        excludeRecords { isValid }
+    }
+    private val invalidSession = spyk(MockSession(null))
 
     @Test
     fun `Existing session is valid`() {
-        sessionInterceptor.stub {
-            on { loadSession(any()) } doReturn validSession
-        }
+        every { sessionInterceptor.loadSession(any()) } returns validSession
 
         sessionInterceptor.intercept(chain)
-        verify(sessionInterceptor.establishSession, never()).call()
-        verify(sessionInterceptor.attachSession).invoke(any(), any())
-        verify(validSession, never()).save(any())
+        verifyAll {
+            sessionInterceptor.establishSession wasNot Called
+            sessionInterceptor.attachSession(any(), any())
+        }
+        verify(exactly = 0) { validSession.save(any()) }
     }
 
     @Test
     fun `Existing session is invalid - establishSession() returns valid session`() {
-        sessionInterceptor.stub {
-            on { loadSession(any()) } doReturn invalidSession
-            on { establishSession() } doReturn validSession
-        }
+        every { sessionInterceptor.loadSession(any()) } returns invalidSession
+        every { sessionInterceptor.establishSession() } returns validSession
 
         sessionInterceptor.intercept(chain)
-        verify(sessionInterceptor.establishSession).call()
-        verify(sessionInterceptor.attachSession).invoke(any(), eq(validSession))
-        verify(validSession).save(any())
-        verify(invalidSession, never()).save(any())
+        verifyAll {
+            sessionInterceptor.establishSession.call()
+            sessionInterceptor.attachSession(any(), validSession)
+            validSession.save(any())
+        }
+        verify(exactly = 0) { invalidSession.save(any()) }
     }
 
     @Test
     fun `Existing session is invalid - establishSession() returns null`() {
-        sessionInterceptor.stub {
-            on { loadSession(any()) } doReturn invalidSession
-            on { establishSession() } doReturn null
-        }
+        every { sessionInterceptor.loadSession(any()) } returns invalidSession
+        every { sessionInterceptor.establishSession() } returns null
 
         assertThrows(InvalidSessionApiException::class.java) {
             sessionInterceptor.intercept(chain)
         }
-        verify(sessionInterceptor.establishSession).call()
-        verify(sessionInterceptor.attachSession, never()).invoke(any(), any())
-        verify(invalidSession, never()).save(any())
+        verifyAll {
+            sessionInterceptor.establishSession.call()
+            sessionInterceptor.attachSession wasNot Called
+        }
+        verify(exactly = 0) { invalidSession.save(any()) }
     }
 
     @Test
     fun `Existing session is invalid - establishSession() throws IOException`() {
-        sessionInterceptor.stub {
-            on { loadSession(any()) } doReturn invalidSession
-            on { establishSession() } doThrow IOException::class
-        }
+        every { sessionInterceptor.loadSession(any()) } returns invalidSession
+        every { sessionInterceptor.establishSession() } throws IOException()
 
         assertThrows(EstablishSessionApiException::class.java) {
             sessionInterceptor.intercept(chain)
         }
-        verify(sessionInterceptor.establishSession).call()
-        verify(sessionInterceptor.attachSession, never()).invoke(any(), any())
-        verify(invalidSession, never()).save(any())
+        verifyAll {
+            sessionInterceptor.establishSession.call()
+            sessionInterceptor.attachSession wasNot Called
+        }
+        verify(exactly = 0) { invalidSession.save(any()) }
     }
 
     @Test
     fun `Existing session is null - establishSession() returns valid session`() {
-        sessionInterceptor.stub {
-            on { loadSession(any()) } doReturn null
-            on { establishSession() } doReturn validSession
-        }
+        every { sessionInterceptor.loadSession(any()) } returns null
+        every { sessionInterceptor.establishSession() } returns validSession
 
         sessionInterceptor.intercept(chain)
-        verify(sessionInterceptor.establishSession).call()
-        verify(sessionInterceptor.attachSession).invoke(any(), eq(validSession))
-        verify(validSession).save(any())
+        verifyAll {
+            sessionInterceptor.establishSession.call()
+            sessionInterceptor.attachSession(any(), validSession)
+            validSession.save(any())
+        }
     }
 
     private class MockSession(id: String?) : Session(id)
     private class MockSessionInterceptor(context: Context) : SessionInterceptor<MockSession>(context) {
-        val loadSession = mock<(SharedPreferences) -> MockSession?>()
-        val establishSession = mock<Callable<MockSession?>>()
-        val attachSession = mock<(Request, MockSession) -> Request> {
-            on { invoke(any(), any()) } doAnswer { it.getArgument(0) }
+        val loadSession = mockk<(SharedPreferences) -> MockSession?>()
+        val establishSession = mockk<Callable<MockSession?>>()
+        val attachSession = mockk<(Request, MockSession) -> Request>().also {
+            every { it(any(), any()) } answers { it.invocation.args[0] as Request }
         }
 
         override fun loadSession(prefs: SharedPreferences) = loadSession.invoke(prefs)
