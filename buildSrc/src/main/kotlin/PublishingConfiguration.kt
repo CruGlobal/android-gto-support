756374d38c9bd7f000e32c21109332209190b40c
Daniel Frett <daniel.frett@cru.org>
2022-10-28 16:36:04 -0400

    rework the PublishingConfiguration to support KMP libraries

--- buildSrc/src/main/kotlin/PublishingConfiguration.kt
+++ buildSrc/src/main/kotlin/PublishingConfiguration.kt
@@ -9,57 +9,55 @@ import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.credentials
+import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.register
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 private const val GROUP = "org.ccci.gto.android"
 private const val GROUP_TESTING = "org.ccci.gto.android.testing"
-private const val POM_SCM_CONNECTION = "scm:git:git@github.com:CruGlobal/android-gto-support.git"
 
 fun Project.configurePublishing() {
-    project.group = GROUP
-
-    extensions.configure<LibraryExtension> {
-        publishing.singleVariant("release")
-    }
-
     apply(plugin = "maven-publish")
-    val sourcesJar = tasks.register<Jar>("sourcesJar") {
-        with(project.extensions.getByType<BaseExtension>().sourceSets["main"]) {
-            from(java.srcDirs, (kotlin as AndroidSourceDirectorySet).srcDirs)
+    group = GROUP
+    extensions.configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "cruGlobalMavenRepository"
+                setUrl(
+                    when {
+                        isSnapshotVersion ->
+                            "https://cruglobal.jfrog.io/cruglobal/list/maven-cru-android-public-snapshots-local/"
+                        else -> "https://cruglobal.jfrog.io/cruglobal/list/maven-cru-android-public-releases-local/"
+                    }
+                )
+
+                credentials(PasswordCredentials::class)
+            }
         }
     }
 
-    afterEvaluate {
-        extensions.configure<PublishingExtension> {
-            publications.register<MavenPublication>("release") {
-                from(components["release"])
+    // not Kotlin Multiplatform, so configure the android publication
+    if (extensions.findByType<KotlinMultiplatformExtension>() == null) {
+        extensions.configure<LibraryExtension> {
+            publishing.singleVariant("release")
+        }
 
-                artifact(sourcesJar) {
-                    classifier = "sources"
-                }
-
-                pom.withXml {
-                    with(asNode().appendNode("scm")) {
-                        appendNode("connection", POM_SCM_CONNECTION)
-                        appendNode("developerConnection", POM_SCM_CONNECTION)
-                    }
-                }
+        val sourcesJar = tasks.register<Jar>("sourcesJar") {
+            with(project.extensions.getByType<BaseExtension>().sourceSets["main"]) {
+                from(java.srcDirs, (kotlin as AndroidSourceDirectorySet).srcDirs)
             }
+        }
 
-            repositories {
-                maven {
-                    name = "cruGlobalMavenRepository"
-                    setUrl(
-                        when {
-                            isSnapshotVersion ->
-                                "https://cruglobal.jfrog.io/cruglobal/list/maven-cru-android-public-snapshots-local/"
-                            else -> "https://cruglobal.jfrog.io/cruglobal/list/maven-cru-android-public-releases-local/"
-                        }
-                    )
+        afterEvaluate {
+            extensions.configure<PublishingExtension> {
+                publications.register<MavenPublication>("release") {
+                    from(components["release"])
 
-                    credentials(PasswordCredentials::class)
+                    artifact(sourcesJar) {
+                        classifier = "sources"
+                    }
                 }
             }
         }
