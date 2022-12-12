ef4343a8296893cd06bbcfb2b56c2d7c27714be7
Daniel Frett <daniel.frett@cru.org>
2022-12-12 14:13:13 -0500

    remove v3.11.2 deprecated DataBindingAdapters

--- gto-support-recyclerview/src/main/kotlin/org/ccci/gto/android/common/recyclerview/adapter/DataBindingViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package org.ccci.gto.android.common.recyclerview.adapter
-
-import androidx.databinding.ViewDataBinding
-import org.ccci.gto.android.common.androidx.recyclerview.adapter.DataBindingViewHolder
-
-@Deprecated("Since v3.11.2, use DataBindingViewHolder from gto-support-androidx-recyclerview instead.")
-open class DataBindingViewHolder<B : ViewDataBinding>(binding: B) : DataBindingViewHolder<B>(binding)
--- gto-support-recyclerview/src/main/kotlin/org/ccci/gto/android/common/recyclerview/adapter/SimpleDataBindingAdapter.kt
+++
@@ -1,9 +0,0 @@
-package org.ccci.gto.android.common.recyclerview.adapter
-
-import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.LifecycleOwner
-import org.ccci.gto.android.common.androidx.recyclerview.adapter.SimpleDataBindingAdapter
-
-@Deprecated("Since v3.11.2, use SimpleDataBindingAdapter from gto-support-androidx-recyclerview instead.")
-abstract class SimpleDataBindingAdapter<B : ViewDataBinding>(lifecycleOwner: LifecycleOwner? = null) :
-    SimpleDataBindingAdapter<B>(lifecycleOwner)
