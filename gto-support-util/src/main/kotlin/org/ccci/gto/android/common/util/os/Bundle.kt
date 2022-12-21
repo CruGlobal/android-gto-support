a382a83705e184dea3f31de9b91b67f7a29843fc
Daniel Frett <daniel.frett@cru.org>
2022-12-21 15:53:09 -0500

    remove v3.13.0 deprecated getParcelableArray()

--- gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/os/Bundle.kt
+++ gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/os/Bundle.kt
@@ -8,15 +8,6 @@ import android.os.Parcelable
 import org.ccci.gto.android.common.compat.os.getParcelableArrayCompat
 
 // region Parcelables
-@Deprecated(
-    "Since v3.13.0, use Bundle.getParcelableArrayCompat(key, clazz) instead.",
-    ReplaceWith(
-        "getParcelableArrayCompat(key, clazz)",
-        "org.ccci.gto.android.common.compat.os.getParcelableArrayCompat"
-    )
-)
-fun <T : Parcelable> Bundle.getParcelableArray(key: String?, clazz: Class<T>) = getParcelableArrayCompat(key, clazz)
-
 inline fun <reified T : Parcelable> Bundle.getTypedParcelableArray(key: String?) =
     getParcelableArrayCompat(key, T::class.java)
 // endregion Parcelables
