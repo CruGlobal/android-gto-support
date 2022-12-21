f8d7b9cc1b8e8489223f13d1e6d6908831dd0606
Daniel Frett <daniel.frett@cru.org>
2022-12-21 16:06:21 -0500

    remove v3.14.0 deprecated Snowplow custom events

--- gto-support-snowplow/src/test/kotlin/org/ccci/gto/android/common/snowplow/events/CustomStructuredTest.kt
+++
@@ -1,5 +0,0 @@
-package org.ccci.gto.android.common.snowplow.events
-
-class CustomStructuredTest : CustomEventTest<CustomStructured>() {
-    override fun event() = CustomStructured("category", "action")
-}
