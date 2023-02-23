754257c0d518b6ca3a087c03efe7a3dde10990be
Daniel Frett <daniel.frett@cru.org>
2023-02-23 08:25:43 -0700

    update TimberLogger to match new Logger signature

--- gto-support-androidx-work/src/main/kotlin/org/ccci/gto/android/common/androidx/work/TimberLogger.kt
+++ gto-support-androidx-work/src/main/kotlin/org/ccci/gto/android/common/androidx/work/TimberLogger.kt
@@ -7,35 +7,25 @@ import timber.log.Timber
 
 @SuppressLint("RestrictedApi")
 class TimberLogger(private val loggingLevel: Int) : Logger(loggingLevel) {
-    override fun verbose(tag: String?, message: String?, vararg throwables: Throwable?) =
-        log(Log.VERBOSE, tag, message, *throwables)
+    override fun verbose(tag: String, message: String) = log(Log.VERBOSE, tag, message)
+    override fun verbose(tag: String, message: String, throwable: Throwable) = log(Log.VERBOSE, tag, message, throwable)
+    override fun debug(tag: String, message: String) = log(Log.DEBUG, tag, message)
+    override fun debug(tag: String, message: String, throwable: Throwable) = log(Log.DEBUG, tag, message, throwable)
+    override fun info(tag: String, message: String) = log(Log.INFO, tag, message)
+    override fun info(tag: String, message: String, throwable: Throwable) = log(Log.INFO, tag, message, throwable)
+    override fun warning(tag: String, message: String) = log(Log.WARN, tag, message)
+    override fun warning(tag: String, message: String, throwable: Throwable) = log(Log.WARN, tag, message, throwable)
+    override fun error(tag: String, message: String) = log(Log.ERROR, tag, message)
+    override fun error(tag: String, message: String, throwable: Throwable) = log(Log.ERROR, tag, message, throwable)
 
-    override fun debug(tag: String?, message: String?, vararg throwables: Throwable?) =
-        log(Log.DEBUG, tag, message, *throwables)
-
-    override fun info(tag: String?, message: String?, vararg throwables: Throwable?) =
-        log(Log.INFO, tag, message, *throwables)
-
-    override fun warning(tag: String?, message: String?, vararg throwables: Throwable?) =
-        log(Log.WARN, tag, message, *throwables)
-
-    override fun error(tag: String?, message: String?, vararg throwables: Throwable?) =
-        log(Log.ERROR, tag, message, *throwables)
-
-    private fun log(priority: Int, tag: String?, message: String?, vararg throwables: Throwable?) {
+    private fun log(priority: Int, tag: String, message: String, throwable: Throwable? = null) {
         if (loggingLevel > priority) return
 
-        val throwablesToLog = throwables.filterNotNull()
-        when {
-            throwablesToLog.isEmpty() -> Timber.tag(tag).log(priority, message)
-            else -> {
-                Timber.tag(tag).log(priority, throwablesToLog.first(), message)
-                if (throwablesToLog.size > 1) throwablesToLog.drop(1).forEach { Timber.tag(tag).log(priority, it) }
-            }
+        when (throwable) {
+            null -> Timber.tag(tag).log(priority, message)
+            else -> Timber.tag(tag).log(priority, throwable, message)
         }
     }
 
     fun install() = setLogger(this)
 }
-
-private fun Timber.Forest.tag(tag: String?) = apply { if (tag != null) tag(tag) }
