d6ee9f493fc883fc781f4e3657ef4aa604c6aa7d
Daniel Frett <daniel.frett@cru.org>
2023-07-20 10:49:05 -0600

    ktlint fixes

--- gto-support-dagger/src/main/kotlin/org/ccci/gto/android/common/dagger/FirstNonNullCachingProvider.kt
+++ gto-support-dagger/src/main/kotlin/org/ccci/gto/android/common/dagger/FirstNonNullCachingProvider.kt
@@ -5,5 +5,9 @@ import javax.inject.Provider
 class FirstNonNullCachingProvider<T>(private var resolver: (() -> T?)?) : Provider<T?> {
     private var component: T? = null
 
-    override fun get() = component ?: resolver?.invoke()?.also { component = it; resolver = null }
+    override fun get() = component
+        ?: resolver?.invoke()?.also {
+            component = it
+            resolver = null
+        }
 }
