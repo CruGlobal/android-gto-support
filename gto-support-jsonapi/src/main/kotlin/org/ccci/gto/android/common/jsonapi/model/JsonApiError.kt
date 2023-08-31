864a3a070a5d7066b310e8fb6e222d8ee8c3150d
Daniel Frett <daniel.frett@cru.org>
2023-08-31 10:33:38 -0600

    make JsonApiError a data class

--- gto-support-jsonapi/src/main/kotlin/org/ccci/gto/android/common/jsonapi/model/JsonApiError.kt
+++ gto-support-jsonapi/src/main/kotlin/org/ccci/gto/android/common/jsonapi/model/JsonApiError.kt
@@ -2,7 +2,13 @@ package org.ccci.gto.android.common.jsonapi.model
 
 import org.json.JSONObject
 
-class JsonApiError {
+data class JsonApiError(
+    var status: Int? = null,
+    var title: String? = null,
+    var detail: String? = null,
+    var source: Source? = null,
+    var rawMeta: JSONObject? = null,
+) {
     companion object {
         const val JSON_ERROR_STATUS = "status"
         const val JSON_ERROR_TITLE = "title"
@@ -11,17 +17,30 @@ class JsonApiError {
         const val JSON_ERROR_META = "meta"
     }
 
-    var status: Int? = null
-    var title: String? = null
-    var detail: String? = null
-    var source: Source? = null
-    var rawMeta: JSONObject? = null
-
-    class Source {
+    data class Source(var pointer: String? = null) {
         companion object {
             const val JSON_ERROR_SOURCE_POINTER = "pointer"
         }
+    }
 
-        var pointer: String? = null
+    override fun equals(other: Any?) = when {
+        this === other -> true
+        other == null || this::class != other::class -> false
+        other !is JsonApiError -> false
+        status != other.status -> false
+        title != other.title -> false
+        detail != other.detail -> false
+        source != other.source -> false
+        rawMeta?.toString() != other.rawMeta?.toString() -> false
+        else -> true
+    }
+
+    override fun hashCode(): Int {
+        var result = status ?: 0
+        result = 31 * result + (title?.hashCode() ?: 0)
+        result = 31 * result + (detail?.hashCode() ?: 0)
+        result = 31 * result + (source?.hashCode() ?: 0)
+        result = 31 * result + (rawMeta?.toString()?.hashCode() ?: 0)
+        return result
     }
 }
