520caccc2bf286a87fc1473bc8ea4432a5766d76
Daniel Frett <daniel.frett@cru.org>
2023-08-31 17:55:05 -0600

    use the kotlin test library instead of junit and hamcrest

--- gto-support-jsonapi/src/test/kotlin/org/ccci/gto/android/common/jsonapi/converter/DateTypeConverterTest.kt
+++ gto-support-jsonapi/src/test/kotlin/org/ccci/gto/android/common/jsonapi/converter/DateTypeConverterTest.kt
@@ -2,9 +2,10 @@ package org.ccci.gto.android.common.jsonapi.converter
 
 import java.sql.Time
 import java.util.Date
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 private const val DEFAULT = "yyyy-MM-dd'T'HH:mm:ssX"
 
@@ -18,18 +19,18 @@ class DateTypeConverterTest {
 
     @Test
     fun verifySupports() {
-        assertThat(converter.supports(Any::class.java), equalTo(false))
-        assertThat(converter.supports(Date::class.java), equalTo(true))
-        assertThat(converter.supports(Time::class.java), equalTo(false))
+        assertFalse(converter.supports(Any::class.java))
+        assertTrue(converter.supports(Date::class.java))
+        assertFalse(converter.supports(Time::class.java))
     }
 
     @Test
     fun verifyToString() {
-        assertThat(converter.toString(Date(0)), equalTo("1970-01-01T00:00:00Z"))
+        assertEquals("1970-01-01T00:00:00Z", converter.toString(Date(0)))
     }
 
     @Test
     fun verifyFromString() {
-        assertThat(converter.fromString("1970-01-01T00:00:00Z"), equalTo(Date(0)))
+        assertEquals(Date(0), converter.fromString("1970-01-01T00:00:00Z"))
     }
 }
