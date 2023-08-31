69e4a28a12578b14e245b432fcd90ca9aa27039c
Daniel Frett <daniel.frett@cru.org>
2023-08-31 10:16:18 -0600

    convert JsonApiConverterErrorsTest to Kotlin

---
+++ gto-support-jsonapi/src/test/kotlin/org/ccci/gto/android/common/jsonapi/JsonApiConverterErrorsTest.kt
@@ -0,0 +1,105 @@
+package org.ccci.gto.android.common.jsonapi
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import net.javacrumbs.jsonunit.JsonMatchers.jsonPartEquals
+import net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson
+import org.ccci.gto.android.common.jsonapi.model.JsonApiError
+import org.ccci.gto.android.common.jsonapi.model.JsonApiObject
+import org.ccci.gto.android.common.jsonapi.model.ModelSimple
+import org.hamcrest.MatcherAssert.assertThat
+import org.json.JSONObject
+
+private const val META_SIMPLE = """
+{
+  "detail": {
+    "person_id": ["This person is already assigned"]
+  }
+}
+"""
+
+// sample error taken from https://jsonapi.org/examples/#error-objects-multiple-errors
+private const val ERROR_JSONAPI_MULTIPLE1 = """
+{
+  "errors": [
+    {
+      "status": "403",
+      "source": { "pointer": "/data/attributes/secretPowers" },
+      "detail": "Editing secret powers is not authorized on Sundays."
+    },
+    {
+      "status": "422",
+      "source": { "pointer": "/data/attributes/volume" },
+      "detail": "Volume does not, in fact, go to 11."
+    },
+    {
+      "status": "500",
+      "source": { "pointer": "/data/attributes/reputation" },
+      "title": "The backend responded with an error",
+      "detail": "Reputation service not responding after three requests."
+    }
+  ]
+}
+"""
+
+class JsonApiConverterErrorsTest {
+    @Test
+    fun verifyToJsonSingleSimpleError() {
+        val converter = JsonApiConverter.Builder().addClasses(ModelSimple::class.java).build()
+        val error = simpleError()
+        val json = converter.toJson(JsonApiObject.error<Any>(error))
+        assertThatJson(json).node("data").isAbsent
+        assertThatJson(json).node("errors").isPresent
+        assertThatJson(json).node("errors").isArray.ofLength(1)
+        assertThat(json, jsonPartEquals("errors[0].detail", error.detail))
+        assertThat(json, jsonPartEquals("errors[0].status", "\"${error.status}\""))
+        assertThat(json, jsonPartEquals("errors[0].source.pointer", error.source!!.pointer))
+        assertThat(json, jsonPartEquals("errors[0].meta", META_SIMPLE))
+    }
+
+    @Test
+    fun verifyFromJsonSingleSimpleError() {
+        val converter = JsonApiConverter.Builder()
+            .addClasses(ModelSimple::class.java)
+            .build()
+        val error = simpleError()
+        val json = converter.toJson(JsonApiObject.error<Any>(error))
+        val obj = converter.fromJson(json, ModelSimple::class.java)
+        assertTrue(obj.hasErrors)
+        assertEquals(1, obj.errors.size)
+        val error1 = obj.errors[0]
+        assertEquals(error.detail, error1.detail)
+        assertEquals(error.status, error1.status)
+        assertEquals(error.source?.pointer, error1.source?.pointer)
+        assertThatJson(error1.rawMeta.toString()).isEqualTo(error.rawMeta.toString())
+    }
+
+    @Test
+    fun verifyFromJsonJsonapiMultiple1() {
+        val converter = JsonApiConverter.Builder().addClasses(ModelSimple::class.java).build()
+        val obj = converter.fromJson(ERROR_JSONAPI_MULTIPLE1, ModelSimple::class.java)
+        assertTrue(obj.hasErrors)
+        assertEquals(3, obj.errors.size)
+        val error1 = obj.errors[0]
+        assertEquals(403, error1.status)
+        assertEquals("Editing secret powers is not authorized on Sundays.", error1.detail)
+        assertEquals("/data/attributes/secretPowers", error1.source!!.pointer)
+        val error3 = obj.errors[2]
+        assertEquals(500, error3.status)
+        assertEquals("The backend responded with an error", error3.title)
+        assertEquals("Reputation service not responding after three requests.", error3.detail)
+        assertEquals("/data/attributes/reputation", error3.source!!.pointer)
+    }
+
+    private fun simpleError(): JsonApiError {
+        val error = JsonApiError()
+        error.status = 200
+        error.detail = "Detail human readable message."
+        val source = JsonApiError.Source()
+        source.pointer = "/data/attributes/title"
+        error.source = source
+        error.rawMeta = JSONObject(META_SIMPLE)
+        return error
+    }
+}
