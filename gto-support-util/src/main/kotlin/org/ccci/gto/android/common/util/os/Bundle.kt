94c2e58f95c0495308b9c0811e88b000e05b45f7
Daniel Frett <daniel.frett@cru.org>
2024-03-01 17:18:00 -0700

    add a method to compare Bundles

--- gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/os/Bundle.kt
+++ gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/os/Bundle.kt
@@ -7,6 +7,19 @@ import android.os.Bundle
 import android.os.Parcelable
 import org.ccci.gto.android.common.compat.os.getParcelableArrayCompat
 
+// region equalsBundle()
+@Suppress("DEPRECATION")
+infix fun Bundle?.equalsBundle(other: Bundle?) = when {
+    this === other -> true
+    this == null -> false
+    other == null -> false
+    size() != other.size() -> false
+    keySet() != other.keySet() -> false
+    keySet().any { get(it) != other.get(it) } -> false
+    else -> true
+}
+// endregion equalsBundle()
+
 // region Parcelables
 inline fun <reified T : Parcelable> Bundle.getTypedParcelableArray(key: String?) =
     getParcelableArrayCompat(key, T::class.java)
--- gto-support-util/src/test/kotlin/org/ccci/gto/android/common/util/os/BundleTest.kt
+++ gto-support-util/src/test/kotlin/org/ccci/gto/android/common/util/os/BundleTest.kt
@@ -4,22 +4,43 @@ import android.graphics.Point
 import android.os.Build
 import android.os.Bundle
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.assertArrayEquals
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+import org.junit.Assert.assertFalse
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.Config.NEWEST_SDK
+import org.robolectric.annotation.Config.OLDEST_SDK
 
 private const val KEY1 = "key1"
 private const val KEY2 = "key2"
 
 @RunWith(AndroidJUnit4::class)
-@Config(sdk = [Build.VERSION_CODES.S_V2, Build.VERSION_CODES.TIRAMISU, NEWEST_SDK])
 class BundleTest {
+    // region equalsBundle
+    @Test
+    fun `equalsBundle()`() {
+        val bundle = Bundle().apply {
+            putString("a", "b")
+            putString("b", "a")
+        }
+
+        assertTrue(null equalsBundle null)
+        assertTrue(bundle equalsBundle bundle)
+        assertTrue(bundle equalsBundle (bundle.clone() as Bundle))
+        assertFalse(bundle equalsBundle null)
+        assertFalse(null equalsBundle bundle)
+        assertFalse(bundle equalsBundle Bundle())
+        assertFalse(Bundle() equalsBundle bundle)
+    }
+    // endregion equalsBundle
+
     // region ParcelableArrays
     @Test
+    @Config(sdk = [OLDEST_SDK, Build.VERSION_CODES.S_V2, Build.VERSION_CODES.TIRAMISU, NEWEST_SDK])
     fun verifyGetParcelableArrayReified() {
         val points = arrayOf(Point(0, 0), null, Point(1, 1))
         val bundle = Bundle().apply {
@@ -29,7 +50,7 @@ class BundleTest {
 
         val resp = bundle.getTypedParcelableArray<Point>(KEY1)!!
         assertEquals(points.javaClass, resp.javaClass)
-        assertArrayEquals(points, resp)
+        assertContentEquals(points, resp)
         assertNull(bundle.getTypedParcelableArray<Point>(KEY2))
     }
     // endregion ParcelableArrays
