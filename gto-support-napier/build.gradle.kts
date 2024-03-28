cd9c79aa0b8ee0c26911f77a68c3c88c4f778fd0
Daniel Frett <daniel.frett@cru.org>
2024-03-28 11:01:42 -0600

    deprecate napier logging extensions

--- gto-support-napier/build.gradle.kts
+++ gto-support-napier/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("gto-support.android-conventions")
 }
 
+@Deprecated("Since v4.2.0, Kotlin Multiplatform has migrated to using Kermit for logging")
+
 android.namespace = "org.ccci.gto.android.common.napier"
 
 dependencies {
--- gto-support-napier/src/main/kotlin/org/ccci/gto/android/common/napier/TimberAntilog.kt
+++ gto-support-napier/src/main/kotlin/org/ccci/gto/android/common/napier/TimberAntilog.kt
@@ -5,6 +5,7 @@ import io.github.aakira.napier.Antilog
 import io.github.aakira.napier.LogLevel
 import timber.log.Timber
 
+@Deprecated("Since v4.2.0, Kotlin Multiplatform logging has been migrated to Kermit")
 object TimberAntilog : Antilog() {
     override fun performLog(priority: LogLevel, tag: String?, throwable: Throwable?, message: String?) =
         Timber.apply { tag?.let { tag(it) } }.log(priority.toValue(), throwable, message)
