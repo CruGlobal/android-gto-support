9be9b89de46d1d09c1f04969034edb6fd81c1c42
Daniel Frett <daniel.frett@cru.org>
2024-04-10 14:52:52 -0600

    remove v4.2.0 deprecated db-stream module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -103,5 +103,4 @@ include("testing:gto-support-timber")
 // deprecated modules
 include("gto-support-db-coroutines")
 include("gto-support-db-livedata")
-include("gto-support-db-stream")
 include("gto-support-napier")
