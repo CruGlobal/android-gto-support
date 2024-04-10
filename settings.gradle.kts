a209b42cf373118c1091b60843dcfdfd02e44c97
Daniel Frett <daniel.frett@cru.org>
2024-04-10 15:08:43 -0600

    remove v4.2.0 deprecated napier module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -98,6 +98,3 @@ include("gto-support-viewpager")
 include("testing:gto-support-dagger")
 include("testing:gto-support-picasso")
 include("testing:gto-support-timber")
-
-// deprecated modules
-include("gto-support-napier")
