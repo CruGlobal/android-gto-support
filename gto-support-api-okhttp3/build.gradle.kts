35ea7073bfe418071c479e9824728293be997d62
Daniel Frett <daniel.frett@cru.org>
2024-05-10 17:32:51 -0600

    adjust the dependencies of gto-support-api-okhttp3

--- gto-support-api-okhttp3/build.gradle.kts
+++ gto-support-api-okhttp3/build.gradle.kts
@@ -6,7 +6,6 @@ android.namespace = "org.ccci.gto.android.common.api.okhttp3"
 
 dependencies {
     api(project(":gto-support-api-base"))
-    implementation(project(":gto-support-okhttp3"))
 
-    implementation(libs.okhttp3)
+    api(libs.okhttp3)
 }
