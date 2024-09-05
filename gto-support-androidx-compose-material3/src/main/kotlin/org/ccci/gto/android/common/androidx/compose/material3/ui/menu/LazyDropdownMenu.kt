b0cd2ce57a867339bab9a3f8fd092b7e2a0f60ce
Daniel Frett <daniel.frett@cru.org>
2024-09-05 14:10:37 -0600

    update forked calculateTransformOrigin

--- gto-support-androidx-compose-material3/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/material3/ui/menu/LazyDropdownMenu.kt
+++ gto-support-androidx-compose-material3/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/material3/ui/menu/LazyDropdownMenu.kt
@@ -261,24 +261,24 @@ private data class DropdownMenuPositionProvider(
 /**
  * Copied from [androidx.compose.material3.calculateTransformOrigin]
  */
-private fun calculateTransformOrigin(parentBounds: IntRect, menuBounds: IntRect): TransformOrigin {
+private fun calculateTransformOrigin(anchorBounds: IntRect, menuBounds: IntRect): TransformOrigin {
     val pivotX = when {
-        menuBounds.left >= parentBounds.right -> 0f
-        menuBounds.right <= parentBounds.left -> 1f
+        menuBounds.left >= anchorBounds.right -> 0f
+        menuBounds.right <= anchorBounds.left -> 1f
         menuBounds.width == 0 -> 0f
         else -> {
             val intersectionCenter =
-                (max(parentBounds.left, menuBounds.left) + min(parentBounds.right, menuBounds.right)) / 2
+                (max(anchorBounds.left, menuBounds.left) + min(anchorBounds.right, menuBounds.right)) / 2
             (intersectionCenter - menuBounds.left).toFloat() / menuBounds.width
         }
     }
     val pivotY = when {
-        menuBounds.top >= parentBounds.bottom -> 0f
-        menuBounds.bottom <= parentBounds.top -> 1f
+        menuBounds.top >= anchorBounds.bottom -> 0f
+        menuBounds.bottom <= anchorBounds.top -> 1f
         menuBounds.height == 0 -> 0f
         else -> {
             val intersectionCenter =
-                (max(parentBounds.top, menuBounds.top) + min(parentBounds.bottom, menuBounds.bottom)) / 2
+                (max(anchorBounds.top, menuBounds.top) + min(anchorBounds.bottom, menuBounds.bottom)) / 2
             (intersectionCenter - menuBounds.top).toFloat() / menuBounds.height
         }
     }
