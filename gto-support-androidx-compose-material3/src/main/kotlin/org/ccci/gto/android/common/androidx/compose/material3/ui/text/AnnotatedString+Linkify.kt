f6034129f27a6d2f02d7bab8a363524be4db86c4
Daniel Frett <daniel.frett@cru.org>
2024-10-03 09:51:12 -0600

    add support for addLinks() that will use Linkify to add LinkAnnotations to a String

--- gto-support-androidx-compose-material3/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/material3/ui/text/AnnotatedString+Linkify.kt
+++ gto-support-androidx-compose-material3/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/material3/ui/text/AnnotatedString+Linkify.kt
@@ -4,9 +4,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkInteractionListener
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.core.text.util.LinkifyCompat.LinkifyMask
+import org.ccci.gto.android.common.androidx.compose.ui.text.addLinks
 import org.ccci.gto.android.common.androidx.compose.ui.text.addUriAnnotations
 
 private val DefaultLinkStyle @Composable get() = SpanStyle(
@@ -14,18 +17,33 @@ private val DefaultLinkStyle @Composable get() = SpanStyle(
     textDecoration = TextDecoration.Underline
 )
 
+private val DefaultTextLinkStyles @Composable get() = TextLinkStyles(style = DefaultLinkStyle)
+
 @Composable
+@Deprecated("Since v4.2.3, use addLinks() instead.")
 fun String.addUriAnnotations(@LinkifyMask mask: Int): AnnotatedString {
     val style = DefaultLinkStyle
     return remember(this, mask, style) { addUriAnnotations(mask, style) }
 }
 
 @Composable
+@Deprecated("Since v4.2.3, use addLinks() instead.")
 fun AnnotatedString.addUriAnnotations(@LinkifyMask mask: Int): AnnotatedString {
     val style = DefaultLinkStyle
     return remember(this, mask, style) { addUriAnnotations(mask, style) }
 }
 
 @Composable
-private fun AnnotatedString.Builder.addUriAnnotations(@LinkifyMask mask: Int) =
-    addUriAnnotations(mask, DefaultLinkStyle)
+fun String.addLinks(@LinkifyMask mask: Int, linkInteractionListener: LinkInteractionListener? = null): AnnotatedString {
+    val styles = DefaultTextLinkStyles
+    return remember(this, mask, styles, linkInteractionListener) { addLinks(mask, styles, linkInteractionListener) }
+}
+
+@Composable
+fun AnnotatedString.addLinks(
+    @LinkifyMask mask: Int,
+    linkInteractionListener: LinkInteractionListener? = null,
+): AnnotatedString {
+    val styles = DefaultTextLinkStyles
+    return remember(this, mask, styles, linkInteractionListener) { addLinks(mask, styles, linkInteractionListener) }
+}
--- gto-support-androidx-compose/src/test/kotlin/org/ccci/gto/android/common/androidx/compose/ui/text/AnnotatedStringLinkifyTest.kt
+++ gto-support-androidx-compose/src/test/kotlin/org/ccci/gto/android/common/androidx/compose/ui/text/AnnotatedStringLinkifyTest.kt
@@ -2,21 +2,28 @@ package org.ccci.gto.android.common.androidx.compose.ui.text
 
 import android.text.util.Linkify
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.LinkInteractionListener
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.mockk.mockk
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertSame
-import org.junit.Assert.assertTrue
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertSame
+import kotlin.test.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
 private const val RAW = "pre-url https://www.example.com post-url"
+private const val LINK = "https://www.example.com"
 
 @RunWith(AndroidJUnit4::class)
 class AnnotatedStringLinkifyTest {
     private val linkStyle = mockk<SpanStyle>()
+    private val textLinkStyles = TextLinkStyles()
+    private val linkInteractionListener = LinkInteractionListener {}
 
     @Test
     fun testStringAddUrlAnnotations() {
@@ -48,5 +55,38 @@ class AnnotatedStringLinkifyTest {
         assertEquals(annotation.start, spanStyle.start)
         assertEquals(annotation.end, spanStyle.end)
         assertSame(linkStyle, spanStyle.item)
+    }
+
+    @Test
+    fun testStringAddLinks() {
+        assertLinkAnnotatedString(RAW.addLinks(Linkify.WEB_URLS, textLinkStyles, linkInteractionListener))
+    }
+
+    @Test
+    fun testAnnotatedStringAddLinks() {
+        assertLinkAnnotatedString(
+            AnnotatedString(RAW).addLinks(Linkify.WEB_URLS, textLinkStyles, linkInteractionListener)
+        )
+    }
+
+    @Test
+    fun testAnnotatedStringBuilderAddLinks() {
+        assertLinkAnnotatedString(
+            buildAnnotatedString {
+                append(RAW)
+                addLinks(Linkify.WEB_URLS, textLinkStyles, linkInteractionListener)
+            }
+        )
+    }
+
+    private fun assertLinkAnnotatedString(string: AnnotatedString) {
+        assertEquals(RAW, string.text)
+        assertTrue(string.getLinkAnnotations(0, 7).isEmpty())
+        assertTrue(string.getLinkAnnotations(string.length - 8, string.length).isEmpty())
+
+        val link = assertIs<LinkAnnotation.Url>(string.getLinkAnnotations(10, 10).single().item)
+        assertEquals(LINK, link.url)
+        assertSame(textLinkStyles, link.styles)
+        assertSame(linkInteractionListener, link.linkInteractionListener)
     }
 }
