448048b1939a1aef500494b7d549b667242c8b34
Daniel Frett <daniel.frett@cru.org>
2025-01-09 15:50:49 -0700

    remove v4.2.3 deprecated gto-support-androidx-annotation module

--- gto-support-androidx-annotation/src/commonMain/kotlin/org/ccci/gto/support/androidx/annotation/RestrictTo.kt
+++
@@ -1,6 +0,0 @@
-package org.ccci.gto.support.androidx.annotation
-
-@Deprecated("Since v4.2.3, use androidx.annotation.RestrictTo instead")
-typealias RestrictTo = androidx.annotation.RestrictTo
-@Deprecated("Since v4.2.3, use androidx.annotation.RestrictTo.Scope instead")
-typealias RestrictToScope = androidx.annotation.RestrictTo.Scope
--- gto-support-androidx-annotation/src/commonMain/kotlin/org/ccci/gto/support/androidx/annotation/VisibleForTesting.kt
+++
@@ -1,4 +0,0 @@
-package org.ccci.gto.support.androidx.annotation
-
-@Deprecated("Since v4.2.3, use androidx.annotation.VisibleForTesting instead")
-typealias VisibleForTesting = androidx.annotation.VisibleForTesting
