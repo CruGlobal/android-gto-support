12662f6f1985a0cdc9856f9c6379809c62722132
Daniel Frett <daniel.frett@cru.org>
2025-01-10 15:25:03 -0700

    configUpdates doesn't emit anything when the config is stable

--- gto-support-firebase-config/src/main/kotlin/org/ccci/gto/android/common/firebase/remoteconfig/FirebaseRemoteConfig+Flow.kt
+++ gto-support-firebase-config/src/main/kotlin/org/ccci/gto/android/common/firebase/remoteconfig/FirebaseRemoteConfig+Flow.kt
@@ -1,10 +1,27 @@
 package org.ccci.gto.android.common.firebase.remoteconfig
 
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
-import com.google.firebase.remoteconfig.configUpdates
-import kotlinx.coroutines.flow.map
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.callbackFlow
 
-fun FirebaseRemoteConfig.getBooleanFlow(key: String) = configUpdates.map { getBoolean(key) }
-fun FirebaseRemoteConfig.getDoubleFlow(key: String) = configUpdates.map { getDouble(key) }
-fun FirebaseRemoteConfig.getLongFlow(key: String) = configUpdates.map { getLong(key) }
-fun FirebaseRemoteConfig.getStringFlow(key: String) = configUpdates.map { getString(key) }
+private fun <T> FirebaseRemoteConfig.configFlow(block: () -> T) = callbackFlow {
+    val registration = addOnConfigUpdateListener(
+        object : ConfigUpdateListener {
+            override fun onUpdate(configUpdate: ConfigUpdate) = schedule { trySendBlocking(block()) }
+            override fun onError(error: FirebaseRemoteConfigException) = Unit
+        }
+    )
+
+    trySend(block())
+
+    awaitClose { registration.remove() }
+}
+
+fun FirebaseRemoteConfig.getBooleanFlow(key: String) = configFlow { getBoolean(key) }
+fun FirebaseRemoteConfig.getDoubleFlow(key: String) = configFlow { getDouble(key) }
+fun FirebaseRemoteConfig.getLongFlow(key: String) = configFlow { getLong(key) }
+fun FirebaseRemoteConfig.getStringFlow(key: String) = configFlow { getString(key) }
