cf6a064c5685bae231583bae54fca5d2ddc8be70
Daniel Frett <daniel.frett@cru.org>
2025-03-04 15:24:24 -0700

    adjust the imports for AuthFoundationDefaultsTest

--- gto-support-okta/src/test/kotlin/org/ccci/gto/android/common/okta/authfoundation/AuthFoundationDefaultsTest.kt
+++ gto-support-okta/src/test/kotlin/org/ccci/gto/android/common/okta/authfoundation/AuthFoundationDefaultsTest.kt
@@ -3,17 +3,15 @@ package org.ccci.gto.android.common.okta.authfoundation
 import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.okta.authfoundation.AuthFoundationDefaults
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
-import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.Config.NEWEST_SDK
 import org.robolectric.annotation.Config.OLDEST_SDK
 
 @RunWith(AndroidJUnit4::class)
-@OptIn(ExperimentalCoroutinesApi::class)
 class AuthFoundationDefaultsTest {
     @Test
     @Config(sdk = [OLDEST_SDK, Build.VERSION_CODES.N_MR1, Build.VERSION_CODES.O, NEWEST_SDK])
