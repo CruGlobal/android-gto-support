49d6d79124e532060dc5552b78535705931adf9c
Daniel Frett <daniel.frett@cru.org>
2025-03-04 14:39:23 -0700

    remove firebase-config module
    this module was just added and not actually being used

--- gto-support-firebase-config/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("gto-support.android-conventions")
-}
-
-android.namespace = "org.ccci.gto.android.common.firebase.remoteconfig"
-
-dependencies {
-    implementation(libs.firebase.config)
-}
