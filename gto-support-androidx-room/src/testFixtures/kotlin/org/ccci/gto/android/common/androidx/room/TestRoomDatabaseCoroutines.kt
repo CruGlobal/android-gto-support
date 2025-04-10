432b318cc64bbcc13c772df88e885b2d47a0ac98
Daniel Frett <daniel.frett@cru.org>
2025-04-10 16:10:43 -0600

    convert TestRoomDatabaseCoroutines to Kotlin

---
+++ gto-support-androidx-room/src/testFixtures/kotlin/org/ccci/gto/android/common/androidx/room/TestRoomDatabaseCoroutines.kt
@@ -0,0 +1,18 @@
+package org.ccci.gto.android.common.androidx.room
+
+import android.annotation.SuppressLint
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.CoroutineScope
+
+object TestRoomDatabaseCoroutines {
+    @JvmStatic
+    @SuppressLint("RestrictedApi")
+    fun setCoroutineScope(db: RoomDatabase, scope: CoroutineScope) {
+        db.backingFieldMap[FIELD_COROUTINE_SCOPE] = scope
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun resetCoroutineScope(db: RoomDatabase) {
+        db.backingFieldMap.remove(FIELD_COROUTINE_SCOPE)
+    }
+}
