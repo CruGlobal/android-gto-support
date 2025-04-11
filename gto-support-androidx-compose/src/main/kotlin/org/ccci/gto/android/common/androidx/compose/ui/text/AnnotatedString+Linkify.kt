336e2a7c091a9483cb952da36766e9faa8edc35f
Daniel Frett <daniel.frett@cru.org>
2025-04-11 13:51:24 -0600

    remove v4.2.3 deprecated addUriAnnotations

--- gto-support-androidx-compose/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/ui/text/AnnotatedString+Linkify.kt
+++ gto-support-androidx-compose/src/main/kotlin/org/ccci/gto/android/common/androidx/compose/ui/text/AnnotatedString+Linkify.kt
@@ -5,20 +5,10 @@ import android.text.style.URLSpan
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.LinkInteractionListener
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.core.text.util.LinkifyCompat
 import androidx.core.text.util.LinkifyCompat.LinkifyMask
-
-@Deprecated("Since v4.2.3, use LinkAnnotations instead.")
-private const val TAG_URI = "uri"
-
-@Deprecated("Since v4.2.3, use addLinks() instead.")
-fun String.addUriAnnotations(@LinkifyMask mask: Int, linkStyle: SpanStyle?) = AnnotatedString.Builder(this).apply {
-    val (spannable, spans) = getUrlSpans(this@addUriAnnotations, mask)
-    addUrlSpans(spannable, spans, linkStyle)
-}.toAnnotatedString()
 
 fun String.addLinks(
     @LinkifyMask mask: Int,
@@ -28,18 +18,6 @@ fun String.addLinks(
     val (spannable, spans) = getUrlSpans(this@addLinks, mask)
     addLinks(spannable, spans, styles, linkInteractionListener)
 }.toAnnotatedString()
-
-@Deprecated("Since v4.2.3, use addLinks() instead.")
-fun AnnotatedString.addUriAnnotations(@LinkifyMask mask: Int, linkStyle: SpanStyle?): AnnotatedString {
-    val (spannable, spans) = getUrlSpans(this, mask)
-    return when {
-        spans.isEmpty() -> this
-        else -> buildAnnotatedString {
-            append(this@addUriAnnotations)
-            addUrlSpans(spannable, spans, linkStyle)
-        }
-    }
-}
 
 fun AnnotatedString.addLinks(
     @LinkifyMask mask: Int,
@@ -56,12 +34,6 @@ fun AnnotatedString.addLinks(
     }
 }
 
-@Deprecated("Since v4.2.3, use addLinks() instead.")
-fun AnnotatedString.Builder.addUriAnnotations(@LinkifyMask mask: Int, linkStyle: SpanStyle?) {
-    val (spannable, spans) = getUrlSpans(toAnnotatedString().text, mask)
-    addUrlSpans(spannable, spans, linkStyle)
-}
-
 fun AnnotatedString.Builder.addLinks(
     @LinkifyMask mask: Int,
     styles: TextLinkStyles?,
@@ -76,20 +48,6 @@ private fun getUrlSpans(string: CharSequence, @LinkifyMask mask: Int): Pair<Span
     return spannable to spannable.getSpans(0, spannable.length, URLSpan::class.java)
 }
 
-@Deprecated("Since v4.2.3, use addLinks() instead.")
-private fun AnnotatedString.Builder.addUrlSpans(
-    spannable: SpannableString,
-    spans: Array<URLSpan>,
-    linkStyle: SpanStyle?,
-) {
-    spans.forEach {
-        val spanStart = spannable.getSpanStart(it)
-        val spanEnd = spannable.getSpanEnd(it)
-        addUriAnnotation(it.url, spanStart, spanEnd)
-        if (linkStyle != null) addStyle(linkStyle, spanStart, spanEnd)
-    }
-}
-
 private fun AnnotatedString.Builder.addLinks(
     spannable: SpannableString,
     spans: Array<URLSpan>,
@@ -102,9 +60,3 @@ private fun AnnotatedString.Builder.addLinks(
         addLink(LinkAnnotation.Url(it.url, styles, linkInteractionListener), spanStart, spanEnd)
     }
 }
-
-@Deprecated("Since v4.2.3, use LinkAnnotations instead.")
-private fun AnnotatedString.Builder.addUriAnnotation(uri: String, start: Int, end: Int) =
-    addStringAnnotation(TAG_URI, uri, start, end)
-@Deprecated("Since v4.2.3, use LinkAnnotations instead.")
-fun AnnotatedString.getUriAnnotations(start: Int, end: Int) = getStringAnnotations(TAG_URI, start, end)
