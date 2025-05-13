d5967ca6901338319394fac099ffeed7fc2de90d
Daniel Frett <daniel.frett@cru.org>
2025-05-13 14:08:18 -0600

    add a getSerializableCompat method

--- gto-support-compat/src/test/kotlin/org/ccci/gto/android/common/compat/os/BundleCompatTest.kt
+++ gto-support-compat/src/test/kotlin/org/ccci/gto/android/common/compat/os/BundleCompatTest.kt
@@ -4,10 +4,13 @@ import android.graphics.Point
 import android.os.Build
 import android.os.Bundle
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.assertArrayEquals
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
-import org.junit.Test
+import java.time.Instant
+import java.time.LocalDate
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.Config.NEWEST_SDK
@@ -42,7 +45,23 @@ class BundleCompatTest {
 
         val resp = bundle.getParcelableArrayCompat(KEY1, Point::class.java)!!
         assertEquals(points.javaClass, resp.javaClass)
-        assertArrayEquals(points, resp)
+        assertContentEquals(points, resp)
         assertNull(bundle.getParcelableArrayCompat(KEY2, Point::class.java))
+    }
+
+    @Test
+    fun verifyGetSerializable() {
+        val date = LocalDate.now()
+        val bundle = Bundle().apply {
+            putSerializable(KEY1, date)
+            putSerializable(KEY2, null)
+        }
+
+        assertNull(bundle.getSerializableCompat(KEY1, Instant::class.java))
+        assertNotNull(bundle.getSerializableCompat(KEY1, LocalDate::class.java)) {
+            assertEquals(date, it)
+            assertEquals(date.javaClass, it.javaClass)
+        }
+        assertNull(bundle.getSerializableCompat(KEY2, LocalDate::class.java))
     }
 }
