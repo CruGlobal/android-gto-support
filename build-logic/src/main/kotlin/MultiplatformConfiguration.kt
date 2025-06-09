721d6e73b724735b74c7181b7144f558e86ba826
Daniel Frett <daniel.frett@cru.org>
2025-06-09 10:33:26 -0600

    move kmp config into base build scripts

--- build-logic/src/main/kotlin/MultiplatformConfiguration.kt
+++ build-logic/src/main/kotlin/MultiplatformConfiguration.kt
@@ -1,23 +1,7 @@
-import com.android.build.gradle.LibraryExtension
-import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
 import org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable
-
-// TODO: provide Project using the new multiple context receivers functionality.
-//       this is prototyped in 1.6.20 and will probably reach beta in Kotlin 1.8 or 1.9
-// context(Project)
-internal fun KotlinMultiplatformExtension.configureAndroidTarget(project: Project) {
-    androidTarget {
-        publishAllLibraryVariants()
-    }
-
-    project.extensions.configure<LibraryExtension> {
-        baseConfiguration(project)
-    }
-}
 
 fun KotlinMultiplatformExtension.configureIosTarget() {
     iosArm64 { enableBackgroundTests() }
