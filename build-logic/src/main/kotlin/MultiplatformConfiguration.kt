b2153638d7757bbd31480bab2cd925cd28a78699
Daniel Frett <daniel.frett@cru.org>
2025-06-09 15:19:05 -0600

    remove some unnecessary duplicate logic

--- build-logic/src/main/kotlin/MultiplatformConfiguration.kt
+++ build-logic/src/main/kotlin/MultiplatformConfiguration.kt
@@ -1,23 +1,10 @@
 import com.android.build.gradle.LibraryExtension
-import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
 import org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable
-
-internal fun KotlinMultiplatformExtension.baseConfiguration() {
-    (this as ExtensionAware).extensions.configure<NamedDomainObjectContainer<KotlinSourceSet>>("sourceSets") {
-        named("commonTest") {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-    }
-}
 
 // TODO: provide Project using the new multiple context receivers functionality.
 //       this is prototyped in 1.6.20 and will probably reach beta in Kotlin 1.8 or 1.9
