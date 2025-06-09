afcb5a1d5c9d19320a8d25cb40225f0afd5f0cf6
Daniel Frett <daniel.frett@cru.org>
2025-06-09 14:10:36 -0600

    switch to the new KMP Android Library plugin

--- gto-support-androidx-annotation/build.gradle.kts
+++ gto-support-androidx-annotation/build.gradle.kts
@@ -1,16 +1,19 @@
+import com.android.build.api.dsl.androidLibrary
+
 // Deprecated since v4.2.3, use the standard androidx-annotation library instead
 
 plugins {
-    id("gto-support.multiplatform-android-conventions")
-}
-
-android {
-    namespace = "org.ccci.gto.support.androidx.annotation"
+    id("gto-support.multiplatform-conventions")
 }
 
 kotlin {
+    configureAndroidLibraryTarget()
     configureIosTarget()
     configureJsTarget()
+
+    androidLibrary {
+        namespace = "org.ccci.gto.support.androidx.annotation"
+    }
 
     sourceSets {
         commonMain {
