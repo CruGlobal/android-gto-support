355b6ee1be5a786b87ea1985ff16f0f82650330e
Daniel Frett <daniel.frett@cru.org>
2025-06-11 16:31:09 -0600

    use `clearAndSetSemantics {}` when the node is invisible

--- gto-support-compose/src/commonMain/kotlin/org/ccci/gto/android/common/compose/ui/draw/InvisibleIfModifier.kt
+++ gto-support-compose/src/commonMain/kotlin/org/ccci/gto/android/common/compose/ui/draw/InvisibleIfModifier.kt
@@ -2,8 +2,12 @@ package org.ccci.gto.android.common.compose.ui.draw
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.semantics.clearAndSetSemantics
 
-fun Modifier.invisibleIf(invisible: Boolean) = if (invisible) drawWithContent { } else this
+fun Modifier.invisibleIf(invisible: Boolean) = when {
+    invisible -> drawWithContent { }.clearAndSetSemantics { }
+    else -> this
+}
 
-inline fun Modifier.invisibleIf(crossinline invisible: () -> Boolean) =
-    drawWithContent { if (!invisible()) drawContent() }
+@Deprecated("Since v4.4.2, There is no performance benefit of deferring the read of invisible()")
+inline fun Modifier.invisibleIf(crossinline invisible: () -> Boolean) = invisibleIf(invisible())
