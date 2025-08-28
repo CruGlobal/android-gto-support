077931e4f1f22a0ff7e61300b27407be040aad7c
Daniel Frett <daniel.frett@cru.org>
2025-08-28 16:08:06 -0600

    update ConstrainedStateLifecycleOwnerTest to not use Mocks

--- gto-support-androidx-lifecycle/src/androidUnitTest/kotlin/org/ccci/gto/android/common/androidx/lifecycle/ConstrainedStateLifecycleOwnerTest.kt
+++ gto-support-androidx-lifecycle/src/androidUnitTest/kotlin/org/ccci/gto/android/common/androidx/lifecycle/ConstrainedStateLifecycleOwnerTest.kt
@@ -1,36 +1,36 @@
 package org.ccci.gto.android.common.androidx.lifecycle
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.testing.TestLifecycleOwner
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Rule
-import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.clearInvocations
-import org.mockito.kotlin.inOrder
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
 
 class ConstrainedStateLifecycleOwnerTest {
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    private val parentLifecycleOwner = TestLifecycleOwner(Lifecycle.State.STARTED, UnconfinedTestDispatcher())
-    private lateinit var observer: DefaultLifecycleObserver
+    private val parentLifecycleOwner = TestLifecycleOwner(Lifecycle.State.CREATED, UnconfinedTestDispatcher())
+    private val lifecycleEvents = mutableListOf<Lifecycle.Event>()
+    private val observer = object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            lifecycleEvents += event
+        }
+    }
 
     private lateinit var lifecycleOwner: ConstrainedStateLifecycleOwner
 
-    @Before
+    @BeforeTest
     fun setup() {
-        parentLifecycleOwner.currentState = Lifecycle.State.CREATED
         lifecycleOwner = ConstrainedStateLifecycleOwner(parentLifecycleOwner)
-        observer = mock()
     }
 
     @Test
@@ -50,16 +50,12 @@ class ConstrainedStateLifecycleOwnerTest {
         parentLifecycleOwner.currentState = Lifecycle.State.CREATED
         lifecycleOwner.maxState = Lifecycle.State.STARTED
         lifecycleOwner.lifecycle.addObserver(observer)
-        clearInvocations(observer)
+        lifecycleEvents.clear()
 
         assertEquals(Lifecycle.State.CREATED, lifecycleOwner.lifecycle.currentState)
         parentLifecycleOwner.currentState = Lifecycle.State.RESUMED
         assertEquals(Lifecycle.State.STARTED, lifecycleOwner.lifecycle.currentState)
-        inOrder(observer) {
-            verify(observer).onStart(any())
-            verify(observer, never()).onResume(any())
-            verifyNoMoreInteractions()
-        }
+        assertContentEquals(listOf(Lifecycle.Event.ON_START), lifecycleEvents)
     }
 
     @Test
@@ -67,28 +63,21 @@ class ConstrainedStateLifecycleOwnerTest {
         parentLifecycleOwner.currentState = Lifecycle.State.RESUMED
         lifecycleOwner.maxState = Lifecycle.State.STARTED
         lifecycleOwner.lifecycle.addObserver(observer)
-        clearInvocations(observer)
+        lifecycleEvents.clear()
 
         parentLifecycleOwner.currentState = Lifecycle.State.CREATED
         assertEquals(Lifecycle.State.CREATED, lifecycleOwner.lifecycle.currentState)
-        inOrder(observer) {
-            verify(observer, never()).onPause(any())
-            verify(observer).onStop(any())
-            verifyNoMoreInteractions()
-        }
+        assertContentEquals(listOf(Lifecycle.Event.ON_STOP), lifecycleEvents)
     }
 
     @Test
     fun verifyChangingMaxStateTriggersEvents() {
         parentLifecycleOwner.currentState = Lifecycle.State.RESUMED
         lifecycleOwner.lifecycle.addObserver(observer)
-        clearInvocations(observer)
+        lifecycleEvents.clear()
 
         assertEquals(Lifecycle.State.RESUMED, lifecycleOwner.lifecycle.currentState)
         lifecycleOwner.maxState = Lifecycle.State.STARTED
-        inOrder(observer) {
-            verify(observer).onPause(any())
-            verifyNoMoreInteractions()
-        }
+        assertContentEquals(listOf(Lifecycle.Event.ON_PAUSE), lifecycleEvents)
     }
 }
