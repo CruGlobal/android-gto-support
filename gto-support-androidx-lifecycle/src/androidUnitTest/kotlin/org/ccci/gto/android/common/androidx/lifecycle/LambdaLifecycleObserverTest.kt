b02a82367d336c0f6fd5386fb761d5c7a26eb730
Daniel Frett <daniel.frett@cru.org>
2025-08-28 15:14:14 -0600

    convert LambdaLifecycleObserverTest to not use mocks

--- gto-support-androidx-lifecycle/src/androidUnitTest/kotlin/org/ccci/gto/android/common/androidx/lifecycle/LambdaLifecycleObserverTest.kt
+++ gto-support-androidx-lifecycle/src/androidUnitTest/kotlin/org/ccci/gto/android/common/androidx/lifecycle/LambdaLifecycleObserverTest.kt
@@ -3,35 +3,26 @@ package org.ccci.gto.android.common.androidx.lifecycle
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.testing.TestLifecycleOwner
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import org.junit.Before
-import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.eq
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
-import org.mockito.kotlin.verify
 
 class LambdaLifecycleObserverTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     private val lifecycleOwner = TestLifecycleOwner(coroutineDispatcher = UnconfinedTestDispatcher())
-    private lateinit var lambda: (LifecycleOwner) -> Unit
-
-    @Before
-    fun setup() {
-        lambda = mock()
-    }
+    private val onDestroyCalls = mutableListOf<LifecycleOwner>()
 
     @Test
     fun testOnDestroy() {
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        lifecycleOwner.lifecycle.onDestroy(lambda)
+        lifecycleOwner.lifecycle.onDestroy { onDestroyCalls.add(it) }
 
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
-        verify(lambda, never()).invoke(any())
+        assertTrue(onDestroyCalls.isEmpty())
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        verify(lambda).invoke(eq(lifecycleOwner))
+        assertEquals(listOf<LifecycleOwner>(lifecycleOwner), onDestroyCalls)
     }
 }
