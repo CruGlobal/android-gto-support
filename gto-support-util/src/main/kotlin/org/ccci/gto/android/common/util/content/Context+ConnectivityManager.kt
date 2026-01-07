4f8a1bc62c60a03d51dffaff6375d47f58799994
Daniel Frett <daniel.frett@cru.org>
2026-01-07 10:52:40 -0700

    remove no longer used fallback logic

--- gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/content/Context+ConnectivityManager.kt
+++ gto-support-util/src/main/kotlin/org/ccci/gto/android/common/util/content/Context+ConnectivityManager.kt
@@ -11,9 +11,7 @@ import androidx.annotation.RequiresPermission
 @RequiresPermission(ACCESS_NETWORK_STATE)
 fun Context.isConnected(): Boolean {
     val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager ?: return false
-    return when {
-        Build.VERSION.SDK_INT < Build.VERSION_CODES.M -> connectivityManager.activeNetworkInfo?.isConnected
-        else -> connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-            ?.let { it.hasCapability(NET_CAPABILITY_INTERNET) && it.hasCapability(NET_CAPABILITY_VALIDATED) }
-    } ?: false
+    return connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        ?.let { it.hasCapability(NET_CAPABILITY_INTERNET) && it.hasCapability(NET_CAPABILITY_VALIDATED) }
+        ?: false
 }
