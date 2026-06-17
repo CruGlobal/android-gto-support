586669efcc20295be7c52219cf68f1494e35b0eb
Daniel Frett <daniel.frett@cru.org>
2026-06-17 09:25:03 -0600

    remove unused internal constant

--- gto-support-androidx-room/src/main/kotlin/org/ccci/gto/android/common/androidx/room/RoomDatabase+Coroutines.kt
+++ gto-support-androidx-room/src/main/kotlin/org/ccci/gto/android/common/androidx/room/RoomDatabase+Coroutines.kt
@@ -3,7 +3,5 @@ package org.ccci.gto.android.common.androidx.room
 import android.annotation.SuppressLint
 import androidx.room.RoomDatabase
 
-internal const val FIELD_COROUTINE_SCOPE = "GtoCoroutineScope"
-
 @get:SuppressLint("RestrictedApi")
 val RoomDatabase.coroutineScope get() = getCoroutineScope()
